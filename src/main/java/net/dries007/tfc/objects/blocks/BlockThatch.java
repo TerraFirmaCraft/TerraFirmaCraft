5a86ee883bc3361bad226ffe0cf5e6f8ea211f6a
Bunsan <bunsan@users.noreply.github.com>
2018-04-17 11:27:39 +0930

    Implement thatch block that behaves like web block, but causes reduced fall damage and sets fall distance to 0. Fixed movement being effected by leaf blocks and have passing through leaves cause reduced fall damage, but sets fall distance to 0.

---
+++ src/main/java/net/dries007/tfc/objects/blocks/BlockThatch.java
@@ -0,0 +1,62 @@
+package net.dries007.tfc.objects.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class BlockThatch extends Block
+{
+    public BlockThatch(Material material)
+    {
+        super(material);
+        setSoundType(SoundType.PLANT);
+        setHardness(0.6F);
+        //OreDictionaryHelper.register(this, "thatch");
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos)
+    {
+        return NULL_AABB;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.CUTOUT_MIPPED;
+    }
+
+    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn)
+    {
+        // Player will take damage when hitting thatch if fall is over 13 blocks, fall damage is then set to 0.
+        entityIn.fall((entityIn.fallDistance - 10), 1.0F); // TODO: 17/4/18 balance fall damage reduction.
+        entityIn.fallDistance = 0;
+        entityIn.setInWeb();
+    }
+
+
+    @SideOnly(Side.CLIENT)
+    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side)
+    {
+        return true;
+    }
+
+}
