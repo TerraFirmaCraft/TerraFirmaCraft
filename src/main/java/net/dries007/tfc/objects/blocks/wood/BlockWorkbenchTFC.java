455aac3a72351563d77017de9fc4d7c6d9ad0965
Bunsan <bunsan@users.noreply.github.com>
2018-04-25 21:45:59 +0930

    Added oredictionary registration. Changed Bookcase to bookshelf to be consistent with Vanilla.

--- src/main/java/net/dries007/tfc/objects/blocks/wood/BlockWorkbenchTFC.java
+++ src/main/java/net/dries007/tfc/objects/blocks/wood/BlockWorkbenchTFC.java
@@ -1,6 +1,7 @@
 package net.dries007.tfc.objects.blocks.wood;
 
 import net.dries007.tfc.objects.Wood;
+import net.dries007.tfc.util.OreDictionaryHelper;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -26,6 +27,8 @@ public class BlockWorkbenchTFC extends Block {
         setSoundType(SoundType.WOOD);
         setHardness(2.0F).setResistance(5.0F);
         setHarvestLevel("axe", 0);
+        OreDictionaryHelper.register(this, "workbench");
+
     }
     @SideOnly(Side.CLIENT)
     public BlockRenderLayer getBlockLayer()
