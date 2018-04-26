fefe5fb5a73dca7412649df70b798c168bf0be5a
Dries007 <admin@dries007.net>
2018-04-26 21:48:08 +0200

    Added license header to files.

--- src/main/java/net/dries007/tfc/world/classic/genlayers/datalayers/tree/GenLayerTreeInit.java
+++ src/main/java/net/dries007/tfc/world/classic/genlayers/datalayers/tree/GenLayerTreeInit.java
@@ -1,3 +1,8 @@
+/*
+ * Work under Copyright. Licensed under the EUPL.
+ * See the project README.md and LICENSE.txt for more information.
+ */
+
 package net.dries007.tfc.world.classic.genlayers.datalayers.tree;
 
 import net.minecraft.world.gen.layer.IntCache;
