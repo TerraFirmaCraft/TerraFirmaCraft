47b9006740b0aefd458d5c63c1c898c8ad91c1ca
alcatrazEscapee <alex@molleroneill.com>
2018-06-16 14:04:46 -0400

    Reformat and rearrange code. THIS IS COOL

--- src/main/java/net/dries007/tfc/util/OreSpawnData.java
+++ src/main/java/net/dries007/tfc/util/OreSpawnData.java
@@ -5,7 +5,15 @@
 
 package net.dries007.tfc.util;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import com.google.common.collect.ImmutableList;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
@@ -13,15 +21,7 @@ import com.typesafe.config.ConfigValue;
 import net.dries007.tfc.TerraFirmaCraft;
 import net.dries007.tfc.objects.Ore;
 import net.dries007.tfc.objects.Rock;
-import net.dries007.tfc.objects.Rock.*;
-import net.minecraft.block.Block;
-import net.minecraft.block.state.IBlockState;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.io.File;
-import java.util.List;
-import java.util.Map;
+import net.dries007.tfc.objects.Rock.Category;
 
 // todo: someone look through assets/tfc/config/tfc_ore_spawn_data.json and verify that everything looks good  -Alex (alcatrazEscapee)
 public class OreSpawnData
@@ -66,14 +66,17 @@ public class OreSpawnData
             {
                 throw new Error("Problem creating TFC world gen json: (unspecified error).");
             }
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             TerraFirmaCraft.getLog().fatal("Problem creating TFC world gen json: ", e);
         }
         TerraFirmaCraft.getLog().info("Complete.");
     }
 
     // todo: test that all the exceptions and try statements catch problems with json
-    public static void reloadOreGen(){
+    public static void reloadOreGen()
+    {
         Config genData = ConfigFactory.parseFile(genFile);
         ImmutableList.Builder<OreEntry> builder = new ImmutableList.Builder<>();
 
@@ -89,7 +92,8 @@ public class OreSpawnData
             final int maxY;
             final int density;
             List<String> baseStrings;
-            try {
+            try
+            {
                 entryData = genData.getConfig(genEntry.getKey());
 
                 //ore = Ore.valueOf(entryData.getString("ore").toUpperCase());
@@ -97,46 +101,62 @@ public class OreSpawnData
                 shape = SpawnType.valueOf(entryData.getString("shape").toUpperCase());
 
                 rarity = entryData.getInt("rarity");
-                minY = entryData.getInt("minimum-height");
-                maxY = entryData.getInt("maximum-height");
+                minY = entryData.getInt("minimum_height");
+                maxY = entryData.getInt("maximum_height");
                 density = entryData.getInt("density");
 
-                baseStrings = entryData.getStringList("base-rocks");
+                baseStrings = entryData.getStringList("base_rocks");
 
-            } catch (Exception e) {
-                TerraFirmaCraft.getLog().warn("Problem parsing data for ore generation entry with key: \""+genEntry.getKey()+"\" Skipping.");
+            }
+            catch (Exception e)
+            {
+                TerraFirmaCraft.getLog().warn("Problem parsing data for ore generation entry with key: \"" + genEntry.getKey() + "\" Skipping.");
                 continue;
             }
 
-            try{
+            try
+            {
                 ore = Ore.valueOf(entryData.getString("ore").toUpperCase());
+                if (ore == Ore.UNKNOWN_ORE)
+                    throw new Exception("Can't assign to unknown ore");
                 state = null;
-            } catch (Exception e1) {
-                try {
+            }
+            catch (Exception e1)
+            {
+                try
+                {
                     String blockName = entryData.getString("ore");
                     Block block = Block.getBlockFromName(blockName);
-                    if(block != null)
+                    if (block != null)
                     {
                         state = block.getDefaultState();
                         ore = Ore.UNKNOWN_ORE;
-                    }else{
+                    }
+                    else
+                    {
                         TerraFirmaCraft.getLog().warn("Problem parsing IBlockState: block doesn't exist for ore generation entry with key: \\\"\"+genEntry.getKey()+\"\\\" Skipping.\"");
                         continue;
                     }
-                } catch (Exception e2){
+                }
+                catch (Exception e2)
+                {
                     TerraFirmaCraft.getLog().warn("Problem parsing Ore / IBlockState for ore generation entry with key: \\\"\"+genEntry.getKey()+\"\\\" Skipping.\"");
                     continue;
                 }
             }
 
             ImmutableList.Builder<Rock> b = new ImmutableList.Builder<>();
-            for(String s : baseStrings)
+            for (String s : baseStrings)
             {
-                try {
+                try
+                {
                     Rock rock = Rock.valueOf(s.toUpperCase());
                     b.add(rock);
-                } catch(IllegalArgumentException e1){
-                    try{
+                }
+                catch (IllegalArgumentException e1)
+                {
+                    try
+                    {
                         Category category = Category.valueOf(s.toUpperCase());
                         for (Rock rock : Rock.values())
                         {
@@ -145,63 +165,20 @@ public class OreSpawnData
                                 b.add(rock);
                             }
                         }
-                    } catch(IllegalArgumentException e2){
-                        TerraFirmaCraft.getLog().warn("Problem parsing base rock \""+s+"\" for ore generation entry with key+\""+genEntry.getKey()+"\" Skipping");
+                    }
+                    catch (IllegalArgumentException e2)
+                    {
+                        TerraFirmaCraft.getLog().warn("Problem parsing base rock \"" + s + "\" for ore generation entry with key+\"" + genEntry.getKey() + "\" Skipping");
                     }
                 }
             }
 
             builder.add(new OreEntry(ore, state, size, shape, b.build(), rarity, minY, maxY, density));
             TOTAL_WEIGHT += 1.0D / (double) rarity;
-            TerraFirmaCraft.getLog().debug("Added ore generation entry for "+genEntry.getKey());
+            TerraFirmaCraft.getLog().debug("Added ore generation entry for " + genEntry.getKey());
         }
 
         ORE_SPAWN_DATA = builder.build();
-    }
-
-    public static final class OreEntry
-    {
-        public final Ore ore;
-        public final IBlockState state;
-        public final SpawnType type;
-        public final SpawnSize size;
-        public final int rarity;
-        public final ImmutableList<Rock> baseRocks;
-        public final int minY;
-        public final int maxY;
-        public final double weight;
-        public final double density;
-
-        private OreEntry(@Nonnull Ore ore, @Nullable IBlockState state, SpawnSize size, SpawnType type, ImmutableList<Rock> baseRocks, int rarity, int minY, int maxY, int density)
-        {
-            this.ore = ore;
-            this.state = state;
-            this.size = size;
-            this.type = type;
-            this.baseRocks = baseRocks;
-
-            this.rarity = rarity;
-            this.weight = 1.0D / (double) rarity;
-            this.minY = minY;
-            this.maxY = maxY;
-            this.density= 0.01D * (double)density; // For debug purposes, removing the 0.01D will lead to ore veins being full size, easy to see shapes
-
-            if(ore == Ore.UNKNOWN_ORE && state == null) throw new IllegalStateException("Ore Entry has neither a IBlockState or a Ore type");
-        }
-
-        @Override
-        public String toString() {
-            return "OreSpawnData{" +
-                "ore=" + ore +
-                ", type=" + type +
-                ", size=" + size +
-                ", rarity=" + rarity +
-                ", baseRocks=" + baseRocks +
-                ", minY=" + minY +
-                ", maxY=" + maxY +
-                ", density=" + density +
-                '}';
-        }
     }
 
     public enum SpawnType
@@ -232,6 +209,53 @@ public class OreSpawnData
         {
             this.radius = radius;
             this.densityModifier = densityModifier;
+        }
+    }
+
+    public static final class OreEntry
+    {
+        public final Ore ore;
+        public final IBlockState state;
+        public final SpawnType type;
+        public final SpawnSize size;
+        public final int rarity;
+        public final ImmutableList<Rock> baseRocks;
+        public final int minY;
+        public final int maxY;
+        public final double weight;
+        public final double density;
+
+        private OreEntry(@Nonnull Ore ore, @Nullable IBlockState state, SpawnSize size, SpawnType type, ImmutableList<Rock> baseRocks, int rarity, int minY, int maxY, int density)
+        {
+            this.ore = ore;
+            this.state = state;
+            this.size = size;
+            this.type = type;
+            this.baseRocks = baseRocks;
+
+            this.rarity = rarity;
+            this.weight = 1.0D / (double) rarity;
+            this.minY = minY;
+            this.maxY = maxY;
+            this.density = 0.01D * (double) density; // For debug purposes, removing the 0.01D will lead to ore veins being full size, easy to see shapes
+
+            if (ore == Ore.UNKNOWN_ORE && state == null)
+                throw new IllegalStateException("Ore Entry has neither a IBlockState or a Ore type");
+        }
+
+        @Override
+        public String toString()
+        {
+            return "OreSpawnData{" +
+                "ore=" + ore +
+                ", type=" + type +
+                ", size=" + size +
+                ", rarity=" + rarity +
+                ", baseRocks=" + baseRocks +
+                ", minY=" + minY +
+                ", maxY=" + maxY +
+                ", density=" + density +
+                '}';
         }
     }
 }
