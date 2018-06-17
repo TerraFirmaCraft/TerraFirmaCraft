d1138ad8a1fb922f605e5297d88d2154e05e22e2
Dries007 <admin@dries007.net>
2018-06-17 15:53:48 +0200

    Some fixes for null issues with orespawns (@alcatrazEscapee)

--- src/main/java/net/dries007/tfc/world/classic/chunkdata/ChunkDataOreSpawned.java
+++ src/main/java/net/dries007/tfc/world/classic/chunkdata/ChunkDataOreSpawned.java
@@ -5,6 +5,9 @@
 
 package net.dries007.tfc.world.classic.chunkdata;
 
+import javax.annotation.Nullable;
+
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTUtil;
 import net.minecraft.util.math.BlockPos;
@@ -14,24 +17,30 @@ import net.dries007.tfc.util.OreSpawnData;
 
 public class ChunkDataOreSpawned
 {
+    @Nullable
     public final Ore ore;
+    @Nullable
+    public final IBlockState state;
     public final OreSpawnData.SpawnSize size;
     public final Ore.Grade grade;
     public final BlockPos pos;
     public final int count;
 
-    public ChunkDataOreSpawned(Ore ore, OreSpawnData.SpawnSize size, Ore.Grade grade, BlockPos pos, int count)
+    public ChunkDataOreSpawned(@Nullable Ore ore, @Nullable IBlockState state, OreSpawnData.SpawnSize size, Ore.Grade grade, BlockPos pos, int count)
     {
         this.ore = ore;
+        this.state = state;
         this.size = size;
         this.grade = grade;
         this.pos = pos;
         this.count = count;
+        if (ore == null && state == null) throw new IllegalArgumentException("Both ore and state can't be null.");
     }
 
     public ChunkDataOreSpawned(NBTTagCompound tag)
     {
-        ore = Ore.valueOf(tag.getString("ore").toUpperCase());
+        ore = tag.hasKey("ore") ? Ore.valueOf(tag.getString("ore").toUpperCase()) : null;
+        state = tag.hasKey("state") ? NBTUtil.readBlockState(tag.getCompoundTag("state")) : null;
         grade = Ore.Grade.valueOf(tag.getString("grade").toUpperCase());
         size = OreSpawnData.SpawnSize.valueOf(tag.getString("size").toUpperCase());
         pos = NBTUtil.getPosFromTag(tag.getCompoundTag("pos"));
@@ -41,7 +50,8 @@ public class ChunkDataOreSpawned
     public NBTTagCompound serialize()
     {
         NBTTagCompound root = new NBTTagCompound();
-        root.setString("ore", ore.name());
+        if (ore != null) root.setString("ore", ore.name());
+        if (state != null) root.setTag("state", NBTUtil.writeBlockState(new NBTTagCompound(), state));
         root.setString("grade", grade.name());
         root.setString("size", size.name());
         root.setTag("pos", NBTUtil.createPosTag(pos));
@@ -52,11 +62,13 @@ public class ChunkDataOreSpawned
     @Override
     public String toString()
     {
-        return "OreSpawned{" +
-                "ore=" + ore +
-                ", grade=" + grade +
-                ", pos=" + pos +
-                ", count=" + count +
-                '}';
+        return "ChunkDataOreSpawned{" +
+            "ore=" + ore +
+            ", state=" + state +
+            ", size=" + size +
+            ", grade=" + grade +
+            ", pos=" + pos +
+            ", count=" + count +
+            '}';
     }
 }
