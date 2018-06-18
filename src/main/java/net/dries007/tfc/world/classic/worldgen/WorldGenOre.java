5c31c5355ca76f0297112cd64f217210d991e72c
alcatrazEscapee <alex@molleroneill.com>
2018-06-17 20:00:34 -0400

    Added surface ore pieces (the world gen for it, not the actual blocks). Currently will generate raw stone blocks, or ore blocks on the surface based on the underground ore composition. It is currently generating WITHOUT access to chunk data. Anything that needs data on ore veins should call WorldGenOre.getNearbyVeins()

--- src/main/java/net/dries007/tfc/world/classic/worldgen/WorldGenOre.java
+++ src/main/java/net/dries007/tfc/world/classic/worldgen/WorldGenOre.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
@@ -24,6 +23,7 @@ import net.dries007.tfc.objects.blocks.stone.BlockOreTFC;
 import net.dries007.tfc.objects.blocks.stone.BlockRockVariant;
 import net.dries007.tfc.util.OreSpawnData;
 import net.dries007.tfc.world.classic.ChunkGenTFC;
+import net.dries007.tfc.world.classic.WorldTypeTFC;
 import net.dries007.tfc.world.classic.chunkdata.ChunkDataTFC;
 import net.dries007.tfc.world.classic.worldgen.vein.VeinType;
 import net.dries007.tfc.world.classic.worldgen.vein.VeinTypeCluster;
@@ -31,10 +31,78 @@ import net.dries007.tfc.world.classic.worldgen.vein.VeinTypeCluster;
 public class WorldGenOre implements IWorldGenerator
 {
 
-    private static final int NUM_ROLLS = 3;
+    private static final int NUM_ROLLS = 2;
     private static final int CHUNK_RADIUS = 2;
     public static final int VEIN_MAX_RADIUS = 16 * CHUNK_RADIUS;
     public static final int VEIN_MAX_RADIUS_SQUARED = VEIN_MAX_RADIUS * VEIN_MAX_RADIUS;
+
+    // Used to generate chunk
+    public static List<VeinType> getNearbyVeins(World world, int chunkX, int chunkZ, long worldSeed, int radius)
+    {
+        List<VeinType> veins = new ArrayList<>();
+
+        for (int x = -radius; x <= radius; x++)
+        {
+            for (int z = -radius; z <= radius; z++)
+            {
+                List<VeinType> vein = getVeinsAtChunk(world, chunkX + x, chunkZ + z, worldSeed);
+                if (!vein.isEmpty()) veins.addAll(vein);
+            }
+        }
+
+        return veins;
+    }
+
+    // Gets veins at a single chunk. Deterministic for a specific chunk x/z and world seed
+    @Nonnull
+    private static List<VeinType> getVeinsAtChunk(World world, int chunkX, int chunkZ, Long worldSeed)
+    {
+        Random rand = new Random(worldSeed + chunkX * 341873128712L + chunkZ * 132897987541L);
+        List<VeinType> veins = new ArrayList<>();
+
+        for (int i = 0; i < NUM_ROLLS; i++)
+        {
+
+            OreSpawnData.OreEntry oreType;
+            BlockPos startPos;
+
+            if (rand.nextDouble() < OreSpawnData.getTotalWeight())
+            {
+
+                oreType = getWeightedOreType(rand);
+                startPos = new BlockPos(
+                    chunkX * 16 + 8 + rand.nextInt(16),
+                    oreType.minY + rand.nextInt(oreType.maxY - oreType.minY),
+                    chunkZ * 16 + 8 + rand.nextInt(16)
+                );
+
+                Ore.Grade grade = Ore.Grade.NORMAL;
+                if (oreType.ore != null && oreType.ore.graded)
+                {
+                    int gradeInt = rand.nextInt(100);
+                    if (gradeInt < 20) grade = Ore.Grade.RICH;
+                    else if (gradeInt < 50) grade = Ore.Grade.POOR;
+                }
+
+                veins.add(new VeinTypeCluster(startPos, oreType, grade, rand));
+            }
+        }
+        return veins;
+    }
+
+    @Nonnull
+    private static OreSpawnData.OreEntry getWeightedOreType(Random rand)
+    {
+        double r = rand.nextDouble() * OreSpawnData.getTotalWeight();
+        double countWeight = 0.0;
+        for (OreSpawnData.OreEntry ore : OreSpawnData.getOreSpawnEntries())
+        {
+            countWeight += ore.weight;
+            if (countWeight >= r)
+                return ore;
+        }
+        throw new RuntimeException("Problem choosing random ore weights. Should never be shown");
+    }
 
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider)
@@ -47,7 +115,7 @@ public class WorldGenOre implements IWorldGenerator
         // Check dimension is overworld
         if (world.provider.getDimension() != 0) return;
 
-        List<VeinType> veins = getNearbyVeins(chunkX, chunkZ, world.getSeed(), chunkData);
+        List<VeinType> veins = getNearbyVeins(world, chunkX, chunkZ, world.getSeed(), CHUNK_RADIUS);
         if (veins.isEmpty()) return;
 
         // Set constant values here
@@ -57,6 +125,12 @@ public class WorldGenOre implements IWorldGenerator
         for (VeinType vein : veins)
         {
             // Do checks here that are specific to each vein
+            if (!vein.oreSpawnData.baseRocks.contains(chunkData.getRock1(0, 0).rock) &&
+                !vein.oreSpawnData.baseRocks.contains(chunkData.getRock2(0, 0).rock) &&
+                !vein.oreSpawnData.baseRocks.contains(chunkData.getRock3(0, 0).rock))
+                continue;
+            if (vein.pos.getY() >= WorldTypeTFC.SEALEVEL + chunkData.getSeaLevelOffset(vein.pos))
+                continue;
 
             for (int x = 0; x < 16; x++)
             {
@@ -93,82 +167,15 @@ public class WorldGenOre implements IWorldGenerator
         }
 
         // TODO: remove the "blocks spawned" count from ore vein chunk data. Not worth it to include (because it won't be accurate)
-        VeinType veinAtChunk = getVeinAtChunk(chunkX, chunkZ, world.getSeed(), chunkData);
-        if (veinAtChunk != null)
-        {
-            chunkData.addSpawnedOre(veinAtChunk.oreSpawnData.ore, veinAtChunk.oreSpawnData.state, veinAtChunk.oreSpawnData.size, veinAtChunk.grade, veinAtChunk.pos, 0);
-        }
-    }
-
-    // Used to generate chunk
-    private List<VeinType> getNearbyVeins(int chunkX, int chunkZ, long worldSeed, ChunkDataTFC chunkData)
-    {
-        List<VeinType> veins = new ArrayList<>();
-
-        for (int x = -CHUNK_RADIUS; x <= CHUNK_RADIUS; x++)
-        {
-            for (int z = -CHUNK_RADIUS; z <= CHUNK_RADIUS; z++)
-            {
-                VeinType vein = getVeinAtChunk(chunkX + x, chunkZ + z, worldSeed, chunkData);
-                if (vein != null) veins.add(vein);
-            }
-        }
-
-        return veins;
-    }
-
-    // Gets veins at a single chunk. Deterministic for a specific chunk x/z and world seed
-    @Nullable
-    private VeinType getVeinAtChunk(int chunkX, int chunkZ, Long worldSeed, ChunkDataTFC chunkData)
-    {
-        Random rand = new Random(worldSeed + chunkX * 341873128712L + chunkZ * 132897987541L);
-
-        if (rand.nextDouble() < OreSpawnData.getTotalWeight())
-        {
-            OreSpawnData.OreEntry oreType;
-            BlockPos startPos;
-            int rolls = 0;
-            while (true)
-            {
-                oreType = getWeightedOreType(rand);
-                startPos = new BlockPos(
-                    chunkX * 16 + rand.nextInt(16),
-                    oreType.minY + rand.nextInt(oreType.maxY - oreType.minY), // Todo: make max value be the min of top block and maxY ? Is that possible?
-                    chunkZ * 16 + rand.nextInt(16)
-                );
-
-                if (oreType.baseRocks.contains(chunkData.getRockHeight(startPos.getX(), startPos.getY(), startPos.getZ()).rock))
-                {
-                    break;
-                }
-
-                rolls++;
-                if (rolls >= NUM_ROLLS) return null;
-            }
-            Ore.Grade grade = Ore.Grade.NORMAL;
-            if (oreType.ore != null && oreType.ore.graded)
-            {
-                int gradeInt = rand.nextInt(100);
-                if (gradeInt < 20) grade = Ore.Grade.RICH;
-                else if (gradeInt < 50) grade = Ore.Grade.POOR;
-            }
-            return new VeinTypeCluster(startPos, oreType, grade, rand);
-        }
-        return null;
-    }
-
-    @Nonnull
-    private OreSpawnData.OreEntry getWeightedOreType(Random rand)
-    {
-        double r = rand.nextDouble() * OreSpawnData.getTotalWeight();
-        double countWeight = 0.0;
-        for (OreSpawnData.OreEntry ore : OreSpawnData.getOreSpawnEntries())
-        {
-            countWeight += ore.weight;
-            if (countWeight >= r)
-                return ore;
-        }
-        throw new RuntimeException("Problem choosing random ore weights. Should never be shown");
+        // TODO: re-add this as chunk data
+        //List<VeinType> veinAtChunk = getVeinsAtChunk(chunkX, chunkZ, world.getSeed(), chunkData);
+        //if (!veinAtChunk.isEmpty())
+        //{
+        //    veinAtChunk.forEach(v ->
+        //        chunkData.addSpawnedOre(v.oreSpawnData.ore, v.oreSpawnData.state, v.oreSpawnData.size, v.grade, v.pos, 0)
+        //    );
+        //chunkData.addSpawnedOre(veinAtChunk.oreSpawnData.ore, veinAtChunk.oreSpawnData.state, veinAtChunk.oreSpawnData.size, veinAtChunk.grade, veinAtChunk.pos, 0);
+        //}
     }
 
 }
