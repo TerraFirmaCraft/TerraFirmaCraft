c41b82679557261db149e1052391d03265c55964
alcatrazEscapee <alex@molleroneill.com>
2018-07-20 22:23:59 -0400

    Added stuff to Ore to mimic other registries. I will not touch ore / rock category anymore for at least a while. Trees next.

--- src/main/java/net/dries007/tfc/api/types/Ore.java
+++ src/main/java/net/dries007/tfc/api/types/Ore.java
@@ -5,18 +5,58 @@
 
 package net.dries007.tfc.api.types;
 
+import javax.annotation.Nullable;
+
+import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.IForgeRegistryEntry;
+
+import net.dries007.tfc.objects.Metal;
 
 /**
  * todo: document API
  */
 public class Ore extends IForgeRegistryEntry.Impl<Ore>
 {
-    public Ore(ResourceLocation name)
+
+    public final boolean graded;
+    public final Metal metal;
+    public final String name;
+
+    public Ore(ResourceLocation name, @Nullable Metal metal)
     {
+        this.graded = (metal != null);
+        this.metal = metal;
+
+        this.name = name.getResourcePath();
         setRegistryName(name);
     }
 
-    // todo: add required fields (metal, graded, nuggets?)
+    public enum Grade implements IStringSerializable
+    {
+        NORMAL(25), POOR(15), RICH(35);
+
+        public static Grade byMetadata(int meta)
+        {
+            return Grade.values()[meta];
+        }
+
+        public final int smeltAmount;
+
+        Grade(int smeltAmount)
+        {
+            this.smeltAmount = smeltAmount;
+        }
+
+        @Override
+        public String getName()
+        {
+            return this.name().toLowerCase();
+        }
+
+        public int getMeta()
+        {
+            return this.ordinal();
+        }
+    }
 }
