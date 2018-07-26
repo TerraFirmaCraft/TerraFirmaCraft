2f73760f878c6bc03e61a8b2e9a6194c082109b2
alcatrazEscapee <alex@molleroneill.com>
2018-07-25 20:24:01 -0400

    Final tree: Kapok. Started on tree logging when using an axe. Still todo: fix up tree generation, sapling growth, better growth checks.

--- src/main/java/net/dries007/tfc/objects/blocks/wood/BlockLogTFC.java
+++ src/main/java/net/dries007/tfc/objects/blocks/wood/BlockLogTFC.java
@@ -5,15 +5,16 @@
 
 package net.dries007.tfc.objects.blocks.wood;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import net.minecraft.block.BlockLog;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
@@ -21,7 +22,11 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import mcp.MethodsReturnNonnullByDefault;
+import net.dries007.tfc.TerraFirmaCraft;
 import net.dries007.tfc.api.types.Tree;
+import net.dries007.tfc.objects.Metal;
+import net.dries007.tfc.objects.items.metal.ItemMetalTool;
+import net.dries007.tfc.objects.items.rock.ItemRockAxe;
 import net.dries007.tfc.util.OreDictionaryHelper;
 
 @MethodsReturnNonnullByDefault
@@ -47,9 +52,12 @@ public class BlockLogTFC extends BlockLog
         if (MAP.put(wood, this) != null) throw new IllegalStateException("There can only be one.");
         setDefaultState(blockState.getBaseState().withProperty(LOG_AXIS, BlockLog.EnumAxis.Y).withProperty(PLACED, true).withProperty(SMALL, false));
         setHarvestLevel("axe", 0);
+        setHardness(10.0F);
+        setResistance(5.0F);
         OreDictionaryHelper.register(this, "log", "wood");
         OreDictionaryHelper.register(this, "log", "wood", wood);
         Blocks.FIRE.setFireInfo(this, 5, 5);
+        setTickRandomly(true);
     }
 
     @SuppressWarnings("deprecation")
@@ -106,4 +114,83 @@ public class BlockLogTFC extends BlockLog
     {
         return super.getStateForPlacement(worldIn, pos, facing, hitX, hitY, hitZ, meta, placer).withProperty(PLACED, true).withProperty(SMALL, placer.isSneaking());
     }
+
+    @Override
+    public void onBlockHarvested(World worldIn, BlockPos pos, IBlockState state, EntityPlayer player)
+    {
+        removeTree(worldIn, pos, player, player.getHeldItemMainhand(), 1);
+        if (state.getValue(PLACED))
+            return;
+
+        // Check if player has a valid tool
+        ItemStack stack = player.getActiveItemStack();
+        int flags = 0;
+        if (stack.getItem() instanceof ItemMetalTool)
+        {
+            ItemMetalTool tool = (ItemMetalTool) stack.getItem();
+            if (tool.type == Metal.ItemType.AXE)
+            {
+                flags += 1;
+            }
+        }
+        else if (stack.getItem() instanceof ItemRockAxe)
+        {
+            flags += 3;
+        }
+        if ((flags & 0b1) == 0b1) // bit 1 = is axe, bit 2 = is stone
+        {
+            // cut down the tree
+            removeTree(worldIn, pos, player, stack, flags);
+        }
+    }
+
+    private void removeTree(World world, BlockPos pos, EntityPlayer player, ItemStack stack, int flags)
+    {
+        int maxLogs = stack.getMaxDamage() - stack.getItemDamage();
+
+        // find all logs and add them to a list
+        List<BlockPos> logs = new ArrayList<>();
+        BlockPos pos1, pos2;
+        logs.add(pos);
+        for (int i = 0; i < logs.size(); i++)
+        {
+            pos1 = logs.get(i);
+            // check for nearby logs
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    for (int z = -1; z <= 1; z++)
+                    {
+                        pos2 = pos1.add(x, y, z);
+                        if (world.getBlockState(pos2).getBlock() == this && !logs.contains(pos2))
+                            logs.add(pos2);
+                    }
+                }
+            }
+        }
+        // sort the list in terms of max distance to the original tree
+        logs.sort(Comparator.comparing(x -> x.distanceSq(pos)));
+        // start removing logs*/
+        logs.forEach(x -> TerraFirmaCraft.getLog().info("Trying to cut log + " + x));
+        for (int i = 0; i < Math.min(logs.size(), maxLogs); i++)
+        {
+            // Remove the top log of the list
+            pos1 = logs.get(i);
+            harvestBlock(world, player, pos1, world.getBlockState(pos1), null, stack);
+            stack.damageItem(1, player);
+            world.setBlockToAir(pos1);
+        }
+    }
+
+    /*@Override
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
+    {
+        for(int x = -1; x <= 1; x++)
+            for(int y = -1; y <= 1; y++)
+                for(int z = -1; z <= 1; z++)
+                    if(world.getBlockState(pos.add(x, y, z)).getBlock() == this && (z != 0 || y != 0 || x != 0))
+                        world.setBlockToAir(pos)
+    }*/
+
 }
--- src/main/java/net/dries007/tfc/world/classic/worldgen/trees/TreeGenKapok.java
+++ src/main/java/net/dries007/tfc/world/classic/worldgen/trees/TreeGenKapok.java
@@ -8,18 +8,85 @@ package net.dries007.tfc.world.classic.worldgen.trees;
 
 import java.util.Random;
 
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.structure.template.PlacementSettings;
+import net.minecraft.world.gen.structure.template.Template;
 import net.minecraft.world.gen.structure.template.TemplateManager;
 
+import net.dries007.tfc.Constants;
+import net.dries007.tfc.TerraFirmaCraft;
 import net.dries007.tfc.api.ITreeGenerator;
 import net.dries007.tfc.api.types.Tree;
+import net.dries007.tfc.objects.blocks.wood.BlockLeavesTFC;
+import net.dries007.tfc.objects.blocks.wood.BlockLogTFC;
+
+import static net.dries007.tfc.objects.blocks.wood.BlockLogTFC.PLACED;
 
 public class TreeGenKapok implements ITreeGenerator
 {
+    private IBlockState trunk;
+    private PlacementSettings settings;
+
     @Override
     public void generateTree(TemplateManager manager, World world, BlockPos pos, Tree tree, Random rand)
     {
+        if (!canGenerateTree(world, pos, tree))
+            return;
         // todo
+
+        trunk = BlockLogTFC.get(tree).getDefaultState().withProperty(PLACED, false);
+        settings = ITreeGenerator.getDefaultSettings();
+
+        int height = 12 + rand.nextInt(8);
+        int branches = 2 + rand.nextInt(3);
+
+        int x1, y1, z1, type;
+        for (int i = 0; i < branches; i++)
+        {
+            y1 = 6 + rand.nextInt(height - 8);
+            x1 = rand.nextInt(7) - 3;
+            z1 = rand.nextInt(7) - 3;
+            type = 1 + rand.nextInt(3);
+            placeBranch(manager, world, pos.add(x1, y1, z1), tree.name + "/branch" + type);
+            checkAndPlace(world, pos.add(x1 * 0.5, y1 - 1, z1 * 0.5));
+        }
+
+        for (int i = 0; i < height; i++)
+            placeTrunk(world, pos.add(0, i, 0));
+        placeBranch(manager, world, pos.add(0, height, 0), tree.name + "/top");
+    }
+
+    private void placeBranch(TemplateManager manager, World world, BlockPos pos, String name)
+    {
+        ResourceLocation base = new ResourceLocation(Constants.MOD_ID, name);
+        Template structureBase = manager.get(world.getMinecraftServer(), base);
+
+        if (structureBase == null)
+        {
+            TerraFirmaCraft.getLog().warn("Unable to find a template for " + base.toString());
+            return;
+        }
+        BlockPos size = structureBase.getSize();
+        pos = pos.add(-size.getX() / 2, 0, -size.getZ() / 2);
+
+        structureBase.addBlocksToWorld(world, pos, settings);
+    }
+
+    private void placeTrunk(World world, BlockPos pos)
+    {
+        checkAndPlace(world, pos);
+        checkAndPlace(world, pos.add(-1, 0, 0));
+        checkAndPlace(world, pos.add(0, 0, -1));
+        checkAndPlace(world, pos.add(-1, 0, -1));
+    }
+
+    private void checkAndPlace(World world, BlockPos pos)
+    {
+        if (world.getBlockState(pos).getBlock() == Blocks.AIR || world.getBlockState(pos).getBlock() instanceof BlockLeavesTFC)
+            world.setBlockState(pos, trunk);
     }
 }
