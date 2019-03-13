4761999c10e7c1b30541236c0f9a1b93ae6d734b
domi1819 <domi1819@dev.null>
2019-03-14 00:16:39 +0100

    Made Anvil items render in 3D

--- generateResources.py
+++ generateResources.py
@@ -839,7 +839,10 @@ for item_type, tool_item in METAL_ITEMS.items():
         parent = 'item/handheld' if item_type in TOOLS else 'item/generated'
         if item_type in ['knife', 'javelin']:
             parent = 'tfc:item/handheld_flipped'
-        item(('metal', item_type, metal), 'tfc:items/metal/%s/%s' % (item_type.replace('unfinished_', ''), metal), parent=parent)
+        if item_type == 'anvil':
+            model(('item', 'metal', 'anvil', metal), 'tfc:item/metal/anvil/transformations', {'all': 'tfc:blocks/metal/%s' % metal})
+        else:
+            item(('metal', item_type, metal), 'tfc:items/metal/%s/%s' % (item_type.replace('unfinished_', ''), metal), parent=parent)
 for metal in STEEL:
     for type in ['high_carbon', 'weak']:
         item(('metal', 'ingot', type + '_' + metal), 'tfc:items/metal/ingot/%s' % metal)
