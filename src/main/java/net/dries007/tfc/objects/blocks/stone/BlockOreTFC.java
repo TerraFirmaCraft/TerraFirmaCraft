894fa5a6abeaac81d869e05e441531d4e14d8cbb
AlcatrazEscapee <alex@molleroneill.com>
2019-05-04 20:31:11 -0400

    (closes #58) Pick block for ore blocks always returns the normal variant

--- src/main/java/net/dries007/tfc/objects/blocks/stone/BlockOreTFC.java
+++ src/main/java/net/dries007/tfc/objects/blocks/stone/BlockOreTFC.java
@@ -9,23 +9,28 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import mcp.MethodsReturnNonnullByDefault;
 import net.dries007.tfc.api.types.Ore;
 import net.dries007.tfc.api.types.Rock;
 import net.dries007.tfc.objects.items.metal.ItemOreTFC;
 
-@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
 public class BlockOreTFC extends Block
 {
     public static final PropertyEnum<Ore.Grade> GRADE = PropertyEnum.create("grade", Ore.Grade.class);
@@ -64,6 +69,7 @@ public class BlockOreTFC extends Block
 
     @SuppressWarnings("deprecation")
     @Override
+    @Nonnull
     public IBlockState getStateFromMeta(int meta)
     {
         return getDefaultState().withProperty(GRADE, Ore.Grade.byMetadata(meta));
@@ -76,6 +82,7 @@ public class BlockOreTFC extends Block
     }
 
     @Override
+    @Nonnull
     public Item getItemDropped(IBlockState state, Random rand, int fortune)
     {
         // todo: handle coal
@@ -91,6 +98,7 @@ public class BlockOreTFC extends Block
     }
 
     @Override
+    @Nonnull
     @SideOnly(Side.CLIENT)
     public BlockRenderLayer getRenderLayer()
     {
@@ -98,14 +106,22 @@ public class BlockOreTFC extends Block
     }
 
     @Override
+    @Nonnull
     protected BlockStateContainer createBlockState()
     {
         return new BlockStateContainer(this, GRADE);
     }
 
     @Override
-    public int quantityDropped(IBlockState state, int fortune, @Nonnull Random random)
+    public int quantityDropped(IBlockState state, int fortune, Random random)
     {
         return super.quantityDropped(state, fortune, random); // todo: see how 1710 handles this
+    }
+
+    @Override
+    @Nonnull
+    public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player)
+    {
+        return new ItemStack(state.getBlock());
     }
 }
