6203e387adb1dd2bba22a7086e51fef1305e4e3d
AlcatrazEscapee <alex@molleroneill.com>
2019-07-10 18:16:52 -0400

    Removed hidden registry name setting on quern recipe. Changed quern recipe to not conflict with decorative blocks. Fixed barrel recipes

--- src/main/java/net/dries007/tfc/objects/recipes/ShapelessDamageRecipe.java
+++ src/main/java/net/dries007/tfc/objects/recipes/ShapelessDamageRecipe.java
@@ -21,8 +21,10 @@ import net.minecraftforge.common.crafting.IRecipeFactory;
 import net.minecraftforge.common.crafting.JsonContext;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 
-//Much of this is borrowed from https://github.com/Choonster-Minecraft-Mods/TestMod3/blob/d064915183a4a3b803d779576f982279268b1ca3/src/main/java/choonster/testmod3/crafting/recipe/ShapelessCuttingRecipe.java
-
+/**
+ * Much of this is borrowed from https://github.com/Choonster-Minecraft-Mods/TestMod3/blob/d064915183a4a3b803d779576f982279268b1ca3/src/main/java/choonster/testmod3/crafting/recipe/ShapelessCuttingRecipe.java
+ */
+@SuppressWarnings("unused")
 public class ShapelessDamageRecipe extends ShapelessOreRecipe
 {
     public ShapelessDamageRecipe(ResourceLocation group, NonNullList<Ingredient> input, @Nonnull ItemStack result)
