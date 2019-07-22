1b52e285543e9f12125cbe1a9733467ee27c55df
DisasterMoo <luan.santos.pinto@gmail.com>
2019-07-22 10:35:26 -0300

    Fix for TerraFirmaCraft#97

--- src/main/java/net/dries007/tfc/world/classic/worldgen/WorldGenLooseRocks.java
+++ src/main/java/net/dries007/tfc/world/classic/worldgen/WorldGenLooseRocks.java
@@ -10,7 +10,6 @@ import java.util.Random;
 import javax.annotation.Nullable;
 
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
@@ -74,7 +73,7 @@ public class WorldGenLooseRocks implements IWorldGenerator
     {
         // Use air, so it doesn't replace other replaceable world gen
         // This matches the check in BlockPlacedItemFlat for if the block can stay
-        if (world.isAirBlock(pos) && world.getBlockState(pos.down()).isSideSolid(world, pos.down(), EnumFacing.UP))
+        if (world.isAirBlock(pos) && world.getBlockState(pos.down()).isFullBlock())
         {
             world.setBlockState(pos, BlocksTFC.PLACED_ITEM_FLAT.getDefaultState(), 2);
             TEPlacedItemFlat tile = Helpers.getTE(world, pos, TEPlacedItemFlat.class);
