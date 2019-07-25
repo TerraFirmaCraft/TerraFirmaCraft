08978048ecb9c5b162c358a35b1ea618e46778aa
DisasterMoo <luan.santos.pinto@gmail.com>
2019-07-25 10:22:45 -0300

    Added Heating recipe category to JEI

--- src/main/java/net/dries007/tfc/objects/recipes/heat/HeatRecipeManager.java
+++ src/main/java/net/dries007/tfc/objects/recipes/heat/HeatRecipeManager.java
@@ -20,45 +20,43 @@ import net.dries007.tfc.util.agriculture.Food;
 
 public final class HeatRecipeManager
 {
-    private static final List<HeatRecipe> recipes = new ArrayList<>();
+    public static final List<HeatRecipe> RECIPES = new ArrayList<>();
 
     public static void postInit()
     {
-        recipes.add(new HeatRecipe(new ItemStack(BlocksTFC.TORCH, 2), "stickWood"));
-        recipes.add(new HeatRecipe(new ItemStack(Blocks.GLASS), "sand"));
+        RECIPES.add(new HeatRecipe(new ItemStack(BlocksTFC.TORCH, 2), "stickWood"));
+        RECIPES.add(new HeatRecipe(new ItemStack(Blocks.GLASS), "sand"));
 
         //Bread
-        recipes.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.BARLEY_BREAD)), new ItemStack(ItemFoodTFC.get(Food.BARLEY_DOUGH))));
-        recipes.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.CORNBREAD)), new ItemStack(ItemFoodTFC.get(Food.CORNMEAL_DOUGH))));
-        recipes.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.OAT_BREAD)), new ItemStack(ItemFoodTFC.get(Food.OAT_DOUGH))));
-        recipes.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.RICE_BREAD)), new ItemStack(ItemFoodTFC.get(Food.RICE_DOUGH))));
-        recipes.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.RYE_BREAD)), new ItemStack(ItemFoodTFC.get(Food.RYE_DOUGH))));
-        recipes.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.WHEAT_BREAD)), new ItemStack(ItemFoodTFC.get(Food.WHEAT_DOUGH))));
+        RECIPES.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.BARLEY_BREAD)), new ItemStack(ItemFoodTFC.get(Food.BARLEY_DOUGH))));
+        RECIPES.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.CORNBREAD)), new ItemStack(ItemFoodTFC.get(Food.CORNMEAL_DOUGH))));
+        RECIPES.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.OAT_BREAD)), new ItemStack(ItemFoodTFC.get(Food.OAT_DOUGH))));
+        RECIPES.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.RICE_BREAD)), new ItemStack(ItemFoodTFC.get(Food.RICE_DOUGH))));
+        RECIPES.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.RYE_BREAD)), new ItemStack(ItemFoodTFC.get(Food.RYE_DOUGH))));
+        RECIPES.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.WHEAT_BREAD)), new ItemStack(ItemFoodTFC.get(Food.WHEAT_DOUGH))));
 
         //Meat
-        recipes.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_BEEF)), new ItemStack(ItemFoodTFC.get(Food.BEEF))));
-        recipes.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_PORK)), new ItemStack(ItemFoodTFC.get(Food.PORK))));
-        recipes.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_CHICKEN)), new ItemStack(ItemFoodTFC.get(Food.CHICKEN))));
-        recipes.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_MUTTON)), new ItemStack(ItemFoodTFC.get(Food.MUTTON))));
-        recipes.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_FISH)), new ItemStack(ItemFoodTFC.get(Food.FISH))));
-        recipes.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_BEAR)), new ItemStack(ItemFoodTFC.get(Food.BEAR))));
-        recipes.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_CALAMARI)), new ItemStack(ItemFoodTFC.get(Food.CALAMARI))));
-        recipes.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_HORSE_MEAT)), new ItemStack(ItemFoodTFC.get(Food.HORSE_MEAT))));
-        recipes.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_PHEASANT)), new ItemStack(ItemFoodTFC.get(Food.PHEASANT))));
-        recipes.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_VENISON)), new ItemStack(ItemFoodTFC.get(Food.VENISON))));
-
-        // todo: craft tweaker supported recipes (See NTP for a nice-ish way to do it)
+        RECIPES.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_BEEF)), new ItemStack(ItemFoodTFC.get(Food.BEEF))));
+        RECIPES.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_PORK)), new ItemStack(ItemFoodTFC.get(Food.PORK))));
+        RECIPES.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_CHICKEN)), new ItemStack(ItemFoodTFC.get(Food.CHICKEN))));
+        RECIPES.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_MUTTON)), new ItemStack(ItemFoodTFC.get(Food.MUTTON))));
+        RECIPES.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_FISH)), new ItemStack(ItemFoodTFC.get(Food.FISH))));
+        RECIPES.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_BEAR)), new ItemStack(ItemFoodTFC.get(Food.BEAR))));
+        RECIPES.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_CALAMARI)), new ItemStack(ItemFoodTFC.get(Food.CALAMARI))));
+        RECIPES.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_HORSE_MEAT)), new ItemStack(ItemFoodTFC.get(Food.HORSE_MEAT))));
+        RECIPES.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_PHEASANT)), new ItemStack(ItemFoodTFC.get(Food.PHEASANT))));
+        RECIPES.add(new HeatRecipe(new ItemStack(ItemFoodTFC.get(Food.COOKED_VENISON)), new ItemStack(ItemFoodTFC.get(Food.VENISON))));
     }
 
     public static void add(HeatRecipe recipe)
     {
-        recipes.add(recipe);
+        RECIPES.add(recipe);
     }
 
     @Nullable
     public static HeatRecipe get(ItemStack stack)
     {
-        Optional<HeatRecipe> recipe = recipes.stream().filter(x -> x.matchesInput(stack)).findFirst();
+        Optional<HeatRecipe> recipe = RECIPES.stream().filter(x -> x.matchesInput(stack)).findFirst();
         if (recipe.isPresent())
         {
             return recipe.get();
