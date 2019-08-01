8fdebbccd4a619b6ad2fb482b9384b309881c9dc
DisasterMoo <luan.santos.pinto@gmail.com>
2019-08-01 15:37:51 -0300

    Added metal armor items.

--- src/main/java/net/dries007/tfc/types/DefaultMetals.java
+++ src/main/java/net/dries007/tfc/types/DefaultMetals.java
@@ -14,6 +14,7 @@ import net.dries007.tfc.api.recipes.AlloyRecipe;
 import net.dries007.tfc.api.registries.TFCRegistryEvent;
 import net.dries007.tfc.api.types.Metal;
 import net.dries007.tfc.api.types.Ore;
+import net.dries007.tfc.objects.ArmorMaterialTFC;
 import net.dries007.tfc.objects.ToolMaterialsTFC;
 
 import static net.dries007.tfc.api.types.Metal.Tier.*;
@@ -141,35 +142,35 @@ public final class DefaultMetals
     public static void onPreRegisterMetal(TFCRegistryEvent.RegisterPreBlock<Metal> event)
     {
         event.getRegistry().registerAll(
-            new Metal(BISMUTH, TIER_I, true, 0.14f, 270, 0xFF486B72, null),
-            new Metal(BISMUTH_BRONZE, TIER_II, true, 0.35f, 985, 0xFF418E4F, ToolMaterialsTFC.BISMUTH_BRONZE),
-            new Metal(BLACK_BRONZE, TIER_II, true, 0.35f, 1070, 0xFF3B2636, ToolMaterialsTFC.BLACK_BRONZE),
-            new Metal(BRASS, TIER_I, true, 0.35f, 930, 0xFF96892E, null),
-            new Metal(BRONZE, TIER_II, true, 0.35f, 950, 0xFF7C5E33, ToolMaterialsTFC.BRONZE),
-            new Metal(COPPER, TIER_I, true, 0.35f, 1080, 0xFFB64027, ToolMaterialsTFC.COPPER),
-            new Metal(GOLD, TIER_I, true, 0.6f, 1060, 0xFFDCBF1B, null),
-            new Metal(LEAD, TIER_I, true, 0.22f, 328, 0xFF40494D, null),
-            new Metal(NICKEL, TIER_I, true, 0.48f, 1453, 0xFF4E4E3C, null),
-            new Metal(ROSE_GOLD, TIER_I, true, 0.35f, 960, 0xFFEB7137, null),
-            new Metal(SILVER, TIER_I, true, 0.48f, 961, 0xFF949495, null),
-            new Metal(TIN, TIER_I, true, 0.14f, 230, 0xFF90A4BB, null),
-            new Metal(ZINC, TIER_I, true, 0.21f, 420, 0xFFBBB9C4, null),
-            new Metal(STERLING_SILVER, TIER_I, true, 0.35f, 900, 0xFFAC927B, null),
-            new Metal(WROUGHT_IRON, TIER_III, true, 0.35f, 1535, 0xFF989897, ToolMaterialsTFC.IRON),
-            new Metal(PIG_IRON, TIER_III, true, 0.35f, 1535, 0xFF6A595C, null),
-            new Metal(STEEL, TIER_IV, true, 0.35f, 1540, 0xFF5F5F5F, ToolMaterialsTFC.STEEL),
-            new Metal(PLATINUM, TIER_V, true, 0.35f, 1730, 0xFF9DADC0, null),
-            new Metal(BLACK_STEEL, TIER_V, true, 0.35f, 1485, 0xFF111111, ToolMaterialsTFC.BLACK_STEEL),
-            new Metal(BLUE_STEEL, TIER_VI, true, 0.35f, 1540, 0xFF2D5596, ToolMaterialsTFC.BLUE_STEEL),
-            new Metal(RED_STEEL, TIER_VI, true, 0.35f, 1540, 0xFF700503, ToolMaterialsTFC.RED_STEEL),
-            new Metal(WEAK_STEEL, TIER_V, false, 0.35f, 1540, 0xFF111111, null),
-            new Metal(WEAK_BLUE_STEEL, TIER_V, false, 0.35f, 1540, 0xFF2D5596, null),
-            new Metal(WEAK_RED_STEEL, TIER_V, false, 0.35f, 1540, 0xFF700503, null),
-            new Metal(HIGH_CARBON_STEEL, TIER_IV, false, 0.35f, 1540, 0xFF5F5F5F, null),
-            new Metal(HIGH_CARBON_BLUE_STEEL, TIER_V, false, 0.35f, 1540, 0xFF2D5596, null),
-            new Metal(HIGH_CARBON_RED_STEEL, TIER_V, false, 0.35f, 1540, 0xFF700503, null),
-            new Metal(HIGH_CARBON_BLACK_STEEL, TIER_V, false, 0.35f, 1540, 0xFF111111, null),
-            new Metal(UNKNOWN, TIER_I, false, 0.5f, 1250, 0xFF2F2B27, null)
+            new Metal(BISMUTH, TIER_I, true, 0.14f, 270, 0xFF486B72, null, null),
+            new Metal(BISMUTH_BRONZE, TIER_II, true, 0.35f, 985, 0xFF418E4F, ToolMaterialsTFC.BISMUTH_BRONZE, ArmorMaterialTFC.BISMUTH_BRONZE),
+            new Metal(BLACK_BRONZE, TIER_II, true, 0.35f, 1070, 0xFF3B2636, ToolMaterialsTFC.BLACK_BRONZE, ArmorMaterialTFC.BLACK_BRONZE),
+            new Metal(BRASS, TIER_I, true, 0.35f, 930, 0xFF96892E, null, null),
+            new Metal(BRONZE, TIER_II, true, 0.35f, 950, 0xFF7C5E33, ToolMaterialsTFC.BRONZE, ArmorMaterialTFC.BRONZE),
+            new Metal(COPPER, TIER_I, true, 0.35f, 1080, 0xFFB64027, ToolMaterialsTFC.COPPER, ArmorMaterialTFC.COPPER),
+            new Metal(GOLD, TIER_I, true, 0.6f, 1060, 0xFFDCBF1B, null, null),
+            new Metal(LEAD, TIER_I, true, 0.22f, 328, 0xFF40494D, null, null),
+            new Metal(NICKEL, TIER_I, true, 0.48f, 1453, 0xFF4E4E3C, null, null),
+            new Metal(ROSE_GOLD, TIER_I, true, 0.35f, 960, 0xFFEB7137, null, null),
+            new Metal(SILVER, TIER_I, true, 0.48f, 961, 0xFF949495, null, null),
+            new Metal(TIN, TIER_I, true, 0.14f, 230, 0xFF90A4BB, null, null),
+            new Metal(ZINC, TIER_I, true, 0.21f, 420, 0xFFBBB9C4, null, null),
+            new Metal(STERLING_SILVER, TIER_I, true, 0.35f, 900, 0xFFAC927B, null, null),
+            new Metal(WROUGHT_IRON, TIER_III, true, 0.35f, 1535, 0xFF989897, ToolMaterialsTFC.IRON, ArmorMaterialTFC.WROUGHT_IRON),
+            new Metal(PIG_IRON, TIER_III, true, 0.35f, 1535, 0xFF6A595C, null, null),
+            new Metal(STEEL, TIER_IV, true, 0.35f, 1540, 0xFF5F5F5F, ToolMaterialsTFC.STEEL, ArmorMaterialTFC.STEEL),
+            new Metal(PLATINUM, TIER_V, true, 0.35f, 1730, 0xFF9DADC0, null, null),
+            new Metal(BLACK_STEEL, TIER_V, true, 0.35f, 1485, 0xFF111111, ToolMaterialsTFC.BLACK_STEEL, ArmorMaterialTFC.BLACK_STEEL),
+            new Metal(BLUE_STEEL, TIER_VI, true, 0.35f, 1540, 0xFF2D5596, ToolMaterialsTFC.BLUE_STEEL, ArmorMaterialTFC.BLUE_STEEL),
+            new Metal(RED_STEEL, TIER_VI, true, 0.35f, 1540, 0xFF700503, ToolMaterialsTFC.RED_STEEL, ArmorMaterialTFC.RED_STEEL),
+            new Metal(WEAK_STEEL, TIER_V, false, 0.35f, 1540, 0xFF111111, null, null),
+            new Metal(WEAK_BLUE_STEEL, TIER_V, false, 0.35f, 1540, 0xFF2D5596, null, null),
+            new Metal(WEAK_RED_STEEL, TIER_V, false, 0.35f, 1540, 0xFF700503, null, null),
+            new Metal(HIGH_CARBON_STEEL, TIER_IV, false, 0.35f, 1540, 0xFF5F5F5F, null, null),
+            new Metal(HIGH_CARBON_BLUE_STEEL, TIER_V, false, 0.35f, 1540, 0xFF2D5596, null, null),
+            new Metal(HIGH_CARBON_RED_STEEL, TIER_V, false, 0.35f, 1540, 0xFF700503, null, null),
+            new Metal(HIGH_CARBON_BLACK_STEEL, TIER_V, false, 0.35f, 1540, 0xFF111111, null, null),
+            new Metal(UNKNOWN, TIER_I, false, 0.5f, 1250, 0xFF2F2B27, null, null)
         );
     }
 
