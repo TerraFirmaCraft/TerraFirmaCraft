46e9c5b7a8f989e1ee22817a053da02dd5b5babd
DisasterMoo <luan.santos.pinto@gmail.com>
2019-08-06 22:26:48 -0300

    Anvil and welding recipes now uses oredict to show hammer + flux. Fire clay knapping uses the correct disabled texture.

--- src/main/java/net/dries007/tfc/compat/jei/categories/AnvilCategory.java
+++ src/main/java/net/dries007/tfc/compat/jei/categories/AnvilCategory.java
@@ -2,7 +2,6 @@ package net.dries007.tfc.compat.jei.categories;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
 import mezz.jei.api.IGuiHelper;
@@ -10,11 +9,9 @@ import mezz.jei.api.gui.IGuiItemStackGroup;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.ingredients.VanillaTypes;
-import net.dries007.tfc.api.types.Metal;
 import net.dries007.tfc.api.util.TFCConstants;
 import net.dries007.tfc.compat.jei.BaseRecipeCategory;
 import net.dries007.tfc.compat.jei.wrappers.SimpleRecipeWrapper;
-import net.dries007.tfc.objects.items.metal.ItemMetal;
 
 @ParametersAreNonnullByDefault
 public class AnvilCategory extends BaseRecipeCategory<SimpleRecipeWrapper>
@@ -35,7 +32,7 @@ public class AnvilCategory extends BaseRecipeCategory<SimpleRecipeWrapper>
         itemStackGroup.init(2, false, 19, 42);
 
         itemStackGroup.set(0, ingredients.getInputs(VanillaTypes.ITEM).get(0));
-        itemStackGroup.set(1, new ItemStack(ItemMetal.get(Metal.WROUGHT_IRON, Metal.ItemType.HAMMER)));
+        itemStackGroup.set(1, ingredients.getInputs(VanillaTypes.ITEM).get(1));
         itemStackGroup.set(2, ingredients.getOutputs(VanillaTypes.ITEM).get(0));
     }
 }
