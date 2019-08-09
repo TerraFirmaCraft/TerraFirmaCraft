2ddb65f8f4b9bcfe789517f7e0c83d1c714267eb
DisasterMoo <luan.santos.pinto@gmail.com>
2019-08-09 10:50:31 -0300

    Fixed TerraFirmaCraft#270, TerraFirmaCraft#278, TerraFirmaCraft#279

--- src/main/java/net/dries007/tfc/objects/items/ceramics/ItemJug.java
+++ src/main/java/net/dries007/tfc/objects/items/ceramics/ItemJug.java
@@ -22,6 +22,7 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.*;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.fluids.Fluid;
@@ -179,7 +180,8 @@ public class ItemJug extends ItemPottery
             FluidStack fluidStack = bucketCap.drain(CAPACITY, false);
             if (fluidStack != null)
             {
-                return fluidStack.getLocalizedName() + " " + super.getItemStackDisplayName(stack);
+                String fluidname = fluidStack.getLocalizedName();
+                return new TextComponentTranslation("item.tfc.ceramics.fired.jug.filled", fluidname).getFormattedText();
             }
         }
         return super.getItemStackDisplayName(stack);
