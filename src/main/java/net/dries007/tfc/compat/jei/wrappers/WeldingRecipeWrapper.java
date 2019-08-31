ccc7cc6fc4c2aeb0a6b3cc8418adc7728cb1f9e6
DisasterMoo <luan.santos.pinto@gmail.com>
2019-08-31 14:13:33 -0300

    Renamed Wrappers to match properly. Added tier requirement info in anvil and welding recipes (closes TerraFirmaCraft#358).

---
+++ src/main/java/net/dries007/tfc/compat/jei/wrappers/WeldingRecipeWrapper.java
@@ -0,0 +1,32 @@
+/*
+ * Work under Copyright. Licensed under the EUPL.
+ * See the project README.md and LICENSE.txt for more information.
+ */
+
+package net.dries007.tfc.compat.jei.wrappers;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.I18n;
+
+import net.dries007.tfc.api.recipes.WeldingRecipe;
+
+public class WeldingRecipeWrapper extends SimpleRecipeWrapper
+{
+    private WeldingRecipe recipe;
+
+    public WeldingRecipeWrapper(WeldingRecipe recipeWrapper)
+    {
+        super(recipeWrapper);
+        recipe = recipeWrapper;
+    }
+
+    @Override
+    public void drawInfo(Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY)
+    {
+        //Draw tier requirement info
+        String text = I18n.format("tfc.enum.tier." + recipe.getTier().name().toLowerCase());
+        float xPos = 88f - minecraft.fontRenderer.getStringWidth(text) / 2.0f;
+        float yPos = 6f;
+        minecraft.fontRenderer.drawString(text, xPos, yPos, 0x000000, false);
+    }
+}
