07169d6ccd8442f1cd2a13b11495699b248abe88
AlcatrazEscapee <alex@molleroneill.com>
2019-09-14 19:57:42 -0400

    Added missing size + weight to sticks

--- src/main/java/net/dries007/tfc/api/capability/ItemStickCapability.java
+++ src/main/java/net/dries007/tfc/api/capability/ItemStickCapability.java
@@ -17,10 +17,13 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import net.dries007.tfc.api.capability.heat.ItemHeatHandler;
+import net.dries007.tfc.api.capability.size.IItemSize;
+import net.dries007.tfc.api.capability.size.Size;
+import net.dries007.tfc.api.capability.size.Weight;
 
 import static net.dries007.tfc.api.util.TFCConstants.MOD_ID;
 
-public class ItemStickCapability extends ItemHeatHandler
+public class ItemStickCapability extends ItemHeatHandler implements IItemSize
 {
     public static final ResourceLocation KEY = new ResourceLocation(MOD_ID, "stick");
     private static final float MELTING_POINT = 40f;
@@ -37,8 +40,26 @@ public class ItemStickCapability extends ItemHeatHandler
     {
         float temperature = getTemperature();
         if (temperature > getMeltTemp() * 0.9f)
+        {
             text.add(I18n.format("tfc.enum.heat.torch.lit"));
+        }
         else if (temperature > 1f)
+        {
             text.add(I18n.format("tfc.enum.heat.torch.catching_fire"));
+        }
+    }
+
+    @Nonnull
+    @Override
+    public Size getSize(@Nonnull ItemStack stack)
+    {
+        return Size.SMALL;
+    }
+
+    @Nonnull
+    @Override
+    public Weight getWeight(@Nonnull ItemStack stack)
+    {
+        return Weight.LIGHT;
     }
 }
