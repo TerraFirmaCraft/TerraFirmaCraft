6277b69524a7a299db9b92ca84e493fa958dae82
DisasterMoo <luan.santos.pinto@gmail.com>
2020-01-22 13:33:02 -0300

    Fixed TerraFirmaCraft#675.

--- src/main/java/net/dries007/tfc/objects/blocks/wood/BlockBookshelfTFC.java
+++ src/main/java/net/dries007/tfc/objects/blocks/wood/BlockBookshelfTFC.java
@@ -14,6 +14,8 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -53,4 +55,9 @@ public class BlockBookshelfTFC extends Block
         return BlockRenderLayer.CUTOUT_MIPPED;
     }
 
+    @Override
+    public float getEnchantPowerBonus(World world, BlockPos pos)
+    {
+        return 1.0F; // Same as vanilla
+    }
 }
