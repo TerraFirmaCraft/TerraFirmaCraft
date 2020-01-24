db5b2d6a0249a48d7757ad187d4937f1a594cadc
DisasterMoo <luan.santos.pinto@gmail.com>
2020-01-23 23:29:48 -0300

    Rework on Bloomery: Now with a proper recipe system, adds the possibility to register blooms for other metals with different additives (where by default Wrought Iron consumes coal, lignite or charcoal). Also fixed the molten block being lit at all times (Closes TerraFirmaCraft#671).

--- src/main/java/net/dries007/tfc/api/registries/TFCRegistryNames.java
+++ src/main/java/net/dries007/tfc/api/registries/TFCRegistryNames.java
@@ -30,4 +30,5 @@ public final class TFCRegistryNames
     public static final ResourceLocation LOOM_RECIPE = new ResourceLocation(MOD_ID, "loom_recipe");
     public static final ResourceLocation QUERN_RECIPE = new ResourceLocation(MOD_ID, "quern_recipe");
     public static final ResourceLocation CHISEL_RECIPE = new ResourceLocation(MOD_ID, "chisel_recipe");
+    public static final ResourceLocation BLOOMERY_RECIPE = new ResourceLocation(MOD_ID, "bloomery_recipe");
 }
