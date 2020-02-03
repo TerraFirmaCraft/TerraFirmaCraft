a9678560a71df6fc08aeab5982c42c62bb09789d
DisasterMoo <luan.santos.pinto@gmail.com>
2020-02-03 20:32:22 -0300

    Metal Heating now shows ones recipe / item and shows how much you get smelting ingredients. Fixed unmold recipes not showing all possible outputs. Added casting category.

---
+++ src/main/java/net/dries007/tfc/compat/jei/wrappers/CastingRecipeWrapper.java
@@ -0,0 +1,43 @@
+/*
+ * Work under Copyright. Licensed under the EUPL.
+ * See the project README.md and LICENSE.txt for more information.
+ */
+
+package net.dries007.tfc.compat.jei.wrappers;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+
+import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.ingredients.VanillaTypes;
+import mezz.jei.api.recipe.IRecipeWrapper;
+import net.dries007.tfc.api.capability.IMoldHandler;
+import net.dries007.tfc.api.types.Metal;
+import net.dries007.tfc.objects.fluids.FluidsTFC;
+import net.dries007.tfc.objects.items.ceramics.ItemMold;
+
+public class CastingRecipeWrapper implements IRecipeWrapper
+{
+    private ItemStack mold;
+    private FluidStack input;
+
+    public CastingRecipeWrapper(Metal metal, Metal.ItemType type)
+    {
+        input = new FluidStack(FluidsTFC.getFluidFromMetal(metal), 100);
+        mold = new ItemStack(ItemMold.get(type));
+        IFluidHandler cap = mold.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, null);
+        if (cap instanceof IMoldHandler)
+        {
+            cap.fill(input, true);
+        }
+    }
+
+    @Override
+    public void getIngredients(IIngredients ingredients)
+    {
+        ingredients.setInput(VanillaTypes.FLUID, input);
+        ingredients.setOutput(VanillaTypes.ITEM, mold);
+    }
+}
