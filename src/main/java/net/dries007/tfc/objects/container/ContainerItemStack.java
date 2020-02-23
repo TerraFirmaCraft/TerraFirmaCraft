f1e874054fc32bfcf0ee9bd033fba9c28a447c74
AlcatrazEscapee <alex@molleroneill.com>
2020-02-23 11:53:48 -0500

    Clean up capability sync weirdness and half-sync solution. Should fix any remnamts of ghost items, or item heat desyncs.

--- src/main/java/net/dries007/tfc/objects/container/ContainerItemStack.java
+++ src/main/java/net/dries007/tfc/objects/container/ContainerItemStack.java
@@ -16,20 +16,17 @@ import net.minecraft.inventory.IContainerListener;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 
-import net.dries007.tfc.api.capability.heat.CapabilityItemHeat;
-import net.dries007.tfc.api.capability.heat.Heat;
-import net.dries007.tfc.api.capability.heat.IItemHeat;
-
 @ParametersAreNonnullByDefault
-public abstract class ContainerItemStack extends Container
+public abstract class ContainerItemStack extends Container implements ICapabilityUpdateContainer
 {
     protected final ItemStack stack;
     protected final EntityPlayer player;
     protected int itemIndex;
     protected int itemDragIndex;
     protected boolean isOffhand;
+    protected IContainerListener capabilityListener;
 
-    ContainerItemStack(InventoryPlayer playerInv, ItemStack stack)
+    protected ContainerItemStack(InventoryPlayer playerInv, ItemStack stack)
     {
         this.player = playerInv.player;
         this.stack = stack;
@@ -69,9 +66,7 @@ public abstract class ContainerItemStack extends Container
         itemstack = itemstack1.copy();
 
         // Begin custom transfer code here
-
         int containerSlots = inventorySlots.size() - player.inventory.mainInventory.size(); // number of slots in the container
-
         if (index < containerSlots)
         {
             // Transfer out of the container
@@ -80,7 +75,6 @@ public abstract class ContainerItemStack extends Container
                 // Don't transfer anything
                 return ItemStack.EMPTY;
             }
-            //tile.setAndUpdateSlots(index);
         }
         // Transfer into the container
         else
@@ -91,7 +85,6 @@ public abstract class ContainerItemStack extends Container
             }
         }
 
-        // Required
         if (itemstack1.getCount() == 0)
         {
             slot.putStack(ItemStack.EMPTY);
@@ -108,21 +101,38 @@ public abstract class ContainerItemStack extends Container
         return itemstack;
     }
 
+    /**
+     * This functionality duplicated from {@link ContainerTE#detectAndSendChanges()}
+     */
     @Override
-    @Nonnull
-    public ItemStack slotClick(int slotID, int dragType, ClickType clickType, EntityPlayer player)
+    public void detectAndSendChanges()
     {
-        if (slotID == itemIndex && (clickType == ClickType.QUICK_MOVE || clickType == ClickType.PICKUP || clickType == ClickType.THROW || clickType == ClickType.SWAP))
+        for (int i = 0; i < inventorySlots.size(); ++i)
         {
-            return ItemStack.EMPTY;
-        }
-        else if ((dragType == itemDragIndex) && clickType == ClickType.SWAP)
-        {
-            return ItemStack.EMPTY;
-        }
-        else
-        {
-            return super.slotClick(slotID, dragType, clickType, player);
+            ItemStack newStack = inventorySlots.get(i).getStack();
+            ItemStack cachedStack = inventoryItemStacks.get(i);
+
+            if (!ItemStack.areItemStacksEqual(cachedStack, newStack))
+            {
+                // Duplicated from Container#detectAndSendChanges
+                boolean clientStackChanged = !ItemStack.areItemStacksEqualUsingNBTShareTag(cachedStack, newStack);
+                cachedStack = newStack.isEmpty() ? ItemStack.EMPTY : newStack.copy();
+                this.inventoryItemStacks.set(i, cachedStack);
+
+                if (clientStackChanged)
+                {
+                    for (IContainerListener listener : this.listeners)
+                    {
+                        listener.sendSlotContents(this, i, cachedStack);
+                    }
+                }
+                else if (capabilityListener != null)
+                {
+                    // There's a capability difference ONLY that needs to be synced, so we use our own handler here, as to not conflict with vanilla's sync, because this won't overwrite the client side item stack
+                    // The listener will check if the item actually needs a sync based on capabilities we know we need to sync
+                    capabilityListener.sendSlotContents(this, i, cachedStack);
+                }
+            }
         }
     }
 
@@ -152,36 +162,27 @@ public abstract class ContainerItemStack extends Container
     }
 
     @Override
-    public void detectAndSendChanges()
+    @Nonnull
+    public ItemStack slotClick(int slotID, int dragType, ClickType clickType, EntityPlayer player)
     {
-        // Same as ContainerTE
-        for (int i = 0; i < inventorySlots.size(); ++i)
+        // Prevent moving of the item stack that is currently open
+        if (slotID == itemIndex && (clickType == ClickType.QUICK_MOVE || clickType == ClickType.PICKUP || clickType == ClickType.THROW || clickType == ClickType.SWAP))
         {
-            ItemStack stack = inventorySlots.get(i).getStack();
-            ItemStack newStack = inventoryItemStacks.get(i);
-
-            if (!ItemStack.areItemStacksEqual(newStack, stack))
-            {
-                // Since heat temperatures are updated every tick, it can cause network issues (server sending too many update packets = overriding slots, ghost items, etc)
-                // To alleviate that, we're gonna update the client on tooltip changes only
-                boolean updateClient = true;
-                IItemHeat cap1 = stack.getCapability(CapabilityItemHeat.ITEM_HEAT_CAPABILITY, null);
-                IItemHeat cap2 = newStack.getCapability(CapabilityItemHeat.ITEM_HEAT_CAPABILITY, null);
-                if (cap1 != null && cap2 != null && Heat.compareHeat(cap1.getTemperature(), cap2.getTemperature()))
-                {
-                    updateClient = false;
-                }
-                // May need to do the same for food decay?
-                newStack = stack.isEmpty() ? ItemStack.EMPTY : stack.copy();
-                inventoryItemStacks.set(i, newStack);
-                if (updateClient)
-                {
-                    for (IContainerListener listener : listeners)
-                    {
-                        listener.sendSlotContents(this, i, newStack);
-                    }
-                }
-            }
+            return ItemStack.EMPTY;
         }
+        else if ((dragType == itemDragIndex) && clickType == ClickType.SWAP)
+        {
+            return ItemStack.EMPTY;
+        }
+        else
+        {
+            return super.slotClick(slotID, dragType, clickType, player);
+        }
+    }
+
+    @Override
+    public void setCapabilityListener(IContainerListener capabilityListener)
+    {
+        this.capabilityListener = capabilityListener;
     }
 }
