08ed46bb942b5de8c614ddcce9c14b0e08b45ddf
DisasterMoo <luan.santos.pinto@gmail.com>
2020-03-05 11:14:04 -0300

    Added missing "/veininfo" command. Improved "/findveins" command, now it is possible to dump all generated veins in radius + dump rate to measure how much a vein type generated.

--- src/main/java/net/dries007/tfc/command/CommandFindVeins.java
+++ src/main/java/net/dries007/tfc/command/CommandFindVeins.java
@@ -5,6 +5,10 @@
 
 package net.dries007.tfc.command;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -21,6 +25,7 @@ import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 
+import net.dries007.tfc.TerraFirmaCraft;
 import net.dries007.tfc.world.classic.chunkdata.ChunkDataTFC;
 import net.dries007.tfc.world.classic.worldgen.vein.Vein;
 import net.dries007.tfc.world.classic.worldgen.vein.VeinRegistry;
@@ -29,7 +34,7 @@ import net.dries007.tfc.world.classic.worldgen.vein.VeinType;
 @ParametersAreNonnullByDefault
 public class CommandFindVeins extends CommandBase
 {
-    public static Collection<Vein> getGeneratedVeins(World world, int chunkX, int chunkZ, int radius)
+    public static Collection<Vein> getGeneratedVeins(World world, int chunkX, int chunkZ, int radius, boolean generated)
     {
         Set<Vein> veins = new HashSet<>();
         for (int x = chunkX - radius; x <= chunkX + radius; x++)
@@ -37,7 +42,7 @@ public class CommandFindVeins extends CommandBase
             for (int z = chunkZ - radius; z <= chunkZ + radius; z++)
             {
                 ChunkPos chunkPos = new ChunkPos(x, z);
-                if (world.isBlockLoaded(chunkPos.getBlock(8, 0, 8)))
+                if (world.isBlockLoaded(chunkPos.getBlock(8, 0, 8)) || (generated && world.isChunkGeneratedAt(x, z)))
                 {
                     Chunk chunk = world.getChunk(x, z);
                     ChunkDataTFC chunkData = ChunkDataTFC.get(chunk);
@@ -59,19 +64,33 @@ public class CommandFindVeins extends CommandBase
     @Nonnull
     public String getUsage(ICommandSender sender)
     {
-        return "/findveins [all|<vein name>] <radius> -> Finds all instances of a specific vein, or all veins within a certain chunk radius";
+        return "/findveins [all|<vein name>] <radius> [dump|rate] -> Finds all instances of a specific vein, or all veins within a certain chunk radius, if dump or rate, also save it to a log file";
     }
 
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
     {
-        if (args.length != 2) throw new WrongUsageException("2 arguments required.");
+        if (args.length != 2 && args.length != 3) throw new WrongUsageException("2 or 3 arguments required.");
         if (sender.getCommandSenderEntity() == null) throw new WrongUsageException("Can only be used by a player");
-
-        sender.sendMessage(new TextComponentString("Veins Found: "));
-
+        int dumprate = 0;
+        if (args.length == 3)
+        {
+            if (args[2].equalsIgnoreCase("dump"))
+            {
+                dumprate = 1;
+            }
+            else if (args[2].equalsIgnoreCase("rate"))
+            {
+                dumprate = 2;
+            }
+            else
+            {
+                throw new WrongUsageException("3rd argument must be dump or rate");
+            }
+        }
         final int radius = parseInt(args[1], 1, 1000);
-        final Collection<Vein> veins = getGeneratedVeins(sender.getEntityWorld(), sender.getCommandSenderEntity().chunkCoordX, sender.getCommandSenderEntity().chunkCoordZ, radius);
+        sender.sendMessage(new TextComponentString("Searching veins..."));
+        final Collection<Vein> veins = getGeneratedVeins(sender.getEntityWorld(), sender.getCommandSenderEntity().chunkCoordX, sender.getCommandSenderEntity().chunkCoordZ, radius, dumprate > 0);
         if (!args[0].equals("all"))
         {
             final VeinType type = VeinRegistry.INSTANCE.getVein(args[0]);
@@ -82,7 +101,73 @@ public class CommandFindVeins extends CommandBase
             // Search for veins matching type
             veins.removeIf(x -> x.getType() != type);
         }
+        sender.sendMessage(new TextComponentString("Veins Found: "));
         veins.forEach(x -> sender.sendMessage(new TextComponentString("> Vein: " + x.getType() + " at " + x.getPos())));
+        if (dumprate == 1)
+        {
+            try
+            {
+                // Save files to MC's root folder
+                String fileName = "tfc-veins-dump.log";
+                File dumpFile = new File(fileName);
+                BufferedWriter writer = new BufferedWriter(new FileWriter(dumpFile));
+                veins.forEach(x -> {
+                    try
+                    {
+                        String dump = "Vein: " + x.getType() + " at " + x.getPos();
+                        writer.write(dump);
+                        writer.newLine();
+                    }
+                    catch (IOException e)
+                    {
+                        TerraFirmaCraft.getLog().error(e.getMessage());
+                    }
+                });
+                writer.close();
+                sender.sendMessage(new TextComponentString("vein dump file saved at: " + dumpFile.getAbsolutePath()));
+            }
+            catch (IOException e)
+            {
+                TerraFirmaCraft.getLog().error(e.getMessage());
+            }
+        }
+        else if (dumprate == 2)
+        {
+            try
+            {
+                // Save files to MC's root folder
+                String fileName = "tfc-veins-rate.log";
+                File dumpFile = new File(fileName);
+                BufferedWriter writer = new BufferedWriter(new FileWriter(dumpFile));
+                Map<VeinType, Integer> veinRateMap = new HashMap<>();
+                veins.forEach(x -> {
+                    int count = 1;
+                    if (veinRateMap.containsKey(x.getType()))
+                    {
+                        count += veinRateMap.get(x.getType());
+                    }
+                    veinRateMap.put(x.getType(), count);
+                });
+                veinRateMap.forEach((veinType, count) -> {
+                    try
+                    {
+                        String dump = "VeinType: " + veinType.getRegistryName() + " count: " + count;
+                        writer.write(dump);
+                        writer.newLine();
+                    }
+                    catch (IOException e)
+                    {
+                        TerraFirmaCraft.getLog().error(e.getMessage());
+                    }
+                });
+                writer.close();
+                sender.sendMessage(new TextComponentString("vein rate file saved at: " + dumpFile.getAbsolutePath()));
+            }
+            catch (IOException e)
+            {
+                TerraFirmaCraft.getLog().error(e.getMessage());
+            }
+        }
     }
 
     @Override
@@ -98,6 +183,10 @@ public class CommandFindVeins extends CommandBase
         if (args.length == 1)
         {
             return getListOfStringsMatchingLastWord(args, VeinRegistry.INSTANCE.keySet());
+        }
+        else if (args.length == 3)
+        {
+            return getListOfStringsMatchingLastWord(args, "dump", "rate");
         }
         return Collections.emptyList();
     }
