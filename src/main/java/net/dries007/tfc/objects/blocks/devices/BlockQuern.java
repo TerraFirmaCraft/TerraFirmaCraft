bd0601cd7c07a6c08847d713892b41561ef3ab6c
DisasterMoo <luan.santos.pinto@gmail.com>
2020-03-23 21:59:06 -0300

    Added interface for customizable highlights in blocks. Should now be possible to draw custom selection boxes depending on where player is looking at: - Chisel is handled directly by the class's event handler - Quern now has it's selection boxes drawn depending on where player is looking Also, some tweaks on quern's interaction to match (everything is now in-world crafting, doesn't matter if player is sneaking or not when right clicking), and removed GUI since it's not needed.

--- src/main/java/net/dries007/tfc/objects/blocks/devices/BlockQuern.java
+++ src/main/java/net/dries007/tfc/objects/blocks/devices/BlockQuern.java
@@ -17,7 +17,6 @@ import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
@@ -25,6 +24,8 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.items.CapabilityItemHandler;
@@ -34,17 +35,21 @@ import net.minecraftforge.items.ItemHandlerHelper;
 import net.dries007.tfc.api.capability.size.IItemSize;
 import net.dries007.tfc.api.capability.size.Size;
 import net.dries007.tfc.api.capability.size.Weight;
-import net.dries007.tfc.client.TFCGuiHandler;
 import net.dries007.tfc.client.TFCSounds;
+import net.dries007.tfc.client.gui.overlay.IHighlightHandler;
 import net.dries007.tfc.objects.te.TEQuern;
 import net.dries007.tfc.util.Helpers;
 
 @ParametersAreNonnullByDefault
-public class BlockQuern extends Block implements IItemSize
+public class BlockQuern extends Block implements IItemSize, IHighlightHandler
 {
     private static final AxisAlignedBB BASE_AABB = new AxisAlignedBB(0D, 0D, 0D, 1D, 0.625D, 1D);
     private static final AxisAlignedBB QUERN_AABB = new AxisAlignedBB(0D, 0D, 0D, 1D, 0.875D, 1D);
-    private static final AxisAlignedBB HANDSTONE_AABB = new AxisAlignedBB(0.375D, 0.625D, 0.375D, 0.625D, 0.875D, 0.625D);
+
+    private static final AxisAlignedBB HANDSTONE_AABB = new AxisAlignedBB(0.1875D, 0.625D, 0.1875D, 0.8125D, 0.86D, 0.8125D);
+    private static final AxisAlignedBB HANDLE_AABB = new AxisAlignedBB(0.27125D, 0.86D, 0.27125D, 0.335D, 1.015D, 0.335D);
+
+    private static final AxisAlignedBB INPUT_SLOT_AABB = new AxisAlignedBB(0.375D, 0.86D, 0.375D, 0.625D, 1.015D, 0.625D);
 
     public BlockQuern()
     {
@@ -160,6 +165,40 @@ public class BlockQuern extends Block implements IItemSize
         super.breakBlock(world, pos, state);
     }
 
+    /**
+     * Gets the selection place player is looking at
+     * Used for interaction / selection box drawing
+     */
+    private static SelectionPlace getPlayerSelection(World world, BlockPos pos, EntityPlayer player)
+    {
+        // This will compute a line from the camera center (crosshair) starting at the player eye pos and a little after this block
+        // so we can grab the exact point regardless from which face player is looking from
+        double length = Math.sqrt(pos.distanceSqToCenter(player.posX, player.posY, player.posZ)) + 0.7D;
+        Vec3d eyePos = new Vec3d(player.posX, player.posY + player.getEyeHeight(), player.posZ);
+        Vec3d lookingPos = eyePos.add(new Vec3d(player.getLookVec().x * length, player.getLookVec().y * length, player.getLookVec().z * length));
+
+        TEQuern teQuern = Helpers.getTE(world, pos, TEQuern.class);
+
+        if (teQuern != null)
+        {
+            IItemHandler inventory = teQuern.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+            // Draws the correct selection box depending on where the player is looking at
+            if (!teQuern.isGrinding() && teQuern.hasHandstone() && HANDLE_AABB.offset(pos).calculateIntercept(eyePos, lookingPos) != null)
+            {
+                return SelectionPlace.HANDLE;
+            }
+            else if (!teQuern.isGrinding() && teQuern.hasHandstone() && (!player.getHeldItem(EnumHand.MAIN_HAND).isEmpty() || (inventory != null && !inventory.getStackInSlot(TEQuern.SLOT_INPUT).isEmpty())) && INPUT_SLOT_AABB.offset(pos).calculateIntercept(eyePos, lookingPos) != null)
+            {
+                return SelectionPlace.INPUT_SLOT;
+            }
+            else if ((teQuern.hasHandstone() || teQuern.isItemValid(TEQuern.SLOT_HANDSTONE, player.getHeldItem(EnumHand.MAIN_HAND))) && HANDSTONE_AABB.offset(pos).calculateIntercept(eyePos, lookingPos) != null)
+            {
+                return SelectionPlace.HANDSTONE;
+            }
+        }
+        return SelectionPlace.BASE;
+    }
+
     @Override
     public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
     {
@@ -168,89 +207,35 @@ public class BlockQuern extends Block implements IItemSize
             TEQuern teQuern = Helpers.getTE(world, pos, TEQuern.class);
             if (teQuern != null && !teQuern.isGrinding())
             {
-                ItemStack stack = playerIn.getHeldItem(hand);
-                if (playerIn.isSneaking())
+                ItemStack heldStack = playerIn.getHeldItem(hand);
+                SelectionPlace selection = getPlayerSelection(world, pos, playerIn);
+                IItemHandler inventory = teQuern.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+                if (inventory != null)
                 {
-                    //In world crafting
-                    if (stack.isEmpty())
+                    if (selection == SelectionPlace.HANDLE)
                     {
-                        //Start grinding animation
-                        if (facing == EnumFacing.UP && hitX > 0.2f && hitX < 0.4f && hitZ > 0.2f && hitZ < 0.4f && hitY >= 0.875)
-                        {
-                            teQuern.grind();
-                            world.playSound(null, pos, TFCSounds.QUERN_USE, SoundCategory.BLOCKS, 1, 1 + ((world.rand.nextFloat() - world.rand.nextFloat()) / 16));
-                            return true;
-                        }
-                        else
-                        {
-                            //Extract items
-                            IItemHandler inventory = teQuern.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
-                            if (inventory != null && teQuern.hasHandstone())
-                            {
-                                if (facing == EnumFacing.UP && hitX > 0.4f && hitX < 0.6f && hitZ > 0.4f && hitZ < 0.6f)
-                                {
-                                    if (!inventory.getStackInSlot(TEQuern.SLOT_INPUT).isEmpty())
-                                    {
-                                        ItemHandlerHelper.giveItemToPlayer(playerIn, inventory.extractItem(TEQuern.SLOT_INPUT, inventory.getStackInSlot(TEQuern.SLOT_INPUT).getCount(), false));
-                                        teQuern.setAndUpdateSlots(TEQuern.SLOT_INPUT);
-                                        return true;
-                                    }
-                                }
-                                else
-                                {
-                                    if (!inventory.getStackInSlot(TEQuern.SLOT_OUTPUT).isEmpty())
-                                    {
-                                        ItemHandlerHelper.giveItemToPlayer(playerIn, inventory.extractItem(TEQuern.SLOT_OUTPUT, inventory.getStackInSlot(TEQuern.SLOT_OUTPUT).getCount(), false));
-                                        teQuern.setAndUpdateSlots(TEQuern.SLOT_OUTPUT);
-                                        return true;
-                                    }
-                                    else if (inventory.getStackInSlot(TEQuern.SLOT_INPUT).isEmpty()) //Prevents taking  handstone out if has input
-                                    {
-                                        ItemHandlerHelper.giveItemToPlayer(playerIn, inventory.extractItem(TEQuern.SLOT_HANDSTONE, inventory.getStackInSlot(TEQuern.SLOT_HANDSTONE).getCount(), false));
-                                        teQuern.setAndUpdateSlots(TEQuern.SLOT_HANDSTONE);
-                                        return true;
-                                    }
-                                }
-                            }
-                        }
+                        teQuern.grind();
+                        world.playSound(null, pos, TFCSounds.QUERN_USE, SoundCategory.BLOCKS, 1, 1 + ((world.rand.nextFloat() - world.rand.nextFloat()) / 16));
+                        return true;
                     }
-                    else
+                    else if (selection == SelectionPlace.INPUT_SLOT)
                     {
-                        //Insert items
-                        IItemHandler inventory = teQuern.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
-                        if (inventory != null)
-                        {
-                            if (teQuern.isItemValid(TEQuern.SLOT_HANDSTONE, stack) && !teQuern.hasHandstone())
-                            {
-                                if (stack.getCount() == 1)
-                                {
-                                    playerIn.setHeldItem(EnumHand.MAIN_HAND, teQuern.insertOrSwapItem(TEQuern.SLOT_HANDSTONE, stack));
-                                }
-                                else
-                                {
-                                    playerIn.addItemStackToInventory(teQuern.insertOrSwapItem(TEQuern.SLOT_HANDSTONE, stack.splitStack(1)));
-                                }
-                                teQuern.setAndUpdateSlots(TEQuern.SLOT_HANDSTONE);
-                                world.playSound(null, pos, SoundEvents.BLOCK_STONE_PLACE, SoundCategory.BLOCKS, 1, 1);
-                                return true;
-                            }
-                            else if (teQuern.hasHandstone() && inventory.getStackInSlot(TEQuern.SLOT_INPUT).isEmpty() && facing == EnumFacing.UP && hitX > 0.4f && hitX < 0.6f && hitZ > 0.4f && hitZ < 0.6f)
-                            {
-                                playerIn.setHeldItem(EnumHand.MAIN_HAND, teQuern.insertOrSwapItem(TEQuern.SLOT_INPUT, stack));
-                                teQuern.setAndUpdateSlots(TEQuern.SLOT_INPUT);
-                                return true;
-                            }
-                        }
+                        playerIn.setHeldItem(EnumHand.MAIN_HAND, teQuern.insertOrSwapItem(TEQuern.SLOT_INPUT, heldStack));
+                        teQuern.setAndUpdateSlots(TEQuern.SLOT_INPUT);
+                        return true;
                     }
-
-                }
-                else
-                {
-                    if (!world.isRemote)
+                    else if (selection == SelectionPlace.HANDSTONE && inventory.getStackInSlot(TEQuern.SLOT_INPUT).isEmpty())
                     {
-                        TFCGuiHandler.openGui(world, pos, playerIn, TFCGuiHandler.Type.QUERN);
+                        playerIn.setHeldItem(EnumHand.MAIN_HAND, teQuern.insertOrSwapItem(TEQuern.SLOT_HANDSTONE, heldStack));
+                        teQuern.setAndUpdateSlots(TEQuern.SLOT_HANDSTONE);
+                        return true;
                     }
-                    return true;
+                    else if (selection == SelectionPlace.BASE && !inventory.getStackInSlot(TEQuern.SLOT_OUTPUT).isEmpty())
+                    {
+                        ItemHandlerHelper.giveItemToPlayer(playerIn, inventory.extractItem(TEQuern.SLOT_OUTPUT, inventory.getStackInSlot(TEQuern.SLOT_OUTPUT).getCount(), false));
+                        teQuern.setAndUpdateSlots(TEQuern.SLOT_OUTPUT);
+                        return true;
+                    }
                 }
             }
         }
@@ -281,5 +266,50 @@ public class BlockQuern extends Block implements IItemSize
     public TileEntity createTileEntity(World world, IBlockState state)
     {
         return new TEQuern();
+    }
+
+    @Override
+    public boolean drawHighlight(World world, BlockPos pos, EntityPlayer player, RayTraceResult rayTrace, double partialTicks)
+    {
+        double dx = player.lastTickPosX + (player.posX - player.lastTickPosX) * partialTicks;
+        double dy = player.lastTickPosY + (player.posY - player.lastTickPosY) * partialTicks;
+        double dz = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * partialTicks;
+
+        SelectionPlace selection = getPlayerSelection(world, pos, player);
+
+        // Draws the correct selection box depending on where the player is looking at
+        if (selection == SelectionPlace.HANDLE)
+        {
+            // Draws handle AABB if player is looking at it
+            IHighlightHandler.drawBox(HANDLE_AABB.offset(pos).offset(-dx, -dy, -dz), 1f, 0, 0, 0, 0.4f);
+        }
+        else if (selection == SelectionPlace.INPUT_SLOT)
+        {
+            // Draws item input AABB if user has item in main hand or there is an item in slot
+            IHighlightHandler.drawBox(INPUT_SLOT_AABB.offset(pos).offset(-dx, -dy, -dz), 1f, 0, 0, 0, 0.4f);
+        }
+        else if (selection == SelectionPlace.HANDSTONE)
+        {
+            // Draws handstone AABB if player is looking at it
+            IHighlightHandler.drawBox(HANDSTONE_AABB.offset(pos).offset(-dx, -dy, -dz).grow(0.002D), 1f, 0, 0, 0, 0.4f);
+        }
+        else
+        {
+            // Just draw the base outline (last grow is just what MC does to actually make the outline visible
+            IHighlightHandler.drawBox(BASE_AABB.offset(pos).offset(-dx, -dy, -dz).grow(0.002D), 1f, 0, 0, 0, 0.4f);
+        }
+        return true;
+    }
+
+    /**
+     * Just a helper enum to figure out where player is looking at
+     * Used to draw selection boxes + handle interaction
+     */
+    private enum SelectionPlace
+    {
+        HANDLE,
+        HANDSTONE,
+        INPUT_SLOT,
+        BASE
     }
 }
