3e26ae49875041a29e79b911781cf8858a265547
DisasterMoo <luan.santos.pinto@gmail.com>
2020-04-24 04:33:22 -0300

    Added steam particle effects to hot water (Closes #1081).

--- src/main/java/net/dries007/tfc/objects/blocks/BlockFluidHotWater.java
+++ src/main/java/net/dries007/tfc/objects/blocks/BlockFluidHotWater.java
@@ -6,15 +6,23 @@
 package net.dries007.tfc.objects.blocks;
 
 import java.util.Random;
+import javax.annotation.ParametersAreNonnullByDefault;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -22,6 +30,9 @@ import net.dries007.tfc.Constants;
 import net.dries007.tfc.api.capability.food.FoodStatsTFC;
 import net.dries007.tfc.objects.fluids.FluidsTFC;
 
+import static net.dries007.tfc.TerraFirmaCraft.MOD_ID;
+
+@ParametersAreNonnullByDefault
 public class BlockFluidHotWater extends BlockFluidTFC
 {
     public BlockFluidHotWater()
@@ -38,7 +49,17 @@ public class BlockFluidHotWater extends BlockFluidTFC
     {
         if (rand.nextInt(4) == 0)
         {
-            worldIn.spawnParticle(EnumParticleTypes.WATER_BUBBLE, (double) (pos.getX() + rand.nextFloat()), pos.getY() + 0.50D, (double) (pos.getZ() + rand.nextFloat()), 0.0D, 0.0D, 0.0D, Block.getStateId(stateIn));
+            worldIn.spawnParticle(EnumParticleTypes.WATER_BUBBLE, pos.getX() + rand.nextFloat(), pos.getY() + 0.50D, pos.getZ() + rand.nextFloat(), 0.0D, 0.0D, 0.0D, Block.getStateId(stateIn));
+        }
+        if (worldIn.isAirBlock(pos.up()))
+        {
+            // Classic made 4 particles spawn at a time
+            // imo just one per surface is good enough
+            double posX = pos.getX() + 0.5D;
+            double posY = pos.getY() + 1.0D;
+            double posZ = pos.getZ() + 0.5D;
+            ParticleSteam steam = new ParticleSteam(worldIn, posX, posY, posZ);
+            Minecraft.getMinecraft().effectRenderer.addEffect(steam);
         }
     }
 
@@ -53,6 +74,48 @@ public class BlockFluidHotWater extends BlockFluidTFC
             {
                 entityLiving.heal(FoodStatsTFC.PASSIVE_HEAL_AMOUNT * 7f);
             }
+        }
+    }
+
+    /**
+     * Inner Class, since it's only needed here
+     * Should more custom particles become needed, feel free to move this to the client subpackage
+     */
+    @ParametersAreNonnullByDefault
+    @SideOnly(Side.CLIENT)
+    @Mod.EventBusSubscriber(modid = MOD_ID)
+    public static final class ParticleSteam extends Particle
+    {
+        private static final ResourceLocation PARTICLES_LOCATION = new ResourceLocation(MOD_ID, "particle/steam");
+        private static TextureAtlasSprite STEAM_SPRITE;
+
+        @SubscribeEvent
+        public static void onTextureStitchEvent(TextureStitchEvent.Pre event)
+        {
+            STEAM_SPRITE = event.getMap().registerSprite(PARTICLES_LOCATION);
+        }
+
+
+        public ParticleSteam(World worldIn, double x, double y, double z)
+        {
+            super(worldIn, x, y, z, 0.0D, 0.0D, 0.0D);
+            this.setParticleTexture(STEAM_SPRITE);
+            this.particleAlpha = 0.05F;
+            this.particleMaxAge = (int) (12.0F / (this.rand.nextFloat() * 0.9F + 0.1F));
+        }
+
+        @Override
+        public boolean shouldDisableDepth()
+        {
+            // This is needed to order the transparency later than the fluid block
+            // Fix it being completely transparent
+            return true;
+        }
+
+        @Override
+        public int getFXLayer()
+        {
+            return 1;
         }
     }
 }
