46a113dc79f8c1655a992f8158ec9c5004fa8093
Claycorp <Claycorp@doubledoordev.net>
2020-04-24 21:33:21 -0500

    Added thirst potion effect. Closes #808

---
+++ src/main/java/net/dries007/tfc/objects/potioneffects/PotionThirst.java
@@ -0,0 +1,50 @@
+package net.dries007.tfc.objects.potioneffects;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayerMP;
+
+import net.dries007.tfc.api.capability.food.IFoodStatsTFC;
+
+public class PotionThirst extends PotionTFC
+{
+    public PotionThirst()
+    {
+        super(true, 0x2c86d4);
+        setPotionName("effectsTFC.thirst");
+        setIconIndex(0, 1);
+    }
+
+    @Override
+    public void performEffect(EntityLivingBase entity, int amplifier)
+    {
+        EntityPlayerMP player = null;
+        IFoodStatsTFC foodStatsTFC = null;
+        if (entity instanceof EntityPlayerMP)
+        {
+            player = (EntityPlayerMP) entity;
+            if (player.getFoodStats() instanceof IFoodStatsTFC)
+            {
+                foodStatsTFC = (IFoodStatsTFC) player.getFoodStats();
+            }
+        }
+
+        if (player != null && foodStatsTFC != null)
+        {
+            float thirst = foodStatsTFC.getThirst();
+
+            foodStatsTFC.setThirst(thirst - 0.05F * (float) (amplifier + 1));
+        }
+    }
+
+    @Override
+    public boolean isReady(int duration, int amplifier)
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isInstant()
+    {
+        return true;
+    }
+}
