04567e737e8cf61bf13f73039c21e365864b55d2
Claycorp <Claycorp@doubledoordev.net>
2020-04-25 01:45:54 -0500

    Changed lit log pile to lit forge texture. Closes #988 Changed the fucking water name for stupid "hot" water to reflect what it really is... Closes #1072 (No we don't need more dammed waters..) Fixed an oops with the change for logs from a fisting trees. Really Closes #918

--- src/main/java/net/dries007/tfc/objects/blocks/wood/BlockLogTFC.java
+++ src/main/java/net/dries007/tfc/objects/blocks/wood/BlockLogTFC.java
@@ -188,7 +188,7 @@ public class BlockLogTFC extends BlockLog implements IItemSize
             // False so vanilla will not drop a log itemstack (which would lead to an exploit)
             return false;
         }
-        return !ConfigTFC.GENERAL.doLogsRequireAxe;
+        return super.removedByPlayer(state, world, pos, player, ConfigTFC.GENERAL.doLogsRequireAxe);
     }
 
     @Override
