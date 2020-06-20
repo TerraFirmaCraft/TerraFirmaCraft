8f812af7973ab487d42ca0361bef145d2e414b07
aldo1028 <abdyman@gmail.com>
2020-06-20 06:24:34 -0400

    Textures for Boar, Turkey, Zebu and Gazelle plus some tweaks

--- src/main/java/net/dries007/tfc/client/model/animal/ModelTurkeyTFC.java
+++ src/main/java/net/dries007/tfc/client/model/animal/ModelTurkeyTFC.java
@@ -16,6 +16,8 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import net.dries007.tfc.api.types.IAnimalTFC;
+import net.dries007.tfc.objects.entity.animal.EntityAnimalTFC;
 import net.dries007.tfc.objects.entity.animal.EntityTurkeyTFC;
 
 /**
@@ -53,7 +55,7 @@ public class ModelTurkeyTFC extends ModelBase
     public ModelRenderer wingRTip;
     public ModelRenderer tailFMain;
     public ModelRenderer tailBase;
-    public ModelRenderer tailFTip;
+    public ModelRenderer nose;
     public ModelRenderer beak;
     public ModelRenderer wingLBack;
     public ModelRenderer wingLFront;
@@ -62,6 +64,7 @@ public class ModelTurkeyTFC extends ModelBase
     public ModelRenderer legRFeet;
     public ModelRenderer legLBottom;
     public ModelRenderer legLFeet;
+    public ModelRenderer thingy;
 
     public ModelTurkeyTFC()
     {
@@ -79,9 +82,9 @@ public class ModelTurkeyTFC extends ModelBase
         tails5.setRotationPoint(-0.2F, 13.0F, 5.1F);
         tails5.addBox(-1.5F, -13.0F, 0.0F, 3, 13, 0, 0.0F);
         setRotateAngle(tails5, -0.3490658503988659F, -0.03490658503988659F, -0.2617993877991494F);
-        neck = new ModelRenderer(this, 38, 22);
-        neck.setRotationPoint(0.0F, 15.4F, -7.8F);
-        neck.addBox(-2.0F, -5.0F, -2.0F, 4, 6, 3, -0.2F);
+        neck = new ModelRenderer(this, 37, 22);
+        neck.setRotationPoint(0.0F, 16.4F, -7.8F);
+        neck.addBox(-2.0F, -5.0F, -2.0F, 4, 5, 4, -0.2F);
         setRotateAngle(neck, 0.08726646259971647F, 0.0F, 0.0F);
         tails4 = new ModelRenderer(this, 0, 0);
         tails4.setRotationPoint(-0.4F, 13.0F, 5.2F);
@@ -95,44 +98,41 @@ public class ModelTurkeyTFC extends ModelBase
         legLTop.setRotationPoint(2.4F, 19.0F, 0.5F);
         legLTop.addBox(-1.0F, 0.0F, -2.0F, 2, 3, 2, 0.2F);
         head = new ModelRenderer(this, 0, 20);
-        head.setRotationPoint(-0.5F, 4.4F, -7.5F);
-        head.addBox(-2.0F, -1.5F, -2.5F, 5, 4, 4, 0.2F);
+        head.setRotationPoint(0.0F, 5.8F, -7.2F);
+        head.addBox(-2.5F, -2.0F, -2.0F, 5, 4, 4, 0.1F);
         chest = new ModelRenderer(this, 31, 31);
         chest.setRotationPoint(0.0F, 14.0F, -5.8F);
         chest.addBox(-3.5F, -3.0F, -3.0F, 7, 7, 7, 0.0F);
         setRotateAngle(chest, -0.3490658503988659F, 0.0F, 0.0F);
         tailFMain = new ModelRenderer(this, 10, 46);
         tailFMain.setRotationPoint(0.0F, -0.8F, 7.5F);
-        tailFMain.addBox(-2.0F, -1.5F, -1.5F, 4, 5, 3, 0.0F);
+        tailFMain.addBox(-2.0F, -1.5F, -1.5F, 4, 4, 3, 0.0F);
         setRotateAngle(tailFMain, -0.3490658503988659F, 0.0F, 0.0F);
         tails3 = new ModelRenderer(this, 0, 0);
         tails3.setRotationPoint(-0.6F, 13.0F, 5.3F);
         tails3.addBox(-1.5F, -13.0F, 0.0F, 3, 13, 0, 0.0F);
         setRotateAngle(tails3, -0.3490658503988659F, -0.10471975511965977F, -0.7853981633974483F);
-        wingLBack = new ModelRenderer(this, 23, 18);
+        wingLBack = new ModelRenderer(this, 23, 16);
         wingLBack.mirror = true;
         wingLBack.setRotationPoint(-0.1F, 0.0F, 2.5F);
         wingLBack.addBox(-0.5F, 0.0F, 0.0F, 1, 3, 2, 0.0F);
         setRotateAngle(wingLBack, -0.17453292519943295F, 0.0F, 0.0F);
-        wingRBack = new ModelRenderer(this, 23, 18);
+        wingRBack = new ModelRenderer(this, 23, 16);
         wingRBack.setRotationPoint(0.1F, 0.0F, 2.5F);
         wingRBack.addBox(-0.5F, 0.0F, 0.0F, 1, 3, 2, 0.0F);
         setRotateAngle(wingRBack, -0.17453292519943295F, 0.0F, 0.0F);
-        tailFTip = new ModelRenderer(this, 10, 39);
-        tailFTip.setRotationPoint(0.0F, -1.0F, 1.0F);
-        tailFTip.addBox(-1.5F, 0.0F, 0.0F, 3, 3, 4, 0.0F);
-        wingLMain = new ModelRenderer(this, 20, 28);
+        wingLMain = new ModelRenderer(this, 20, 27);
         wingLMain.mirror = true;
         wingLMain.setRotationPoint(4.0F, 11.5F, -0.5F);
-        wingLMain.addBox(-0.5F, 0.0F, -2.5F, 1, 4, 5, 0.0F);
+        wingLMain.addBox(-0.5F, 0.0F, -2.5F, 1, 5, 5, 0.0F);
         setRotateAngle(wingLMain, -0.08726646259971647F, 0.0F, -0.0F);
-        wingLFront = new ModelRenderer(this, 23, 23);
+        wingLFront = new ModelRenderer(this, 23, 21);
         wingLFront.mirror = true;
         wingLFront.setRotationPoint(0.0F, 0.0F, -4.5F);
-        wingLFront.addBox(-0.5F, 0.0F, 0.0F, 1, 3, 2, 0.0F);
-        wingRMain = new ModelRenderer(this, 20, 28);
+        wingLFront.addBox(-0.5F, 0.0F, 0.0F, 1, 4, 2, 0.0F);
+        wingRMain = new ModelRenderer(this, 20, 27);
         wingRMain.setRotationPoint(-4.0F, 11.5F, -0.5F);
-        wingRMain.addBox(-0.5F, 0.0F, -2.5F, 1, 4, 5, 0.0F);
+        wingRMain.addBox(-0.5F, 0.0F, -2.5F, 1, 5, 5, 0.0F);
         setRotateAngle(wingRMain, -0.08726646259971647F, 0.0F, 0.0F);
         tails8 = new ModelRenderer(this, 0, 0);
         tails8.setRotationPoint(0.4F, 13.0F, 5.2F);
@@ -150,16 +150,19 @@ public class ModelTurkeyTFC extends ModelBase
         tailBase.setRotationPoint(0.0F, -0.8F, 5.5F);
         tailBase.addBox(-3.5F, -2.5F, -1.5F, 7, 7, 3, 0.0F);
         setRotateAngle(tailBase, -0.17453292519943295F, 0.0F, 0.0F);
-        beak = new ModelRenderer(this, 3, 15);
-        beak.setRotationPoint(0.0F, 1.0F, -3.5F);
-        beak.addBox(-1.0F, -1.0F, -1.0F, 3, 2, 3, 0.0F);
+        nose = new ModelRenderer(this, 5, 17);
+        nose.setRotationPoint(0.0F, 0.8F, -1.8F);
+        nose.addBox(-1.5F, -1.0F, -1.0F, 3, 2, 1, 0.0F);
+        beak = new ModelRenderer(this, 4, 12);
+        beak.setRotationPoint(0.0F, 0.25F, -1.5F);
+        beak.addBox(-1.0F, -1.0F, -1.0F, 2, 2, 3, 0.0F);
         tails10 = new ModelRenderer(this, 0, 0);
         tails10.setRotationPoint(0.8F, 13.0F, 5.4F);
         tails10.addBox(-1.5F, -13.0F, 0.0F, 3, 13, 0, 0.0F);
         setRotateAngle(tails10, -0.3490658503988659F, 0.13962634015954636F, 1.0471975511965976F);
-        neck3 = new ModelRenderer(this, 38, 10);
-        neck3.setRotationPoint(0.0F, 8.4F, -7.5F);
-        neck3.addBox(-2.0F, -1.5F, -1.5F, 4, 2, 3, -0.2F);
+        neck3 = new ModelRenderer(this, 37, 7);
+        neck3.setRotationPoint(0.0F, 8.78F, -7.56F);
+        neck3.addBox(-2.0F, -1.5F, -1.5F, 4, 3, 4, -0.22F);
         legRBottom = new ModelRenderer(this, 2, 44);
         legRBottom.setRotationPoint(0.0F, 3.0F, -1.0F);
         legRBottom.addBox(-0.5F, 0.0F, -0.5F, 1, 2, 1, 0.0F);
@@ -171,42 +174,46 @@ public class ModelTurkeyTFC extends ModelBase
         tails1.setRotationPoint(-1.0F, 13.0F, 5.5F);
         tails1.addBox(-1.5F, -13.0F, 0.0F, 3, 13, 0, 0.0F);
         setRotateAngle(tails1, -0.3490658503988659F, -0.17453292519943295F, -1.3089969389957472F);
-        wingLTip = new ModelRenderer(this, 24, 16);
+        wingLTip = new ModelRenderer(this, 24, 14);
         wingLTip.mirror = true;
         wingLTip.setRotationPoint(0.0F, 0.0F, 2.0F);
         wingLTip.addBox(-0.5F, 0.0F, 0.0F, 1, 1, 1, 0.0F);
-        neck2 = new ModelRenderer(this, 38, 15);
-        neck2.setRotationPoint(0.0F, 9.7F, -8.1F);
-        neck2.addBox(-2.0F, -2.0F, -1.5F, 4, 4, 3, -0.2F);
+        neck2 = new ModelRenderer(this, 37, 14);
+        neck2.setRotationPoint(0.0F, 10.72F, -8.1F);
+        neck2.addBox(-2.0F, -2.0F, -1.5F, 4, 4, 4, -0.21F);
         setRotateAngle(neck2, -0.4363323129985824F, 0.0F, 0.0F);
         legRFeet = new ModelRenderer(this, -1, 40);
-        legRFeet.setRotationPoint(0.0F, 2.0F, 0.0F);
+        legRFeet.setRotationPoint(0.0F, 2.0F, 0.5F);
         legRFeet.addBox(-1.5F, 0.0F, -2.5F, 3, 0, 4, 0.0F);
         tails9 = new ModelRenderer(this, 0, 0);
         tails9.setRotationPoint(0.6F, 13.0F, 5.3F);
         tails9.addBox(-1.5F, -13.0F, 0.0F, 3, 13, 0, 0.0F);
         setRotateAngle(tails9, -0.3490658503988659F, 0.10471975511965977F, 0.7853981633974483F);
-        wingRFront = new ModelRenderer(this, 23, 23);
+        wingRFront = new ModelRenderer(this, 23, 21);
         wingRFront.setRotationPoint(0.0F, 0.0F, -4.5F);
-        wingRFront.addBox(-0.5F, 0.0F, 0.0F, 1, 3, 2, 0.0F);
+        wingRFront.addBox(-0.5F, 0.0F, 0.0F, 1, 4, 2, 0.0F);
         legLFeet = new ModelRenderer(this, -1, 40);
-        legLFeet.setRotationPoint(0.0F, 2.0F, 0.0F);
+        legLFeet.setRotationPoint(0.0F, 2.0F, 0.5F);
         legLFeet.addBox(-1.5F, 0.0F, -2.5F, 3, 0, 4, 0.0F);
         legRTop = new ModelRenderer(this, 0, 47);
         legRTop.setRotationPoint(-2.4F, 19.0F, 0.5F);
         legRTop.addBox(-1.0F, 0.0F, -2.0F, 2, 3, 2, 0.2F);
-        wingRTip = new ModelRenderer(this, 24, 16);
+        wingRTip = new ModelRenderer(this, 24, 14);
         wingRTip.setRotationPoint(0.0F, 0.0F, 2.0F);
         wingRTip.addBox(-0.5F, 0.0F, 0.0F, 1, 1, 1, 0.0F);
+        thingy = new ModelRenderer(this, 7, 8);
+        thingy.setRotationPoint(-1.0F, -1.0F, 0.8F);
+        thingy.addBox(0.0F, 0.0F, -1.0F, 0, 3, 1, 0.0F);
 
         legLTop.addChild(legLBottom);
         body.addChild(tailFMain);
         wingLMain.addChild(wingLBack);
         wingRMain.addChild(wingRBack);
-        tailFMain.addChild(tailFTip);
         wingLMain.addChild(wingLFront);
         body.addChild(tailBase);
-        head.addChild(beak);
+        head.addChild(nose);
+        nose.addChild(beak);
+        beak.addChild(thingy);
         legRTop.addChild(legRBottom);
         wingLBack.addChild(wingLTip);
         legRBottom.addChild(legRFeet);
@@ -224,6 +231,36 @@ public class ModelTurkeyTFC extends ModelBase
 
         float percent = (float) turkey.getPercentToAdulthood();
         float ageScale = 2.0F - percent;
+
+        if (turkey.getGender() == EntityAnimalTFC.Gender.FEMALE)
+        {
+            setRotateAngle(tails1, -1.6390387005478748F, -0.17453292519943295F, -1.3089969389957472F);
+            setRotateAngle(tails2, -1.6845917940249266F, -0.13962634015954636F, -1.0471975511965976F);
+            setRotateAngle(tails3, -1.8212510744560826F, -0.10471975511965977F, -0.7853981633974483F);
+            setRotateAngle(tails4, -1.8212510744560826F, -0.06981317007977318F, -0.5235987755982988F);
+            setRotateAngle(tails5, -1.9577358219620393F, -0.03490658503988659F, -0.2617993877991494F);
+            setRotateAngle(tails6, -1.8668041679331349F, 0.0F, 0.0F);
+            setRotateAngle(tails7, -1.8212510744560826F, 0.03490658503988659F, 0.2617993877991494F);
+            setRotateAngle(tails8, -1.9123572614101867F, 0.06981317007977318F, 0.5235987755982988F);
+            setRotateAngle(tails9, -1.7756979809790308F, 0.10471975511965977F, 0.7853981633974483F);
+            setRotateAngle(tails10, -1.7756979809790308F, 0.13962634015954636F, 1.0471975511965976F);
+            setRotateAngle(tails11, -1.730144887501979F, 0.17453292519943295F, 1.3089969389957472F);
+            tails1.setRotationPoint(-2.0F, 14.0F, 5.0F);
+            tails2.setRotationPoint(-2.0F, 14.0F, 5.0F);
+            tails3.setRotationPoint(-1.0F, 13.0F, 5.0F);
+            tails4.setRotationPoint(0.0F, 13.0F, 5.0F);
+            tails5.setRotationPoint(0.0F, 13.0F, 5.0F);
+            tails6.setRotationPoint(0.0F, 13.0F, 5.0F);
+            tails7.setRotationPoint(0.0F, 12.0F, 5.0F);
+            tails8.setRotationPoint(1.0F, 14.0F, 5.0F);
+            tails9.setRotationPoint(1.0F, 15.0F, 5.0F);
+            tails10.setRotationPoint(2.0F, 13.0F, 5.0F);
+            tails11.setRotationPoint(3.0F, 14.0F, 5.0F);
+        }
+        else
+        {
+            tailFMain.isHidden = true;
+        }
 
         GlStateManager.pushMatrix();
         GlStateManager.scale(1 / ageScale, 1 / ageScale, 1 / ageScale);
@@ -265,8 +302,10 @@ public class ModelTurkeyTFC extends ModelBase
         wingLMain.rotateAngleZ = -par3;
         wingRMain.rotateAngleX = 0;
         wingLMain.rotateAngleX = 0;
-        //rightWing.setRotationPoint(-4.0F, 13, 0.0F);
-        //leftWing.setRotationPoint(4.0F, 13, 0.0F);
+        //wingRMain.setRotationPoint(-4.0F, 11.5, -0.5F);
+        //wingLMain.setRotationPoint(4.0F, 11.5, -0.5F);
+
+        tailFMain.isHidden = false;
     }
 
     public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
