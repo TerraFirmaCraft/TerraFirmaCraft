b28031aa0390cb76f6582a6daadcf3118301ba8e
aldo1028 <abdyman@gmail.com>
2020-07-25 03:03:57 -0400

    Updated Quail Model and Textures

--- src/main/java/net/dries007/tfc/client/model/animal/ModelQuailTFC.java
+++ src/main/java/net/dries007/tfc/client/model/animal/ModelQuailTFC.java
@@ -29,17 +29,22 @@ public class ModelQuailTFC extends ModelBase
 {
     public ModelRenderer head;
     public ModelRenderer bodyTop;
-    public ModelRenderer legL;
     public ModelRenderer wingR;
     public ModelRenderer body;
-    public ModelRenderer legR;
     public ModelRenderer wingL;
-    public ModelRenderer bill;
+    public ModelRenderer beak1;
+    public ModelRenderer beak2;
     public ModelRenderer crown;
     public ModelRenderer wingR2;
     public ModelRenderer body2;
     public ModelRenderer tail;
     public ModelRenderer wingL2;
+    public ModelRenderer legL1;
+    public ModelRenderer legL2;
+    public ModelRenderer feetL;
+    public ModelRenderer legR1;
+    public ModelRenderer legR2;
+    public ModelRenderer feetR;
 
     public ModelQuailTFC()
     {
@@ -47,57 +52,89 @@ public class ModelQuailTFC extends ModelBase
         textureHeight = 32;
 
         head = new ModelRenderer(this, 2, 4);
-        head.setRotationPoint(0.0F, 16.0F, -3.2F);
+        head.setRotationPoint(0.0F, 17.0F, -3.2F);
         head.addBox(-2.0F, -6.0F, -2.0F, 4, 5, 3, 0.0F);
         crown = new ModelRenderer(this, 0, -3);
-        crown.setRotationPoint(0.0F, -7.5F, -1.0F);
+        crown.setRotationPoint(0.0F, -7.5F, -2.0F);
         crown.addBox(0.0F, -1.5F, -1.5F, 0, 3, 3, 0.0F);
         body2 = new ModelRenderer(this, 37, 12);
-        body2.setRotationPoint(0.0F, 4.8F, 0.5F);
+        body2.setRotationPoint(0.0F, 4.0F, 0.5F);
         body2.addBox(-1.5F, -2.0F, -2.0F, 3, 3, 4, 0.0F);
         setRotateAngle(body2, 0.08726646259971647F, 0.0F, 0.0F);
         wingL = new ModelRenderer(this, 16, 23);
         wingL.mirror = true;
-        wingL.setRotationPoint(3.5F, 14.0F, 1.0F);
+        wingL.setRotationPoint(3.5F, 16.0F, 1.5F);
         wingL.addBox(-0.5F, 0.0F, -3.0F, 1, 4, 5, 0.0F);
+        setRotateAngle(wingL, -0.2617993877991494F, 0.0F, -0.0F);
         wingL2 = new ModelRenderer(this, 20, 20);
         wingL2.setRotationPoint(0.0F, 0.0F, 2.0F);
         wingL2.addBox(-0.5F, 0.0F, 0.0F, 1, 2, 1, 0.0F);
-        legR = new ModelRenderer(this, 0, 25);
-        legR.setRotationPoint(-2.0F, 20.0F, 1.0F);
-        legR.addBox(-1.0F, 0.0F, -3.0F, 3, 4, 3, 0.0F);
         wingR2 = new ModelRenderer(this, 20, 20);
         wingR2.setRotationPoint(0.0F, 0.0F, 2.0F);
         wingR2.addBox(-0.5F, 0.0F, 0.0F, 1, 2, 1, 0.0F);
         wingR = new ModelRenderer(this, 16, 23);
-        wingR.setRotationPoint(-3.5F, 14.0F, 1.0F);
+        wingR.setRotationPoint(-3.5F, 16.0F, 1.5F);
         wingR.addBox(-0.5F, 0.0F, -3.0F, 1, 4, 5, 0.0F);
-        tail = new ModelRenderer(this, 41, 9);
-        tail.setRotationPoint(0.0F, 1.0F, 0.0F);
-        tail.addBox(-1.0F, 0.0F, 0.0F, 2, 2, 1, 0.0F);
+        setRotateAngle(wingR, -0.2617993877991494F, 0.0F, 0.0F);
+        tail = new ModelRenderer(this, 40, 6);
+        tail.setRotationPoint(0.0F, 1.0F, -0.2F);
+        tail.addBox(-1.0F, 0.0F, 0.0F, 2, 4, 2, 0.0F);
         setRotateAngle(tail, 0.08726646259971647F, 0.0F, 0.0F);
         body = new ModelRenderer(this, 32, 19);
-        body.setRotationPoint(0.0F, 17.0F, 1.0F);
+        body.setRotationPoint(0.0F, 18.0F, 1.0F);
         body.addBox(-3.0F, -4.0F, -3.0F, 6, 7, 6, 0.0F);
         setRotateAngle(body, 1.3962634015954636F, 0.0F, 0.0F);
-        legL = new ModelRenderer(this, 0, 25);
-        legL.mirror = true;
-        legL.setRotationPoint(1.0F, 20.0F, 1.0F);
-        legL.addBox(-1.0F, 0.0F, -3.0F, 3, 4, 3, 0.0F);
         bodyTop = new ModelRenderer(this, 0, 12);
-        bodyTop.setRotationPoint(0.0F, 16.0F, -3.1F);
+        bodyTop.setRotationPoint(0.0F, 17.0F, -3.1F);
         bodyTop.addBox(-2.5F, -2.0F, -1.5F, 5, 4, 4, 0.0F);
         setRotateAngle(bodyTop, 0.6108652381980153F, 0.0F, 0.0F);
-        bill = new ModelRenderer(this, 17, 11);
-        bill.setRotationPoint(1.0F, -4.0F, -3.0F);
-        bill.addBox(-2.0F, -1.0F, -1.0F, 2, 2, 2, 0.0F);
+        beak1 = new ModelRenderer(this, 20, 14);
+        beak1.setRotationPoint(0.0F, -3.5F, -2.8F);
+        beak1.addBox(-1.0F, -1.0F, -1.0F, 2, 1, 2, 0.0F);
+        setRotateAngle(beak1, 0.17453292519943295F, 0.0F, 0.0F);
+        beak2 = new ModelRenderer(this, 20, 14);
+        beak2.setRotationPoint(0.0F, -3.5F, -2.8F);
+        beak2.addBox(-1.0F, -0.5F, -1.0F, 2, 1, 2, 0.0F);
+
+        legR1 = new ModelRenderer(this, 5, 29);
+        legR1.setRotationPoint(-1.5F, 21.0F, 0.0F);
+        legR1.addBox(-0.5F, -1.0F, 0.0F, 1, 2, 1, 0.2F);
+        setRotateAngle(legR1, 0.08726646259971647F, 0.0F, 0.0F);
+        legR2 = new ModelRenderer(this, 5, 26);
+        legR2.setRotationPoint(0.0F, 0.8F, 0.0F);
+        legR2.addBox(-0.5F, 0.0F, 0.0F, 1, 2, 1, 0.0F);
+        setRotateAngle(legR2, -0.17453292519943295F, 0.0F, 0.0F);
+        feetR = new ModelRenderer(this, 3, 22);
+        feetR.setRotationPoint(1.0F, 1.8F, 1.8F);
+        feetR.addBox(-2.5F, 0.0F, -3.0F, 3, 1, 3, 0.0F);
+        setRotateAngle(feetR, 0.04363323129985824F, 0.0F, 0.0F);
+        legL1 = new ModelRenderer(this, 5, 29);
+        legL1.mirror = true;
+        legL1.setRotationPoint(1.5F, 21.0F, 0.0F);
+        legL1.addBox(-0.5F, -1.0F, 0.0F, 1, 2, 1, 0.2F);
+        setRotateAngle(legL1, 0.08726646259971647F, 0.0F, 0.0F);
+        legL2 = new ModelRenderer(this, 5, 26);
+        legL2.mirror = true;
+        legL2.setRotationPoint(0.0F, 0.8F, 0.0F);
+        legL2.addBox(-0.5F, 0.0F, 0.0F, 1, 2, 1, 0.0F);
+        setRotateAngle(legL2, -0.17453292519943295F, 0.0F, 0.0F);
+        feetL = new ModelRenderer(this, 3, 22);
+        feetL.mirror = true;
+        feetL.setRotationPoint(1.0F, 1.8F, 1.8F);
+        feetL.addBox(-2.5F, 0.0F, -3.0F, 3, 1, 3, 0.0F);
+        setRotateAngle(feetL, 0.04363323129985824F, 0.0F, 0.0F);
 
         head.addChild(crown);
         body.addChild(body2);
         wingL.addChild(wingL2);
         wingR.addChild(wingR2);
         body2.addChild(tail);
-        head.addChild(bill);
+        head.addChild(beak1);
+        head.addChild(beak2);
+        legR1.addChild(legR2);
+        legR2.addChild(feetR);
+        legL1.addChild(legL2);
+        legL2.addChild(feetL);
     }
 
     @Override
@@ -117,8 +154,8 @@ public class ModelQuailTFC extends ModelBase
         head.render(par7);
         body.render(par7);
         bodyTop.render(par7);
-        legR.render(par7);
-        legL.render(par7);
+        legR1.render(par7);
+        legL1.render(par7);
         wingR.render(par7);
         wingL.render(par7);
         GlStateManager.popMatrix();
@@ -130,15 +167,15 @@ public class ModelQuailTFC extends ModelBase
         this.head.rotateAngleX = -(par5 / (180F / (float) Math.PI));
         this.head.rotateAngleY = par4 / (180F / (float) Math.PI);
 
-        //setRotateAngle(wingR, -0.17453292519943295F, 0.0F, 0.0F);
-        //setRotateAngle(wingL, -0.17453292519943295F, 0.0F, 0.0F);
+        setRotateAngle(wingR, -0.2617993877991494F, 0.0F, 0.0F);
+        setRotateAngle(wingL, -0.2617993877991494F, 0.0F, -0.0F);
 
-        this.legR.rotateAngleX = MathHelper.cos(par1 * 0.6662F) * 1.4F * par2;
-        this.legL.rotateAngleX = MathHelper.cos(par1 * 0.6662F + (float) Math.PI) * 1.4F * par2;
+        this.legR1.rotateAngleX = MathHelper.cos(par1 * 0.6662F) * 1.4F * par2;
+        this.legL1.rotateAngleX = MathHelper.cos(par1 * 0.6662F + (float) Math.PI) * 1.4F * par2;
         this.wingR.rotateAngleZ = par3;
         this.wingL.rotateAngleZ = -par3;
-        this.wingR.rotateAngleX = 0;
-        this.wingL.rotateAngleX = 0;
+        //this.wingR.rotateAngleX = 0;
+        //this.wingL.rotateAngleX = 0;
         //this.wingR.setRotationPoint(-4.0F, 13, -10.0F);
         //this.wingL.setRotationPoint(4.0F, 13, -10.0F);
     }
