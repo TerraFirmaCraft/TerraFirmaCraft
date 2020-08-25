f2608b38b2f56b1d721419f955dc87abe482c254
AlcatrazEscapee <alex@molleroneill.com>
2020-08-25 10:39:58 -0400

    WIP tree features

--- src/main/java/net/dries007/tfc/world/feature/trees/NormalTreeConfig.java
+++ src/main/java/net/dries007/tfc/world/feature/trees/NormalTreeConfig.java
@@ -5,6 +5,10 @@
 
 package net.dries007.tfc.world.feature.trees;
 
+import javax.annotation.Nullable;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.gen.feature.IFeatureConfig;
 
 import com.mojang.datafixers.Dynamic;
@@ -12,9 +16,50 @@ import com.mojang.datafixers.types.DynamicOps;
 
 public class NormalTreeConfig implements IFeatureConfig
 {
+    @Nullable
     public static <T> NormalTreeConfig deserialize(Dynamic<T> config)
     {
-        return new NormalTreeConfig();
+        return null;
+    }
+
+    private final ResourceLocation base;
+    private final ResourceLocation overlay;
+    private final int heightMin;
+    private final int heightRange;
+    private final BlockState trunkState;
+
+    public NormalTreeConfig(ResourceLocation base, ResourceLocation overlay, int heightMin, int heightRange, BlockState trunkState)
+    {
+        this.base = base;
+        this.overlay = overlay;
+        this.heightMin = heightMin;
+        this.heightRange = heightRange;
+        this.trunkState = trunkState;
+    }
+
+    public ResourceLocation getBase()
+    {
+        return base;
+    }
+
+    public ResourceLocation getOverlay()
+    {
+        return overlay;
+    }
+
+    public int getHeightMin()
+    {
+        return heightMin;
+    }
+
+    public int getHeightRange()
+    {
+        return heightRange;
+    }
+
+    public BlockState getTrunkState()
+    {
+        return trunkState;
     }
 
     @Override
