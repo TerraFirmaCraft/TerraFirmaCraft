30663a9fdf3597b001677ddecd61706f65e9bfb0
LordFokas <lordfokas@gmail.com>
2020-12-10 21:15:51 +0000

    Fix issues raised in the review: - String ore changed to ItemStack vein; - Encapsulated event attributes; - Added Side attribute to event; - Added more performant enumeration lookup; - Serialize BlockPos as long instead;
    Also moved the classification of the ProspectResult ( score -> type ) from a bunch of loose logic inside a propick method to a proper method in ProspectResult that classifies itself. If this class ends up being used elsewhere, for whatever reason, we have the guarantee that the classification will be consistent accross the board.

--- src/main/java/net/dries007/tfc/api/events/ProspectEvent.java
+++ src/main/java/net/dries007/tfc/api/events/ProspectEvent.java
@@ -1,8 +1,10 @@
 package net.dries007.tfc.api.events;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.relauncher.Side;
 
 /**
  * Event fired when the Prospector's Pickaxe is used.
@@ -12,14 +14,14 @@ import net.minecraftforge.fml.common.eventhandler.Event;
 public abstract class ProspectEvent extends Event {
 
     public static class Server extends ProspectEvent {
-        public Server(EntityPlayer player, BlockPos pos, ResultType type, String ore, double score){
-            super(player, pos, type, ore, score);
+        public Server(EntityPlayer player, BlockPos pos, ResultType type, ItemStack vein){
+            super(Side.SERVER, player, pos, type, vein);
         }
     }
 
     public static class Client extends ProspectEvent {
-        public Client(EntityPlayer player, BlockPos pos, ResultType type, String ore, double score){
-            super(player, pos, type, ore, score);
+        public Client(EntityPlayer player, BlockPos pos, ResultType type, ItemStack vein){
+            super(Side.CLIENT, player, pos, type, vein);
         }
     }
 
@@ -33,24 +35,49 @@ public abstract class ProspectEvent extends Event {
         FOUND      ("tfc.propick.found"),         // right click on block
         NOTHING    ("tfc.propick.found_nothing"); // nothing interesting here
 
+        private static final ResultType[] VALUES = values();
         public final String translation;
 
         ResultType(String translation){
             this.translation = translation;
         }
+
+        public static ResultType valueOf(int ordinal){
+            return VALUES[ordinal];
+        }
     }
 
-    public final EntityPlayer player;
-    public final BlockPos pos;
-    public final ResultType type;
-    public final String ore;
-    public final double score;
+    private Side side;
+    private EntityPlayer player;
+    private BlockPos pos;
+    private ResultType type;
+    private ItemStack vein;
 
-    public ProspectEvent(EntityPlayer player, BlockPos pos, ResultType type, String ore, double score){
+    protected ProspectEvent(Side side, EntityPlayer player, BlockPos pos, ResultType type, ItemStack vein){
+        this.side = side;
         this.player = player;
         this.pos = pos;
         this.type = type;
-        this.ore = ore;
-        this.score = score;
+        this.vein = vein;
+    }
+
+    public Side getSide(){
+        return side;
+    }
+
+    public EntityPlayer getPlayer(){
+        return player;
+    }
+
+    public BlockPos getBlockPos(){
+        return pos;
+    }
+
+    public ResultType getResultType(){
+        return type;
+    }
+
+    public ItemStack getVein(){
+        return vein;
     }
 }
