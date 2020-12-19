e6a3a35795989c0a60e34b3375db2616cb8e55a2
LordFokas <lordfokas@gmail.com>
2020-12-19 15:37:28 +0000

    Make my previous changes respect the project's code style

--- src/main/java/net/dries007/tfc/api/events/ProspectEvent.java
+++ src/main/java/net/dries007/tfc/api/events/ProspectEvent.java
@@ -1,6 +1,7 @@
 package net.dries007.tfc.api.events;
 
 import net.dries007.tfc.objects.items.metal.ItemProspectorPick.ProspectResult.Type;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
@@ -12,16 +13,21 @@ import net.minecraftforge.fml.relauncher.Side;
  * Carries all the data relative to the result displayed to the player.
  * One of the two subclasses will be used according to the logical side.
  */
-public abstract class ProspectEvent extends Event {
+public abstract class ProspectEvent extends Event
+{
 
-    public static class Server extends ProspectEvent {
-        public Server(EntityPlayer player, BlockPos pos, Type type, ItemStack vein){
+    public static class Server extends ProspectEvent
+    {
+        public Server(EntityPlayer player, BlockPos pos, Type type, ItemStack vein)
+        {
             super(Side.SERVER, player, pos, type, vein);
         }
     }
 
-    public static class Client extends ProspectEvent {
-        public Client(EntityPlayer player, BlockPos pos, Type type, ItemStack vein){
+    public static class Client extends ProspectEvent
+    {
+        public Client(EntityPlayer player, BlockPos pos, Type type, ItemStack vein)
+        {
             super(Side.CLIENT, player, pos, type, vein);
         }
     }
@@ -32,7 +38,8 @@ public abstract class ProspectEvent extends Event {
     private Type type;
     private ItemStack vein;
 
-    protected ProspectEvent(Side side, EntityPlayer player, BlockPos pos, Type type, ItemStack vein){
+    protected ProspectEvent(Side side, EntityPlayer player, BlockPos pos, Type type, ItemStack vein)
+    {
         this.side = side;
         this.player = player;
         this.pos = pos;
@@ -40,23 +47,28 @@ public abstract class ProspectEvent extends Event {
         this.vein = vein;
     }
 
-    public Side getSide(){
+    public Side getSide()
+    {
         return side;
     }
 
-    public EntityPlayer getPlayer(){
+    public EntityPlayer getPlayer()
+    {
         return player;
     }
 
-    public BlockPos getBlockPos(){
+    public BlockPos getBlockPos()
+    {
         return pos;
     }
 
-    public Type getResultType(){
+    public Type getResultType()
+    {
         return type;
     }
 
-    public ItemStack getVein(){
+    public ItemStack getVein()
+    {
         return vein;
     }
 }
