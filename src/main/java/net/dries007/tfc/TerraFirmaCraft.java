c948acb2354851b008b310a53a0173af7c9faeea
Charnuz <charnuz.sc2@gmail.com>
2020-12-20 04:13:16 -0600

    register inventory packet handler

--- src/main/java/net/dries007/tfc/TerraFirmaCraft.java
+++ src/main/java/net/dries007/tfc/TerraFirmaCraft.java
@@ -131,6 +131,7 @@ public final class TerraFirmaCraft
         network.registerMessage(new PacketPlayerDataUpdate.Handler(), PacketPlayerDataUpdate.class, ++id, Side.CLIENT);
         network.registerMessage(new PacketSpawnTFCParticle.Handler(), PacketSpawnTFCParticle.class, ++id, Side.CLIENT);
         network.registerMessage(new PacketSimpleMessage.Handler(), PacketSimpleMessage.class, ++id, Side.CLIENT);
+        network.registerMessage(new PacketBlockInventoryUpdate.Handler(), PacketBlockInventoryUpdate.class, ++id, Side.CLIENT);
 
         EntitiesTFC.preInit();
         JsonConfigRegistry.INSTANCE.preInit(event.getModConfigurationDirectory());
