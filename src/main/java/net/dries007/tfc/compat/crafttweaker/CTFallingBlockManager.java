cfe73bfa769249e3e58145ed30ea64513e384f2d
Rongmario <ethanmahmood@gmail.com>
2021-02-03 20:03:21 +0000

    More methods exposed to CT

--- src/main/java/net/dries007/tfc/compat/crafttweaker/CTFallingBlockManager.java
+++ src/main/java/net/dries007/tfc/compat/crafttweaker/CTFallingBlockManager.java
@@ -19,6 +19,7 @@ import crafttweaker.api.item.IItemStack;
 import crafttweaker.api.minecraft.CraftTweakerMC;
 import crafttweaker.api.world.IBlockPos;
 import crafttweaker.api.world.IWorld;
+import crafttweaker.mc1120.block.MCBlockState;
 import net.dries007.tfc.api.util.FallingBlockManager;
 import net.dries007.tfc.client.TFCSounds;
 import stanhebben.zenscript.annotations.Optional;
@@ -97,15 +98,22 @@ public class CTFallingBlockManager
     {
 
         @ZenMethod
-        public static CTSpecification create(boolean canFallHorizontally, boolean collapsable)
+        public static CTSpecification create(boolean canFallHorizontally, boolean canCaveIn)
         {
-            return new CTSpecification(canFallHorizontally, collapsable, () -> TFCSounds.ROCK_SLIDE_SHORT);
+            return new CTSpecification(canFallHorizontally, canCaveIn, () -> TFCSounds.ROCK_SLIDE_SHORT);
         }
 
         @ZenMethod
-        public static CTSpecification create(boolean canFallHorizontally, boolean collapsable, String soundEventId, @Optional CTFallDropsProvider dropsProvider)
+        public static CTSpecification create(boolean canFallHorizontally, boolean canCaveIn, String soundEventId, @Optional CTFallDropsProvider dropsProvider)
         {
-            return new CTSpecification(canFallHorizontally, collapsable, new ResourceLocation(soundEventId), dropsProvider);
+            return new CTSpecification(canFallHorizontally, canCaveIn, new ResourceLocation(soundEventId), dropsProvider);
+        }
+
+        @ZenMethod
+        public static CTSpecification get(IBlockState state)
+        {
+            FallingBlockManager.Specification spec = FallingBlockManager.getSpecification((net.minecraft.block.state.IBlockState) state.getInternal());
+            return spec == null ? null : new CTSpecification(spec);
         }
 
         private static FallingBlockManager.Specification.FallDropsProvider transform(CTFallDropsProvider ctProvider)
@@ -123,24 +131,29 @@ public class CTFallingBlockManager
 
         final FallingBlockManager.Specification internalSpec;
 
-        CTSpecification(boolean canFallHorizontally, boolean collapsable, ResourceLocation soundEventId)
+        CTSpecification(FallingBlockManager.Specification internalSpec)
         {
-            this(canFallHorizontally, collapsable, () -> SoundEvent.REGISTRY.getObject(soundEventId));
+            this.internalSpec = internalSpec;
         }
 
-        CTSpecification(boolean canFallHorizontally, boolean collapsable, Supplier<SoundEvent> soundEvent)
+        CTSpecification(boolean canFallHorizontally, boolean canCaveIn, ResourceLocation soundEventId)
         {
-            this.internalSpec = new FallingBlockManager.Specification(canFallHorizontally, collapsable, soundEvent, FallingBlockManager.Specification.DEFAULT_DROPS_PROVIDER);
+            this(canFallHorizontally, canCaveIn, () -> SoundEvent.REGISTRY.getObject(soundEventId));
         }
 
-        CTSpecification(boolean canFallHorizontally, boolean collapsable, ResourceLocation soundEventId, CTFallDropsProvider dropsProvider)
+        CTSpecification(boolean canFallHorizontally, boolean canCaveIn, Supplier<SoundEvent> soundEvent)
         {
-            this(canFallHorizontally, collapsable, () -> SoundEvent.REGISTRY.getObject(soundEventId), dropsProvider);
+            this.internalSpec = new FallingBlockManager.Specification(canFallHorizontally, canCaveIn, soundEvent, FallingBlockManager.Specification.DEFAULT_DROPS_PROVIDER);
         }
 
-        CTSpecification(boolean canFallHorizontally, boolean collapsable, Supplier<SoundEvent> soundEvent, CTFallDropsProvider drops)
+        CTSpecification(boolean canFallHorizontally, boolean canCaveIn, ResourceLocation soundEventId, CTFallDropsProvider dropsProvider)
         {
-            this.internalSpec = new FallingBlockManager.Specification(canFallHorizontally, collapsable, soundEvent, transform(drops));
+            this(canFallHorizontally, canCaveIn, () -> SoundEvent.REGISTRY.getObject(soundEventId), dropsProvider);
+        }
+
+        CTSpecification(boolean canFallHorizontally, boolean canCaveIn, Supplier<SoundEvent> soundEvent, CTFallDropsProvider drops)
+        {
+            this.internalSpec = new FallingBlockManager.Specification(canFallHorizontally, canCaveIn, soundEvent, transform(drops));
         }
 
         @ZenMethod
@@ -166,12 +179,80 @@ public class CTFallingBlockManager
             this.internalSpec.setResultingState(((net.minecraft.block.state.IBlockState) block.getInternal()));
         }
 
+        @ZenMethod
+        public void whenBeginningFall(CTBeginFallCallback beginFall)
+        {
+            this.internalSpec.setBeginFallCallback((world, pos) -> beginFall.beginFall(CraftTweakerMC.getIWorld(world), CraftTweakerMC.getIBlockPos(pos)));
+        }
+
+        @ZenMethod
+        public void whenEndingFall(CTEndFallCallback endFall)
+        {
+            this.internalSpec.setEndFallCallback((world, pos) -> endFall.endFall(CraftTweakerMC.getIWorld(world), CraftTweakerMC.getIBlockPos(pos)));
+        }
+
+        @ZenMethod
+        public void whenCavingIn(CTCollapseChecker collapseChecker)
+        {
+            if (!this.internalSpec.isCollapsable())
+            {
+                throw new IllegalArgumentException("This specification does not allow for cave-ins!");
+            }
+            this.internalSpec.setCollapseCondition((world, pos) -> collapseChecker.canCollapse(CraftTweakerMC.getIWorld(world), CraftTweakerMC.getIBlockPos(pos)));
+        }
+
+        @ZenMethod
+        public boolean canFallHorizontally()
+        {
+            return this.internalSpec.canFallHorizontally();
+        }
+
+        @ZenMethod
+        public boolean canCaveIn()
+        {
+            return this.internalSpec.isCollapsable();
+        }
+
+        @ZenMethod
+        public IBlockState getCustomResultingState()
+        {
+            if (this.internalSpec.getResultingState() == null)
+            {
+                return null;
+            }
+            return new MCBlockState(this.internalSpec.getResultingState());
+        }
+
         @ZenRegister
         @FunctionalInterface
         @ZenClass("mods.terrafirmacraft.fallingblock.FallDropsProvider")
         public interface CTFallDropsProvider
         {
             IItemStack[] getDropsFromFall(IWorld world, IBlockPos pos, IBlockState state, IData teData, int fallTime, float fallDistance);
+        }
+
+        @ZenRegister
+        @FunctionalInterface
+        @ZenClass("mods.terrafirmacraft.fallingblock.BeginFallCallback")
+        public interface CTBeginFallCallback
+        {
+            void beginFall(IWorld world, IBlockPos startPos);
+        }
+
+        @ZenRegister
+        @FunctionalInterface
+        @ZenClass("mods.terrafirmacraft.fallingblock.EndFallCallback")
+        public interface CTEndFallCallback
+        {
+            void endFall(IWorld world, IBlockPos endPos);
+        }
+
+        @ZenRegister
+        @FunctionalInterface
+        @ZenClass("mods.terrafirmacraft.fallingblock.CollapseChecker")
+        public interface CTCollapseChecker
+        {
+            boolean canCollapse(IWorld world, IBlockPos collapsePos);
         }
 
     }
