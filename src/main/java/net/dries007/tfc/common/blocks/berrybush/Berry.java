91e3bbd3e5df588b162e13ac962e16ab34a79e6b
EERussianguy <eerussianguy@gmail.com>
2021-05-14 11:35:33 -0400

    update licenses

--- src/main/java/net/dries007/tfc/common/blocks/berrybush/Berry.java
+++ src/main/java/net/dries007/tfc/common/blocks/berrybush/Berry.java
@@ -1,3 +1,9 @@
+/*
+ * Licensed under the EUPL, Version 1.2.
+ * You may obtain a copy of the Licence at:
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ */
+
 package net.dries007.tfc.common.blocks.berrybush;
 
 //todo: this is a temporary enum for registration. Feel free to tear it apart
