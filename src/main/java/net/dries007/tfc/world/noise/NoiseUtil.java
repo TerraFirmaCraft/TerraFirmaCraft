88416c14b069ac02a30e73904be6f21be53c441e
AlcatrazEscapee <alex@molleroneill.com>
2021-08-08 16:12:43 -0400

    Licenses + remove old stuff.

--- src/main/java/net/dries007/tfc/world/noise/NoiseUtil.java
+++ src/main/java/net/dries007/tfc/world/noise/NoiseUtil.java
@@ -1,28 +1,7 @@
 /*
- * This was originally part of FastNoise (https://github.com/Auburn/FastNoise) and has been included as per the MIT license:
- *
- * MIT License
- *
- * Copyright(c) 2020 Jordan Peck (jordan.me2@gmail.com)
- * Copyright(c) 2020 Contributors
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files(the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions :
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Licensed under the EUPL, Version 1.2.
+ * You may obtain a copy of the Licence at:
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
  */
 
 package net.dries007.tfc.world.noise;
