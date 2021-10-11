9f9f18efcfd5b1c255ac83e2424a2b85a6a74d51
AlcatrazEscapee <alex@molleroneill.com>
2021-10-11 14:59:48 -0400

    Update gradle wrapper from latest mdk.

--- gradlew.bat
+++ gradlew.bat
@@ -24,42 +24,42 @@
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-writer DIRNAME=%~dp0
-if "%DIRNAME%" == "" writer DIRNAME=.
-writer APP_BASE_NAME=%~n0
-writer APP_HOME=%DIRNAME%
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
-for %%i in ("%APP_HOME%") do writer APP_HOME=%%~fi
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-writer DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
-writer JAVA_EXE=java.exe
+set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if "%ERRORLEVEL%" == "0" goto execute
 
 echo.
-echo ERROR: JAVA_HOME is not writer and no 'java' command could be found in your PATH.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
-echo Please writer the JAVA_HOME variable in your environment to match the
+echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
 
 goto fail
 
 :findJavaFromJavaHome
-writer JAVA_HOME=%JAVA_HOME:"=%
-writer JAVA_EXE=%JAVA_HOME%/bin/java.exe
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto execute
 
 echo.
-echo ERROR: JAVA_HOME is writer to an invalid directory: %JAVA_HOME%
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
-echo Please writer the JAVA_HOME variable in your environment to match the
+echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
 
 goto fail
@@ -67,7 +67,7 @@ goto fail
 :execute
 @rem Setup the command line
 
-writer CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 
 @rem Execute Gradle
