1f7bd7ee202cd15252b3625f9f14f77337ccb244
Charnuz <charnuz.sc2@gmail.com>
2022-03-11 18:17:27 -0600

    use helper method for loom texture

--- src/main/java/net/dries007/tfc/common/blocks/wood/Wood.java
+++ src/main/java/net/dries007/tfc/common/blocks/wood/Wood.java
@@ -14,7 +14,6 @@ import java.util.function.Supplier;
 import javax.annotation.Nullable;
 
 import net.minecraft.core.Direction;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
@@ -32,10 +31,9 @@ import net.dries007.tfc.common.blocks.ExtendedProperties;
 import net.dries007.tfc.common.blocks.GroundcoverBlock;
 import net.dries007.tfc.common.blocks.TFCBlocks;
 import net.dries007.tfc.common.items.ChestBlockItem;
-import net.dries007.tfc.util.Helpers;
 import net.dries007.tfc.world.feature.tree.TFCTreeGrower;
 
-import static net.dries007.tfc.TerraFirmaCraft.MOD_ID;
+import static net.dries007.tfc.util.Helpers.identifier;
 
 /**
  * Default wood types used for block registration calls
@@ -78,7 +76,7 @@ public enum Wood implements StringRepresentable
         this.mainColor = mainColor;
         this.topColor = topColor;
         this.barkColor = barkColor;
-        this.tree = new TFCTreeGrower(Helpers.identifier("tree/" + serializedName), Helpers.identifier("tree/" + serializedName + "_large"));
+        this.tree = new TFCTreeGrower(identifier("tree/" + serializedName), identifier("tree/" + serializedName + "_large"));
         this.maxDecayDistance = maxDecayDistance;
         this.daysToGrow = daysToGrow;
     }
@@ -151,7 +149,7 @@ public enum Wood implements StringRepresentable
         WORKBENCH(wood -> new TFCCraftingTableBlock(ExtendedProperties.of(properties(wood).strength(2.5F)).flammable(60, 30)), true),
         CHEST((self, wood) -> new TFCChestBlock(ExtendedProperties.of(properties(wood).strength(2.5F)).flammable(60, 30).blockEntity(TFCBlockEntities.CHEST).clientTicks(ChestBlockEntity::lidAnimateTick), wood.getSerializedName()), false, (block, properties) -> new ChestBlockItem(block, properties)),
         TRAPPED_CHEST((self, wood) -> new TFCTrappedChestBlock(ExtendedProperties.of(properties(wood).strength(2.5F)).flammable(60, 30).blockEntity(TFCBlockEntities.TRAPPED_CHEST).clientTicks(ChestBlockEntity::lidAnimateTick), wood.getSerializedName()), false, (block, properties) -> new ChestBlockItem(block, properties)),
-        LOOM(wood -> new TFCLoomBlock(ExtendedProperties.of(properties(wood).strength(2.5F).noOcclusion()).flammable(60, 30).blockEntity(TFCBlockEntities.LOOM).ticks(LoomBlockEntity::tick), new ResourceLocation(MOD_ID, "block/wood/planks/" + wood.name().toLowerCase())), true);
+        LOOM(wood -> new TFCLoomBlock(ExtendedProperties.of(properties(wood).strength(2.5F).noOcclusion()).flammable(60, 30).blockEntity(TFCBlockEntities.LOOM).ticks(LoomBlockEntity::tick), identifier("block/wood/planks/" + wood.name().toLowerCase())), true);
 
         public static final BlockType[] VALUES = values();
 
