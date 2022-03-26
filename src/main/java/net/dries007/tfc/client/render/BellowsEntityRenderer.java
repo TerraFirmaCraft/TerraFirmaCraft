d46d638409216860c8eddbbf252c81bac13b1eac
Charnuz <charnuz.sc2@gmail.com>
2022-03-26 16:44:35 -0500

    second iteration of bellows renderer

--- src/main/java/net/dries007/tfc/client/render/BellowsEntityRenderer.java
+++ src/main/java/net/dries007/tfc/client/render/BellowsEntityRenderer.java
@@ -19,14 +19,27 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Matrix3f;
 import com.mojang.math.Matrix4f;
 import com.mojang.math.Vector3f;
-import net.dries007.tfc.client.ClientHelpers;
+import net.dries007.tfc.client.RenderHelpers;
 import net.dries007.tfc.common.blockentities.BellowsBlockEntity;
 import net.dries007.tfc.common.blocks.devices.BellowsBlock;
 import net.dries007.tfc.util.Helpers;
 
 public class BellowsEntityRenderer implements BlockEntityRenderer<BellowsBlockEntity>
 {
-    private static final ResourceLocation TEXTURE = Helpers.identifier("block/bellows_side.png");
+    private static final ResourceLocation BACK_TEXTURE = Helpers.identifier("block/bellows_back");
+    private static final ResourceLocation SIDE_TEXTURE = Helpers.identifier("block/bellows_side");
+    // The number of planes each side of the bellows has
+    private static final int planeCount = 4;
+    private static final double texWidth = 16D / planeCount;
+
+    // The width of the frond and back of the bellows. Each are 2/16th of a block wide
+    private static final float headWidth = 0.125f;
+    private static final float bellowsWidthMin = 0.125f;
+    private static final float bellowsWidthMax = 0.875f;
+
+    private static final float indentBase = 0.0125f;
+    private static final float indentFactor = 1.5f;
+
 
     @Override
     public void render(BellowsBlockEntity bellows, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int combinedLight, int combinedOverlay)
@@ -34,34 +47,46 @@ public class BellowsEntityRenderer implements BlockEntityRenderer<BellowsBlockEn
         int meta = bellows.getBlockState().getValue(BellowsBlock.FACING).get2DDataValue();
 
         poseStack.pushPose();
-        TextureAtlasSprite endSprite = Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS).apply(Helpers.identifier("block/bellows_back"));
-        TextureAtlasSprite sideSprite = Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS).apply(Helpers.identifier("block/bellows_side"));
+        TextureAtlasSprite endSprite = Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS).apply(BACK_TEXTURE);
+        TextureAtlasSprite sideSprite = Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS).apply(SIDE_TEXTURE);
 
         VertexConsumer builder = buffer.getBuffer(RenderType.cutout());
         poseStack.translate(0.5d, 0, 0.5d);
         poseStack.mulPose(Vector3f.YP.rotationDegrees(180.0F - 90.0F * meta));
         poseStack.translate(-0.5d, 0.0d, -0.5d);
 
-        float tileY = 1 - bellows.getHeight();
-        drawMiddle(builder, poseStack, sideSprite, tileY, combinedOverlay, combinedLight);
-        drawTop(builder, poseStack, endSprite, tileY, combinedOverlay, combinedLight);
+        float width = 1 - bellows.getExtensionLength();
+        drawMiddle(builder, poseStack, sideSprite, width, combinedOverlay, combinedLight);
+        drawTop(builder, poseStack, endSprite, width, combinedOverlay, combinedLight);
         poseStack.popPose();
     }
 
-    private void drawMiddle(VertexConsumer b, PoseStack matrixStack, TextureAtlasSprite sprite, float y, int combinedOverlay, int combinedLight)
+    private void drawMiddle(VertexConsumer b, PoseStack matrixStack, TextureAtlasSprite sprite, float width, int combinedOverlay, int combinedLight)
     {
-        float[][] sides = ClientHelpers.getVerticesBySide(0.125f, 0.875f, y, 0.875f, 0.125f, 0.125f, "xy");
-
-        for (float[] v : sides)
+        float widthPerSection = (width - headWidth) / planeCount;
+        float currentWidth = headWidth;
+        float lastWidth = currentWidth;
+        float change = indentBase * (indentFactor / width);
+        for (int i = 0; i < planeCount; i++)
         {
-            vertex(b, matrixStack.last().pose(), matrixStack.last().normal(), v[0], v[1], v[2], sprite.getU((v[3] * 16D)), sprite.getV((v[4] * 16D)), combinedOverlay, combinedLight);
+            boolean isIndented = i % 2 == 0;
+            currentWidth += widthPerSection;
+            float min = isIndented ? bellowsWidthMin + change : bellowsWidthMin;
+            float max = isIndented ? bellowsWidthMax - change : bellowsWidthMax;
+            for (float[] v : getVertices(min, max, currentWidth, max, min, lastWidth, isIndented ? -change : change, isIndented ? -change : change))
+            {
+                // Texture needs to the reversed due to the directionMapper the planes are rendered in
+                // Otherwise the texture is cut up and displayed out of order
+                vertex(b, matrixStack.last().pose(), matrixStack.last().normal(), v[0], v[1], v[2], sprite.getU(v[3] * -texWidth + (texWidth * (i + 1))), sprite.getV(v[4] * 16D), combinedOverlay, combinedLight);
+            }
+            lastWidth = currentWidth;
         }
     }
 
-    private void drawTop(VertexConsumer b, PoseStack matrixStack, TextureAtlasSprite sprite, float y, int combinedOverlay, int combinedLight)
+    private void drawTop(VertexConsumer b, PoseStack matrixStack, TextureAtlasSprite sprite, float width, int combinedOverlay, int combinedLight)
     {
-        float[][] sides = ClientHelpers.getVerticesBySide(0, 1, 0.125f + y, 1, 0, y, "xy");
-        float[][] tops = ClientHelpers.getVerticesBySide(0, 1, 0.125f + y, 1, 0, y, "z");
+        float[][] sides = RenderHelpers.getVerticesBySide(0, 1, 0.125f + width, 1, 0, width, "xy");
+        float[][] tops = RenderHelpers.getVerticesBySide(0, 1, 0.125f + width, 1, 0, width, "z");
 
         for (float[] v : sides)
         {
@@ -69,13 +94,43 @@ public class BellowsEntityRenderer implements BlockEntityRenderer<BellowsBlockEn
         }
         for (float[] v : tops)
         {
-            vertex(b, matrixStack.last().pose(), matrixStack.last().normal(), v[0], v[1], v[2], sprite.getU(v[3] * 16D), sprite.getV(v[4] * 16D), combinedOverlay, combinedLight);
+            vertex(b, matrixStack.last().pose(), matrixStack.last().normal(), v[0], v[1], v[2], sprite.getU(v[3] * 16F), sprite.getV(v[4] * 16D), combinedOverlay, combinedLight);
         }
     }
 
     private void vertex(VertexConsumer builder, Matrix4f mat, Matrix3f norm, float x, float y, float z, float u, float v, int combinedOverlay, int combinedLight)
     {
-        builder.vertex(mat, x, y, z).color(1.0F, 1.0F, 1.0F, 1.0F).uv(u, v).uv2(combinedLight).overlayCoords(combinedOverlay).normal(norm, 1F, 1F, 1F).endVertex();
+        builder.vertex(mat, x, y, z).color(1.0F, 1.0F, 1.0F, 1.0F).uv(u, v).uv2(combinedLight).overlayCoords(combinedOverlay).normal(norm, 1, 1, 1).endVertex();
     }
+
+    public static float[][] getVertices(float minX, float minY, float minZ, float maxX, float maxY, float maxZ, float changeX, float changeY)
+    {
+        return new float[][] {
+            // Main +X Side
+            {minX, minY, minZ, 0, 1},
+            {minX + changeX, minY - changeY, maxZ, 1, 1},
+            {minX + changeX, maxY + changeY, maxZ, 1, 0},
+            {minX, maxY, minZ, 0, 0},
+
+            // Main -X Side
+            {maxX - changeX, minY - changeY, maxZ, 1, 0},
+            {maxX, minY, minZ, 0, 0},
+            {maxX, maxY, minZ, 0, 1},
+            {maxX - changeX, maxY + changeY, maxZ, 1, 1},
+
+            // Bottom
+            {minX, maxY, minZ, 0, 1},
+            {minX + changeX, maxY + changeY, maxZ, 1, 1},
+            {maxX - changeX, maxY + changeY, maxZ, 1, 0},
+            {maxX, maxY, minZ, 0, 0},
+
+            // Top
+            {minX + changeX, minY - changeY, maxZ, 1, 0},
+            {minX, minY, minZ, 0, 0},
+            {maxX, minY, minZ, 0, 1},
+            {maxX - changeX, minY - changeY, maxZ, 1, 1}
+        };
+    }
+
 
 }
