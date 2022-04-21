0a6776267ef0c5505a5f0548afb1605dc3a2c4f4
AlcatrazEscapee <alex@molleroneill.com>
2022-04-21 19:49:09 -0400

    Fixes for jug sizing (Closes #1921), intoxication, and replace the server side default level type.

--- resources/data.py
+++ resources/data.py
@@ -606,7 +606,7 @@ def generate(rm: ResourceManager):
 
     drinkable(rm, 'fresh_water', ['minecraft:water', 'tfc:river_water'], thirst=10)
     drinkable(rm, 'salt_water', 'tfc:salt_water', thirst=-1)
-    drinkable(rm, 'alcohol', '#tfc:alcohols', thirst=10, intoxication=1000)
+    drinkable(rm, 'alcohol', '#tfc:alcohols', thirst=10, intoxication=4000)
     drinkable(rm, 'milk', '#tfc:milks', thirst=10)
 
     # Climate Ranges
---
+++ src/main/java/net/dries007/tfc/mixin/DedicatedServerPropertiesMixin.java
@@ -0,0 +1,23 @@
+/*
+ * Licensed under the EUPL, Version 1.2.
+ * You may obtain a copy of the Licence at:
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ */
+
+package net.dries007.tfc.mixin;
+
+import net.minecraft.server.dedicated.DedicatedServerProperties;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(DedicatedServerProperties.class)
+public abstract class DedicatedServerPropertiesMixin
+{
+    @ModifyConstant(method = "<init>", constant = @Constant(stringValue = "default"))
+    private static String selectDefaultWorldType(String type)
+    {
+        return "tfc:tng";
+    }
+}
