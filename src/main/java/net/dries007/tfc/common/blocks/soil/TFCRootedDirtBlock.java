2bff0edaf499f2752c873cc8922f6eb3462c810e
AlcatrazEscapee <alex@molleroneill.com>
2022-04-23 12:39:47 -0400

    Replacements for new Forge deprecations.

--- src/main/java/net/dries007/tfc/common/blocks/soil/TFCRootedDirtBlock.java
+++ src/main/java/net/dries007/tfc/common/blocks/soil/TFCRootedDirtBlock.java
@@ -7,12 +7,8 @@
 package net.dries007.tfc.common.blocks.soil;
 
 import java.util.function.Supplier;
-import org.jetbrains.annotations.Nullable;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.ToolAction;
@@ -20,6 +16,7 @@ import net.minecraftforge.common.ToolActions;
 
 import net.dries007.tfc.common.blocks.TFCBlocks;
 import net.dries007.tfc.config.TFCConfig;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Dirt that doesn't let grass spread to it but can still be interacted on
@@ -36,10 +33,9 @@ public class TFCRootedDirtBlock extends Block
 
     @Nullable
     @Override
-    public BlockState getToolModifiedState(BlockState state, Level world, BlockPos pos, Player player, ItemStack stack, ToolAction action)
+    public BlockState getToolModifiedState(BlockState state, UseOnContext context, ToolAction action, boolean simulate)
     {
-        //todo: should idiomatically be hoe till action, which forge didn't implement yet... https://github.com/MinecraftForge/MinecraftForge/pull/8025
-        if (stack.canPerformAction(action) && action == ToolActions.SHOVEL_FLATTEN && TFCConfig.SERVER.enableGrassPathCreation.get())
+        if (context.getItemInHand().canPerformAction(action) && action == ToolActions.HOE_TILL && TFCConfig.SERVER.enableGrassPathCreation.get())
         {
             return dirt.get().defaultBlockState();
         }
