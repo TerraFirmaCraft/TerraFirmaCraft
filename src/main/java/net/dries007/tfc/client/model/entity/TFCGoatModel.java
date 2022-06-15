04d458288cd63fbc12e7ebb79f2a1dd2c750add2
Therighthon <jackwitecki@gmail.com>
2022-06-15 13:28:22 -0700

    Rooster and Goat Model Fixes
    Fixed head rotation for goats and roosters
    Made horns shorter on female goats
    
    Goat fix does not match vanilla implementation, but achieves similar results in game.

--- src/main/java/net/dries007/tfc/client/model/entity/TFCGoatModel.java
+++ src/main/java/net/dries007/tfc/client/model/entity/TFCGoatModel.java
@@ -35,7 +35,12 @@ public class TFCGoatModel extends QuadrupedModel<DairyAnimal>
     {
         this.head.getChild("left_horn").visible = !goat.isBaby();
         this.head.getChild("right_horn").visible = !goat.isBaby();
-        super.setupAnim(goat, limbSwing, limbSwingAmount, ageInTicks, headYaw, headPitch);
+        if (goat.displayFemaleCharacteristics())
+        {
+            this.head.getChild("left_horn").y = 2;
+            this.head.getChild("right_horn").y = 2;
+        }
+        super.setupAnim(goat, limbSwing, limbSwingAmount, ageInTicks, headYaw / 3, headPitch);
     }
 }
 
