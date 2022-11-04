99305a38ff5f619d13e4e5dafc340b91ddd89ea2
AlcatrazEscapee <alex@molleroneill.com>
2022-11-04 08:24:11 -0400

    Fix candles being immediately extinguished via right click, if you lit them via a fire starter. This only occurred when not shifting (which would be unique to candles, unlike other lightable blocks). And while this is a hack, it's the cleanest hack that preserves all interesting behavior. Closes #2192

--- src/main/java/net/dries007/tfc/common/blocks/TFCCandleBlock.java
+++ src/main/java/net/dries007/tfc/common/blocks/TFCCandleBlock.java
@@ -11,11 +11,18 @@ import java.util.Random;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.CandleBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 
 import net.dries007.tfc.common.blockentities.TFCBlockEntities;
+import net.dries007.tfc.common.items.TFCItems;
 import net.dries007.tfc.config.TFCConfig;
+import net.dries007.tfc.util.Helpers;
 
 public class TFCCandleBlock extends CandleBlock implements IForgeBlockExtension, EntityBlockExtension
 {
@@ -25,6 +32,18 @@ public class TFCCandleBlock extends CandleBlock implements IForgeBlockExtension,
     {
         super(properties.properties());
         this.properties = properties;
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit)
+    {
+        // This is a QoL fix for candle-firestarter interactions. Candles extinguish on right click, so firestarter + NOT SHIFT = immediately extinguished, after you light it.
+        // Firestarter can only be used in the main hand, so if we detect that specific case, we don't allow the candle to be extinguished.
+        if (Helpers.isItem(player.getMainHandItem(), TFCItems.FIRESTARTER.get()))
+        {
+            return InteractionResult.PASS;
+        }
+        return super.use(state, level, pos, player, hand, hit);
     }
 
     @Override
