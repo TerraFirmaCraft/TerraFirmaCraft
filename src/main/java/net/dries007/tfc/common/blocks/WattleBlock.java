4338bbb1d5cd05e9130df3a38b66b668e3d93b61
EERussianguy <eerussianguy@gmail.com>
2022-12-07 11:07:53 -0500

    Make wattle fluid loggable and break when fallen on. (Closes #2255)

--- src/main/java/net/dries007/tfc/common/blocks/WattleBlock.java
+++ src/main/java/net/dries007/tfc/common/blocks/WattleBlock.java
@@ -10,14 +10,19 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -26,24 +31,29 @@ import net.minecraftforge.common.Tags;
 
 import net.dries007.tfc.client.IGhostBlockHandler;
 import net.dries007.tfc.client.TFCSounds;
+import net.dries007.tfc.common.fluids.FluidHelpers;
+import net.dries007.tfc.common.fluids.FluidProperty;
+import net.dries007.tfc.common.fluids.IFluidLoggable;
 import net.dries007.tfc.common.items.TFCItems;
 import net.dries007.tfc.util.Helpers;
 import org.jetbrains.annotations.Nullable;
 
-public class WattleBlock extends StainedWattleBlock implements IGhostBlockHandler
+public class WattleBlock extends StainedWattleBlock implements IGhostBlockHandler, IFluidLoggable
 {
     public static final BooleanProperty WOVEN = TFCBlockStateProperties.WATTLE_WOVEN;
+    public static final FluidProperty FLUID = TFCBlockStateProperties.WATER;
 
     public WattleBlock(ExtendedProperties properties)
     {
         super(properties);
-        registerDefaultState(getStateDefinition().any().setValue(TOP, false).setValue(BOTTOM, false).setValue(LEFT, false).setValue(RIGHT, false).setValue(WOVEN, false));
+        registerDefaultState(getStateDefinition().any().setValue(TOP, false).setValue(BOTTOM, false).setValue(LEFT, false).setValue(RIGHT, false).setValue(WOVEN, false).setValue(getFluidProperty(), getFluidProperty().keyFor(Fluids.EMPTY)));
     }
 
     @Override
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit)
     {
         if (hand == InteractionHand.OFF_HAND) return InteractionResult.PASS;
+        if (state.getValue(getFluidProperty()).getFluid() != Fluids.EMPTY) return InteractionResult.PASS;
         final ItemStack item = player.getItemInHand(hand);
         final boolean woven = state.getValue(WOVEN);
         if (!woven && Helpers.isItem(item, Tags.Items.RODS_WOODEN) && item.getCount() >= 4)
@@ -63,7 +73,8 @@ public class WattleBlock extends StainedWattleBlock implements IGhostBlockHandle
     @Override
     public BlockState getStateToDraw(Level level, Player player, BlockState lookState, Direction direction, BlockPos lookPos, double x, double y, double z, ItemStack item)
     {
-        boolean woven = lookState.getValue(WOVEN);
+        if (lookState.getValue(getFluidProperty()).getFluid() != Fluids.EMPTY) return null;
+        final boolean woven = lookState.getValue(WOVEN);
         if (!woven && Helpers.isItem(item, Tags.Items.RODS_WOODEN) && item.getCount() >= 4)
         {
             return lookState.setValue(WOVEN, true);
@@ -73,6 +84,24 @@ public class WattleBlock extends StainedWattleBlock implements IGhostBlockHandle
             return TFCBlocks.UNSTAINED_WATTLE.get().withPropertiesOf(lookState);
         }
         return super.getStateToDraw(level, player, lookState, direction, lookPos, x, y, z, item);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context)
+    {
+        final FluidState fluidState = context.getLevel().getFluidState(context.getClickedPos());
+        if (getFluidProperty().canContain(fluidState.getType()))
+        {
+            return defaultBlockState().setValue(getFluidProperty(), getFluidProperty().keyFor(fluidState.getType()));
+        }
+        return defaultBlockState();
+    }
+
+    @Override
+    public FluidProperty getFluidProperty()
+    {
+        return FLUID;
     }
 
     @Override
@@ -93,5 +122,29 @@ public class WattleBlock extends StainedWattleBlock implements IGhostBlockHandle
     public VoxelShape getOcclusionShape(BlockState state, BlockGetter level, BlockPos pos)
     {
         return state.getValue(WOVEN) ? super.getOcclusionShape(state, level, pos) : Shapes.empty();
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public FluidState getFluidState(BlockState state)
+    {
+        return IFluidLoggable.super.getFluidState(state);
+    }
+
+    @Override
+    public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor level, BlockPos currentPos, BlockPos facingPos)
+    {
+        FluidHelpers.tickFluid(level, currentPos, state);
+        return super.updateShape(state, facing, facingState, level, currentPos, facingPos);
+    }
+
+    @Override
+    public void fallOn(Level level, BlockState state, BlockPos pos, Entity entity, float distance)
+    {
+        super.fallOn(level, state, pos, entity, distance);
+        if (distance > 3f)
+        {
+            level.destroyBlock(pos, true);
+        }
     }
 }
