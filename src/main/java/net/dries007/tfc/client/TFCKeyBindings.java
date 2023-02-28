0b789c9dffa93ab38991365678394886d7069426
EERussianguy <eerussianguy@gmail.com>
2023-02-28 12:29:41 -0500

    (experimental) mouse key support for tfc keybinds in config.

--- src/main/java/net/dries007/tfc/client/TFCKeyBindings.java
+++ src/main/java/net/dries007/tfc/client/TFCKeyBindings.java
@@ -6,16 +6,26 @@
 
 package net.dries007.tfc.client;
 
-import org.lwjgl.glfw.GLFW;
+import java.util.function.Supplier;
+import net.minecraftforge.client.settings.IKeyConflictContext;
 import net.minecraft.client.KeyMapping;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraftforge.client.settings.KeyConflictContext;
+import org.lwjgl.glfw.GLFW;
+
+import net.dries007.tfc.config.TFCConfig;
 
 import static net.dries007.tfc.TerraFirmaCraft.MOD_NAME;
 
 public class TFCKeyBindings
 {
-    public static final KeyMapping PLACE_BLOCK = new KeyMapping("tfc.key.place_block", KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_V, MOD_NAME);
-    public static final KeyMapping CYCLE_CHISEL_MODE = new KeyMapping("tfc.key.cycle_chisel_mode", KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_M, MOD_NAME);
-    public static final KeyMapping STACK_FOOD = new KeyMapping("tfc.key.stack_food", KeyConflictContext.GUI, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_I, MOD_NAME);
+    public static final KeyMapping PLACE_BLOCK = create("tfc.key.place_block", KeyConflictContext.IN_GAME, InputConstants.KEY_V, GLFW.GLFW_MOUSE_BUTTON_4, TFCConfig.CLIENT.useMouseKeyForSpecialPlace);
+    public static final KeyMapping CYCLE_CHISEL_MODE = create("tfc.key.cycle_chisel_mode", KeyConflictContext.IN_GAME, InputConstants.KEY_M, GLFW.GLFW_MOUSE_BUTTON_5, TFCConfig.CLIENT.useMouseKeyForChiselCycle);
+    public static final KeyMapping STACK_FOOD = create("tfc.key.stack_food", KeyConflictContext.GUI, InputConstants.KEY_I, GLFW.GLFW_MOUSE_BUTTON_6, TFCConfig.CLIENT.useMouseKeyForFoodStack);
+
+    public static KeyMapping create(String description, IKeyConflictContext keyConflictContext, int keyCode, int mouseKeyCode, Supplier<Boolean> mouseKey)
+    {
+        return new KeyMapping(description, keyConflictContext, mouseKey.get() ? InputConstants.Type.MOUSE : InputConstants.Type.KEYSYM, mouseKey.get() ? mouseKeyCode : keyCode, MOD_NAME);
+    }
+
 }
