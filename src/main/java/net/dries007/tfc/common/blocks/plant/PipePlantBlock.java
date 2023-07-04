4016348a85e0426d7dc7119c5a1c80b955259ec2
EERussianguy <eerussianguy@gmail.com>
2023-07-04 11:16:21 -0400

    create an abstraction for six-way plants, update licenses

---
+++ src/main/java/net/dries007/tfc/common/blocks/plant/PipePlantBlock.java
@@ -0,0 +1,137 @@
+package net.dries007.tfc.common.blocks.plant;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.PipeBlock;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+
+import net.dries007.tfc.common.blocks.DirectionPropertyBlock;
+import net.dries007.tfc.common.blocks.ExtendedProperties;
+import net.dries007.tfc.common.blocks.IForgeBlockExtension;
+
+public abstract class PipePlantBlock extends PipeBlock implements IForgeBlockExtension
+{
+    private final ExtendedProperties properties;
+
+    public PipePlantBlock(float size, ExtendedProperties properties)
+    {
+        super(size, properties.properties());
+        this.properties = properties;
+    }
+
+    @Override
+    public ExtendedProperties getExtendedProperties()
+    {
+        return properties;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
+    {
+        super.createBlockStateDefinition(builder.add(NORTH, EAST, SOUTH, WEST, UP, DOWN));
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor level, BlockPos currentPos, BlockPos facingPos)
+    {
+        if (!state.canSurvive(level, currentPos))
+        {
+            level.scheduleTick(currentPos, this, 1);
+            return state;
+        }
+        else
+        {
+            final boolean exists = (facing.getAxis().isHorizontal() && testHorizontal(facingState)) || (facing == Direction.DOWN && testDown(facingState)) || (facing == Direction.UP && testUp(facingState));
+            return state.setValue(PROPERTY_BY_DIRECTION.get(facing), exists);
+        }
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context)
+    {
+        return this.getStateForPlacement(context.getLevel(), context.getClickedPos());
+    }
+
+    public BlockState getStateForPlacement(BlockGetter level, BlockPos pos)
+    {
+        final BlockState down = level.getBlockState(pos.below());
+        final BlockState up = level.getBlockState(pos.above());
+        final BlockState north = level.getBlockState(pos.north());
+        final BlockState east = level.getBlockState(pos.east());
+        final BlockState south = level.getBlockState(pos.south());
+        final BlockState west = level.getBlockState(pos.west());
+        return defaultBlockState()
+            .setValue(DOWN, testDown(down))
+            .setValue(UP, testUp(up))
+            .setValue(NORTH, testHorizontal(north))
+            .setValue(WEST, testHorizontal(west))
+            .setValue(EAST, testHorizontal(east))
+            .setValue(SOUTH, testHorizontal(south));
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos)
+    {
+        final BlockPos.MutableBlockPos cursor = new BlockPos.MutableBlockPos().set(pos);
+        for (Direction direction : Direction.Plane.HORIZONTAL)
+        {
+            cursor.setWithOffset(pos, direction);
+            // if we have a horizontal branch
+            if (testHorizontal(level.getBlockState(cursor)))
+            {
+                // check for a block below
+                cursor.setWithOffset(pos, 0, -1, 0);
+                if (testDown(level.getBlockState(cursor)))
+                {
+                    return true;
+                }
+            }
+        }
+        cursor.setWithOffset(pos, 0, -1, 0);
+        return testDown(level.getBlockState(cursor));
+    }
+
+    protected abstract boolean testDown(BlockState state);
+
+    protected abstract boolean testUp(BlockState state);
+
+    protected abstract boolean testHorizontal(BlockState state);
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public BlockState rotate(BlockState state, Rotation rot)
+    {
+        return DirectionPropertyBlock.rotate(state, rot);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public BlockState mirror(BlockState state, Mirror mirror)
+    {
+        return DirectionPropertyBlock.mirror(state, mirror);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource rand)
+    {
+        if (!state.canSurvive(level, pos))
+        {
+            level.destroyBlock(pos, true);
+        }
+    }
+
+
+}
--- src/main/java/net/dries007/tfc/common/blocks/plant/fruit/FruitTreeBranchBlock.java
+++ src/main/java/net/dries007/tfc/common/blocks/plant/fruit/FruitTreeBranchBlock.java
@@ -9,53 +9,34 @@ package net.dries007.tfc.common.blocks.plant.fruit;
 import java.util.List;
 import java.util.function.Supplier;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Mirror;
-import net.minecraft.world.level.block.PipeBlock;
-import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
 import net.dries007.tfc.common.TFCTags;
-import net.dries007.tfc.common.blocks.DirectionPropertyBlock;
 import net.dries007.tfc.common.blocks.ExtendedProperties;
-import net.dries007.tfc.common.blocks.IForgeBlockExtension;
 import net.dries007.tfc.common.blocks.TFCBlockStateProperties;
+import net.dries007.tfc.common.blocks.plant.PipePlantBlock;
 import net.dries007.tfc.common.blocks.soil.FarmlandBlock;
 import net.dries007.tfc.common.blocks.soil.HoeOverlayBlock;
 import net.dries007.tfc.util.Helpers;
 import net.dries007.tfc.util.climate.ClimateRange;
 
-import static net.dries007.tfc.common.blocks.plant.fruit.FruitTreeSaplingBlock.maySplice;
+import static net.dries007.tfc.common.blocks.plant.fruit.FruitTreeSaplingBlock.*;
 
-public class FruitTreeBranchBlock extends PipeBlock implements IForgeBlockExtension, HoeOverlayBlock
+public class FruitTreeBranchBlock extends PipePlantBlock implements HoeOverlayBlock
 {
     public static final IntegerProperty STAGE = TFCBlockStateProperties.STAGE_3;
-    private final ExtendedProperties properties;
     private final Supplier<ClimateRange> climateRange;
 
     public FruitTreeBranchBlock(ExtendedProperties properties, Supplier<ClimateRange> climateRange)
     {
-        super(0.25F, properties.properties());
-        this.properties = properties;
+        super(0.25F, properties);
         this.climateRange = climateRange;
         registerDefaultState(stateDefinition.any().setValue(NORTH, false).setValue(EAST, false).setValue(SOUTH, false).setValue(WEST, false).setValue(UP, false).setValue(DOWN, false));
-    }
-
-    @Override
-    public BlockState getStateForPlacement(BlockPlaceContext context)
-    {
-        return getStateForPlacement(context.getLevel(), context.getClickedPos());
     }
 
     @Override
@@ -80,91 +61,25 @@ public class FruitTreeBranchBlock extends PipeBlock implements IForgeBlockExtens
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
     {
-        super.createBlockStateDefinition(builder.add(NORTH, EAST, SOUTH, WEST, UP, DOWN, STAGE));
-    }
-
-    public BlockState getStateForPlacement(BlockGetter level, BlockPos pos)
-    {
-        Block downBlock = level.getBlockState(pos.below()).getBlock();
-        Block upBlock = level.getBlockState(pos.above()).getBlock();
-        Block northBlock = level.getBlockState(pos.north()).getBlock();
-        Block eastBlock = level.getBlockState(pos.east()).getBlock();
-        Block southBlock = level.getBlockState(pos.south()).getBlock();
-        Block westBlock = level.getBlockState(pos.west()).getBlock();
-        return defaultBlockState()
-            .setValue(DOWN, Helpers.isBlock(downBlock, TFCTags.Blocks.FRUIT_TREE_BRANCH) || Helpers.isBlock(downBlock, TFCTags.Blocks.BUSH_PLANTABLE_ON))
-            .setValue(UP, Helpers.isBlock(upBlock, TFCTags.Blocks.FRUIT_TREE_BRANCH) || Helpers.isBlock(upBlock, TFCTags.Blocks.FRUIT_TREE_SAPLING))
-            .setValue(NORTH, Helpers.isBlock(northBlock, TFCTags.Blocks.FRUIT_TREE_BRANCH))
-            .setValue(EAST, Helpers.isBlock(eastBlock, TFCTags.Blocks.FRUIT_TREE_BRANCH))
-            .setValue(SOUTH, Helpers.isBlock(southBlock, TFCTags.Blocks.FRUIT_TREE_BRANCH))
-            .setValue(WEST, Helpers.isBlock(westBlock, TFCTags.Blocks.FRUIT_TREE_BRANCH));
+        super.createBlockStateDefinition(builder.add(STAGE));
     }
 
     @Override
-    @SuppressWarnings("deprecation")
-    public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor level, BlockPos currentPos, BlockPos facingPos)
+    protected boolean testDown(BlockState state)
     {
-        if (!state.canSurvive(level, currentPos))
-        {
-            level.scheduleTick(currentPos, this, 1);
-            return state;
-        }
-        else
-        {
-            boolean flag = Helpers.isBlock(facingState, TFCTags.Blocks.FRUIT_TREE_BRANCH) || (facing == Direction.DOWN && Helpers.isBlock(facingState, TFCTags.Blocks.BUSH_PLANTABLE_ON) || (facing == Direction.UP && Helpers.isBlock(facingState, TFCTags.Blocks.FRUIT_TREE_SAPLING)));
-            return state.setValue(PROPERTY_BY_DIRECTION.get(facing), flag);
-        }
+        return Helpers.isBlock(state, TFCTags.Blocks.FRUIT_TREE_BRANCH) || Helpers.isBlock(state, TFCTags.Blocks.BUSH_PLANTABLE_ON);
     }
 
     @Override
-    @SuppressWarnings("deprecation")
-    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos)
+    protected boolean testUp(BlockState state)
     {
-        BlockState belowState = level.getBlockState(pos.below());
-        for (Direction direction : Direction.Plane.HORIZONTAL)
-        {
-            BlockPos relativePos = pos.relative(direction);
-            if (Helpers.isBlock(level.getBlockState(relativePos).getBlock(), TFCTags.Blocks.FRUIT_TREE_BRANCH))
-            {
-                Block below = level.getBlockState(relativePos.below()).getBlock();
-                if (Helpers.isBlock(below, TFCTags.Blocks.FRUIT_TREE_BRANCH) || Helpers.isBlock(below, TFCTags.Blocks.BUSH_PLANTABLE_ON))
-                {
-                    return true;
-                }
-            }
-        }
-        Block block = belowState.getBlock();
-        return Helpers.isBlock(block, TFCTags.Blocks.FRUIT_TREE_BRANCH) || Helpers.isBlock(block, TFCTags.Blocks.BUSH_PLANTABLE_ON);
+        return Helpers.isBlock(state, TFCTags.Blocks.FRUIT_TREE_BRANCH) || Helpers.isBlock(state, TFCTags.Blocks.FRUIT_TREE_SAPLING);
     }
 
     @Override
-    @SuppressWarnings("deprecation")
-    public void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource rand)
+    protected boolean testHorizontal(BlockState state)
     {
-        if (!state.canSurvive(level, pos) && !level.isClientSide())
-        {
-            level.destroyBlock(pos, true);
-        }
+        return Helpers.isBlock(state, TFCTags.Blocks.FRUIT_TREE_BRANCH);
     }
 
-    @Override
-    public ExtendedProperties getExtendedProperties()
-    {
-        return properties;
-    }
-
-
-    @Override
-    @SuppressWarnings("deprecation")
-    public BlockState rotate(BlockState state, Rotation rot)
-    {
-        return DirectionPropertyBlock.rotate(state, rot);
-    }
-
-    @Override
-    @SuppressWarnings("deprecation")
-    public BlockState mirror(BlockState state, Mirror mirror)
-    {
-        return DirectionPropertyBlock.mirror(state, mirror);
-    }
 }
