e8c8750bcd8cf348bed83de1023ea4b26f8aceee
06395706314 <06395706314@tjce.jus.br>
2023-08-14 22:54:54 -0300

    Fix eating animation of the rat.

--- src/main/java/net/dries007/tfc/common/entities/prey/Pest.java
+++ src/main/java/net/dries007/tfc/common/entities/prey/Pest.java
@@ -97,7 +97,7 @@ public class Pest extends Prey
     public void tick()
     {
         final ItemStack held = getMainHandItem();
-        if (!held.isEmpty())
+        if (!held.isEmpty() || eatingAnimation.isStarted())
         {
             dragTicks++;
             if (dragTicks < DRAG_TIME)
@@ -119,7 +119,7 @@ public class Pest extends Prey
                         playSound(SoundEvents.GENERIC_EAT, getSoundVolume(), getVoicePitch());
                     }
                 }
-                if (dragTicks > DRAG_TIME + EAT_TIME)
+                if (dragTicks > (DRAG_TIME + EAT_TIME))
                 {
                     setItemSlot(EquipmentSlot.MAINHAND, ItemStack.EMPTY);
                     if (level.isClientSide)
