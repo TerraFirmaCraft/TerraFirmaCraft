3360111b6f0908713f3f8a695ca9e03e27406072
Peffern <salamander496@gmail.com>
2023-10-11 01:42:17 -0400

    add mixins

--- src/main/java/net/dries007/tfc/client/render/blockentity/TFCHangingSignBlockEntityRenderer.java
+++ src/main/java/net/dries007/tfc/client/render/blockentity/TFCHangingSignBlockEntityRenderer.java
@@ -10,10 +10,16 @@ import java.util.Map;
 import java.util.stream.Stream;
 import com.google.common.collect.ImmutableMap;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.dries007.tfc.common.blocks.wood.Wood;
+import net.dries007.tfc.util.Metal;
+import net.minecraft.client.model.Model;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.blockentity.HangingSignRenderer;
+import net.minecraft.client.resources.model.Material;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.SignBlock;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
@@ -56,8 +62,46 @@ public class TFCHangingSignBlockEntityRenderer extends HangingSignRenderer
         final BlockState blockstate = sign.getBlockState();
         final SignBlock signblock = (SignBlock)blockstate.getBlock();
         final WoodType woodtype = SignBlock.getWoodType(signblock);
+
+        // Placeholder - should determine the metal from the SignBlock instance.
+        // TODO register TFCCeilingHangingSignBlock and TFCWallHangingSignBlock versions for every combination
+        // for now just unknown so that we have something to pass.
+        final Metal metal = Metal.unknown();
+
         final HangingSignRenderer.HangingSignModel model = this.hangingSignModels.get(woodtype);
         model.evaluateVisibleParts(blockstate);
-        ((SignRendererAccessor) (Object) this).invoke$renderSignWithText(sign, poseStack, buffer, light, overlay, blockstate, signblock, woodtype, model);
+        this.renderSignWithText(sign, poseStack, buffer, light, overlay, blockstate, signblock, woodtype, metal, model);
     }
+
+    // behavior copied from SignRenderer#renderSignWithText
+    void renderSignWithText(SignBlockEntity sign, PoseStack poseStack, MultiBufferSource buffer, int light, int overlay, BlockState blockstate, SignBlock signblock, WoodType woodtype, Metal metal, Model model) {
+        poseStack.pushPose();
+        ((SignRendererAccessor) this).invoke$translateSign(poseStack, -signblock.getYRotationDegrees(blockstate), blockstate);
+        this.renderSign(poseStack, buffer, light, overlay, woodtype, metal, model);
+        ((SignRendererAccessor) this).invoke$renderSignText(sign.getBlockPos(), sign.getFrontText(), poseStack, buffer, light, sign.getTextLineHeight(), sign.getMaxTextLineWidth(), true);
+        ((SignRendererAccessor) this).invoke$renderSignText(sign.getBlockPos(), sign.getBackText(), poseStack, buffer, light, sign.getTextLineHeight(), sign.getMaxTextLineWidth(), false);
+        poseStack.popPose();
+    }
+
+    // behavior copied from SignRenderer#renderSign
+    void renderSign(PoseStack poseStack, MultiBufferSource buffer, int light, int overlay, WoodType woodtype, Metal metal, Model model) {
+        poseStack.pushPose();
+        float f = this.getSignModelRenderScale();
+        poseStack.scale(f, -f, -f);
+
+        Material material = this.getCompoundSignMaterial(woodtype, metal);
+
+        VertexConsumer vertexconsumer = material.buffer(buffer, model::renderType);
+        ((SignRendererAccessor) this).invoke$renderSignModel(poseStack, light, overlay, model, vertexconsumer);
+        poseStack.popPose();
+    }
+
+    private Material getCompoundSignMaterial(WoodType woodType, Metal metal) {
+        // Placeholder - should use the provided woodType and metal to determine the material.
+        // TODO register new sign materials somewhere so they can be looked up here
+        // for now just use willow so that there's visual confirmation the mixins worked.
+        return Sheets.getHangingSignMaterial(Wood.WILLOW.getVanillaWoodType());
+    }
+
+
 }
--- src/main/java/net/dries007/tfc/mixin/client/accessor/SignRendererAccessor.java
+++ src/main/java/net/dries007/tfc/mixin/client/accessor/SignRendererAccessor.java
@@ -7,11 +7,15 @@
 package net.dries007.tfc.mixin.client.accessor;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.dries007.tfc.util.Metal;
 import net.minecraft.client.model.Model;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.SignRenderer;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.SignBlock;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
+import net.minecraft.world.level.block.entity.SignText;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.WoodType;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,4 +26,13 @@ public interface SignRendererAccessor
 {
     @Invoker("renderSignWithText")
     void invoke$renderSignWithText(SignBlockEntity sign, PoseStack poseStack, MultiBufferSource buffers, int light, int overlay, BlockState state, SignBlock block, WoodType wood, Model model);
+
+    @Invoker("translateSign")
+    void invoke$translateSign(PoseStack poseStack, float rotationDegrees, BlockState blockState);
+
+    @Invoker("renderSignText")
+    void invoke$renderSignText(BlockPos blockPos, SignText signText, PoseStack poseStack, MultiBufferSource buffer, int light, int lineHeight, int lineWidth, boolean mirrored);
+
+    @Invoker("renderSignModel")
+    void invoke$renderSignModel(PoseStack poseStack, int light, int overlay, Model model, VertexConsumer vertexconsumer);
 }
