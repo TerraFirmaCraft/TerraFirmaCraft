d4f38b4373a54f27047a83fad2db92c28c09b676
Peffern <salamander496@gmail.com>
2023-10-16 19:48:01 -0400

    license

--- src/main/java/net/dries007/tfc/mixin/client/HangingSignEditScreenMixin.java
+++ src/main/java/net/dries007/tfc/mixin/client/HangingSignEditScreenMixin.java
@@ -1,10 +1,15 @@
+/*
+ * Licensed under the EUPL, Version 1.2.
+ * You may obtain a copy of the Licence at:
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ */
+
 package net.dries007.tfc.mixin.client;
 
 import net.dries007.tfc.common.blocks.wood.ITFCHangingSignBlock;
 import net.minecraft.client.gui.screens.inventory.HangingSignEditScreen;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SignBlock;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
