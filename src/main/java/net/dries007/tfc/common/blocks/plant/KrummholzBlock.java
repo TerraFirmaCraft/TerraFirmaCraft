1362674b1aafcf4787bf1951eabcbaf8595badfc
EERussianguy <eerussianguy@gmail.com>
2023-10-30 18:33:32 -0400

    tweaks for krummholz -- add snow with rclick, random offsets like bamboo.

--- src/main/java/net/dries007/tfc/common/blocks/plant/KrummholzBlock.java
+++ src/main/java/net/dries007/tfc/common/blocks/plant/KrummholzBlock.java
@@ -10,16 +10,25 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.pathfinder.PathComputationType;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -29,6 +38,7 @@ import net.dries007.tfc.common.blocks.ExtendedBlock;
 import net.dries007.tfc.common.blocks.ExtendedProperties;
 import net.dries007.tfc.common.blocks.TFCBlockStateProperties;
 import net.dries007.tfc.common.blocks.devices.BottomSupportedDeviceBlock;
+import net.dries007.tfc.util.Helpers;
 
 public class KrummholzBlock extends ExtendedBlock
 {
@@ -62,6 +72,21 @@ public class KrummholzBlock extends ExtendedBlock
     {
         super(properties);
         registerDefaultState(getStateDefinition().any().setValue(TIP, false).setValue(SNOWY, false).setValue(BOTTOM, false));
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result)
+    {
+        final ItemStack held = player.getItemInHand(hand);
+        if (held.getItem() == Items.SNOW && !state.getValue(SNOWY))
+        {
+            level.setBlockAndUpdate(pos, state.cycle(SNOWY));
+            held.shrink(1);
+            Helpers.playPlaceSound(level, pos, SoundType.SNOW);
+            return InteractionResult.sidedSuccess(level.isClientSide);
+        }
+        return InteractionResult.PASS;
     }
 
     @Override
@@ -113,7 +138,9 @@ public class KrummholzBlock extends ExtendedBlock
     public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context)
     {
         final boolean addSnow = state.getValue(BOTTOM) && state.getValue(SNOWY);
-        return state.getValue(TIP) ? addSnow ? TIP_WITH_SNOW_SHAPE : TIP_SHAPE : addSnow ? NORMAL_WITH_SNOW_SHAPE : NORMAL_SHAPE;
+        final VoxelShape shape = state.getValue(TIP) ? addSnow ? TIP_WITH_SNOW_SHAPE : TIP_SHAPE : addSnow ? NORMAL_WITH_SNOW_SHAPE : NORMAL_SHAPE;
+        final Vec3 offset = state.getOffset(level, pos);
+        return shape.move(offset.x, offset.y, offset.z);
     }
 
     @Override
@@ -121,4 +148,33 @@ public class KrummholzBlock extends ExtendedBlock
     {
         super.createBlockStateDefinition(builder.add(TIP, SNOWY, BOTTOM));
     }
+
+    @Override
+    public boolean propagatesSkylightDown(BlockState state, BlockGetter level, BlockPos pos)
+    {
+        return true;
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public boolean isPathfindable(BlockState pState, BlockGetter pLevel, BlockPos pPos, PathComputationType pType)
+    {
+        return false;
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context)
+    {
+        Vec3 vec3 = state.getOffset(level, pos);
+        return NORMAL_SHAPE.move(vec3.x, vec3.y, vec3.z);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public boolean isCollisionShapeFullBlock(BlockState pState, BlockGetter pLevel, BlockPos pPos)
+    {
+        return false;
+    }
+
 }
