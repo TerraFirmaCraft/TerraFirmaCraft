8e0e88c3565dc4b44b397e22be5b9aed0554d5ee
EERussianguy <eerussianguy@gmail.com>
2023-11-06 11:22:27 -0500

    Fix wooden bucket coloring (Closes #2541)

--- src/main/java/net/dries007/tfc/client/ClientEventHandler.java
+++ src/main/java/net/dries007/tfc/client/ClientEventHandler.java
@@ -764,7 +764,7 @@ public final class ClientEventHandler
         }
 
         TFCItems.MOLDS.values().forEach(reg -> event.register(new ContainedFluidModel.Colors(), reg.get()));
-        event.register(new ContainedFluidModel.Colors(), TFCItems.BELL_MOLD.get(), TFCItems.FIRE_INGOT_MOLD.get(), TFCItems.JUG.get(), TFCItems.SILICA_GLASS_BOTTLE.get(), TFCItems.HEMATITIC_GLASS_BOTTLE.get(), TFCItems.VOLCANIC_GLASS_BOTTLE.get(), TFCItems.OLIVINE_GLASS_BOTTLE.get());
+        event.register(new ContainedFluidModel.Colors(), TFCItems.WOODEN_BUCKET.get(), TFCItems.BELL_MOLD.get(), TFCItems.FIRE_INGOT_MOLD.get(), TFCItems.JUG.get(), TFCItems.SILICA_GLASS_BOTTLE.get(), TFCItems.HEMATITIC_GLASS_BOTTLE.get(), TFCItems.VOLCANIC_GLASS_BOTTLE.get(), TFCItems.OLIVINE_GLASS_BOTTLE.get());
     }
 
     public static void registerClientReloadListeners(RegisterClientReloadListenersEvent event)
