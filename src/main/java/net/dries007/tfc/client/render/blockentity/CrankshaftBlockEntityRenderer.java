1ad405033a55af9f3fe5ac8dc785085c2a086726
AlcatrazEscapee <alex@molleroneill.com>
2023-11-16 19:14:43 -0500

    Fix various issues with the crankshaft renderer.

--- src/main/java/net/dries007/tfc/client/render/blockentity/CrankshaftBlockEntityRenderer.java
+++ src/main/java/net/dries007/tfc/client/render/blockentity/CrankshaftBlockEntityRenderer.java
@@ -14,7 +14,6 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.ModelBlockRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
-import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
@@ -28,13 +27,10 @@ import net.minecraftforge.client.model.data.ModelData;
 
 import net.dries007.tfc.client.RenderHelpers;
 import net.dries007.tfc.common.blockentities.rotation.CrankshaftBlockEntity;
-import net.dries007.tfc.common.blocks.TFCBlocks;
-import net.dries007.tfc.common.blocks.rotation.AxleBlock;
 import net.dries007.tfc.common.blocks.rotation.ConnectedAxleBlock;
 import net.dries007.tfc.common.blocks.rotation.CrankshaftBlock;
-import net.dries007.tfc.common.blocks.wood.Wood;
 import net.dries007.tfc.util.Helpers;
-import net.dries007.tfc.util.rotation.Node;
+
 
 public class CrankshaftBlockEntityRenderer implements BlockEntityRenderer<CrankshaftBlockEntity>
 {
@@ -48,7 +44,7 @@ public class CrankshaftBlockEntityRenderer implements BlockEntityRenderer<Cranks
         final BlockPos pos = crankshaft.getBlockPos();
         final BlockState state = crankshaft.getBlockState();
 
-        if (!(state.getBlock() instanceof CrankshaftBlock crankshaftBlock) || level == null)
+        if (!(state.getBlock() instanceof CrankshaftBlock) || level == null)
         {
             return;
         }
@@ -69,29 +65,22 @@ public class CrankshaftBlockEntityRenderer implements BlockEntityRenderer<Cranks
 
         final VertexConsumer buffer = bufferSource.getBuffer(RenderType.cutout());
 
-        final float rotationAngle = crankshaft.getRotationAngle(partialTick);
+        final float sourceRotationAngle = crankshaft.getRotationAngle(partialTick);
+        final float rotationAngle = face == Direction.NORTH || face == Direction.EAST ? Mth.TWO_PI - sourceRotationAngle : sourceRotationAngle;
 
         stack.pushPose();
-
-        stack.translate(0.5f, 0, 0.5f);
-        float rotation = switch (face)
-        {
-            case EAST -> 270f;
-            case WEST -> 90f;
-            case SOUTH -> 180f;
-            default -> 0f;
-        };
-        stack.mulPose(Axis.YP.rotationDegrees(rotation));
-        stack.translate(-0.5f, 0, -0.5f);
+        stack.translate(0.5f, 0.5f, 0.5f);
+        stack.mulPose(Axis.YP.rotationDegrees(180f - 90f * face.get2DDataValue()));
 
         if (part == CrankshaftBlock.Part.BASE)
         {
-            stack.translate(0f, 0.5f, 0.5f);
-            stack.mulPose(Axis.XN.rotation(-rotationAngle + Mth.PI));
-            stack.translate(0f, -0.5f, -0.5f);
+            stack.mulPose(Axis.XP.rotation(rotationAngle + Mth.PI));
+            stack.translate(-0.5f, -0.5f, -0.5f);
 
+            // Render the attached wheel, rotating with the axle
             final ModelBlockRenderer modelRenderer = Minecraft.getInstance().getBlockRenderer().getModelRenderer();
             final BakedModel baked = Minecraft.getInstance().getModelManager().getModel(WHEEL_MODEL);
+
             modelRenderer.tesselateWithAO(level, baked, crankshaft.getBlockState(), crankshaft.getBlockPos(), stack, buffer, true, level.getRandom(), packedLight, packedOverlay, ModelData.EMPTY, RenderType.cutout());
 
             // Render an extension of the axle
@@ -106,46 +95,65 @@ public class CrankshaftBlockEntityRenderer implements BlockEntityRenderer<Cranks
         }
         else
         {
+            stack.translate(-0.5f, -0.5f, -0.5f);
+
             final TextureAtlasSprite rodSprite = Minecraft.getInstance().getTextureAtlas(RenderHelpers.BLOCKS_ATLAS).apply(ROD_TEXTURE);
 
             // Render three parts of the shaft
             // Two parts are static boxes that move back and forth
             // The third is the one that is rotated s.t. it connects to the wheel, and the other two
-            final int quadrant = (int) (rotationAngle / Mth.HALF_PI);
+            final int quadrant = Mth.clamp((int) (rotationAngle / Mth.HALF_PI), 0, 3);
 
-            final float unitCircleAngle = switch (quadrant)
-                {
-                    case 3 -> rotationAngle;
-                    case 2 -> rotationAngle - Mth.PI;
-                    case 1 -> Mth.PI - rotationAngle;
-                    default -> rotationAngle;
-                };
+            final float unitCircleAngle = quadrant == 0 || quadrant == 3
+                ? rotationAngle
+                : (quadrant == 1
+                    ? Mth.PI - rotationAngle
+                    : rotationAngle - Mth.PI);
 
             // All measurements are in pixels
-            final float wheelRadius = 4 / 16f;
+            final float wheelRadius = 2.5f / 16f; // todo: wider wheel? And then measure center <-> center point to connecting arm, and change here.
             final float armLength = 12 / 16f;
+            final float armRadius = 1 / 16f; // Half of the thickness of the arm
             final float px = (1 / 16f);
 
-            final float offsetH = wheelRadius * Mth.sin(unitCircleAngle);
-            final float offsetC = wheelRadius * Mth.cos(unitCircleAngle);
-            final float offsetY = Mth.sqrt(armLength * armLength - offsetH * offsetH);
+            // Where:
+            //   O := The center of the circle with radius R
+            //        The radius is the distance to the _center_ of the connecting point - not the whole radius of the wheel
+            //   H := The center of the connecting box
+            //   A := The point where the line OH intersects the circle
+            //   B := A point on the radius of the circle s.t. the angle HOB is ~45° - call this angle theta
+            //   C := A point on the line OH s.t. angles OCB and HCB are 90°
+            //
+            // Then, relative to the shaft block's origin, where the direction conventions are:
+            //   +x := Into the page (The axis of the connected axle)
+            //   +y := Up
+            //   +z := Left (the axis of the shaft)
+            //
+            // We have the following positions:
+            //
+            // O = (0.5, 0.5, 1.5)
+            //   Note that the X value describes the point touching the wheel, but the midpoint of the shaft is actually 0.5 + armRadius
+            //
+            // |OB| = |OA| = R
+            // |BH| = armLength, but the length between connecting points (so total length rendered += 2 x armRadius)
+            // |CB| = R sin theta
+            // |OC| = R cos theta
+            // L^2 = |CB|^2 + |CH|^2
 
-            final float raiseAngle = (float) Math.acos(offsetY / armLength);
-            final float actualRaiseAngle = switch (quadrant)
-                {
-                    case 3, 2 -> raiseAngle;
-                    default -> -raiseAngle;
-                };
+            final float lengthCB = wheelRadius * Mth.sin(unitCircleAngle);
+            final float lengthOC = wheelRadius * Mth.cos(unitCircleAngle);
+            final float lengthCH = Mth.sqrt(armLength * armLength - lengthCB * lengthCB);
 
-            // Offset from the center of the wheel, to the connecting point that moves horizontally
-            final float totalOffset = (rotationAngle > Mth.HALF_PI && rotationAngle < Mth.PI + Mth.HALF_PI)
-                ? offsetC + offsetY
-                : -offsetC + offsetY;
+            final float angleHCB = (float) Math.acos(lengthCH / armLength);
+
+            final float raiseAngle = quadrant == 2 || quadrant == 3
+                ? angleHCB
+                : -angleHCB;
 
             // Draw the two flat cuboids, based on `totalOffset`
 
-            // Translate to the horizontal connecting point, so we can rotate from here
-            stack.translate(9 / 16f, 8 / 16f, totalOffset);
+            // Translate to the (center of the) horizontal connecting point, so we can rotate from here
+            stack.translate(8 / 16f + armRadius, 8 / 16f, 1.5f - lengthCH + (quadrant == 1 || quadrant == 2 ? lengthOC : -lengthOC));
 
             // Draw the connecting (flat) cuboid
             RenderHelpers.renderTexturedCuboid(stack, buffer, rodSprite, packedLight, packedOverlay, -2 * px, -2 * px, -2 * px, 2 * px, 2 * px, 2 * px);
@@ -154,8 +162,9 @@ public class CrankshaftBlockEntityRenderer implements BlockEntityRenderer<Cranks
             RenderHelpers.renderTexturedCuboid(stack, buffer, rodSprite, packedLight, packedOverlay, -px, -px, -17 * px, px, px, px);
 
             // Raise the connecting cuboid to the right angle and render
-            stack.mulPose(Axis.XP.rotation(actualRaiseAngle));
-            RenderHelpers.renderTexturedCuboid(stack, buffer, rodSprite, packedLight, packedOverlay, -px, -px, -px, px, px, 11 * px);
+            // +1 / -1's all around due to the radius of the arm around the line segment
+            stack.mulPose(Axis.XP.rotation(raiseAngle));
+            RenderHelpers.renderTexturedCuboid(stack, buffer, rodSprite, packedLight, packedOverlay, -px, -px, -px, px, px, armLength + px);
         }
 
         stack.popPose();
