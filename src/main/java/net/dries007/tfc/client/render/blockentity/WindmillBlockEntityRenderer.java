7187c961275437861824c27feef3c63347989d1f
HyperDashPony <hyperdashpony@gmail.com>
2024-04-30 12:44:55 -0700

    Change checking for sets to happen on gameticks instead of frames, change checking for an identical set to be color-agnostic for dyeable blades

--- src/main/java/net/dries007/tfc/client/render/blockentity/WindmillBlockEntityRenderer.java
+++ src/main/java/net/dries007/tfc/client/render/blockentity/WindmillBlockEntityRenderer.java
@@ -111,12 +111,13 @@ public class WindmillBlockEntityRenderer implements BlockEntityRenderer<Windmill
                 }
             }
 
-
             stack.pushPose();
 
             bladeModel.setupAnim(windmill, partialTick, offsetAngle * i);
             bladeModel.renderToBuffer(stack, bufferSource.getBuffer(RenderType.entityCutoutNoCull(bladeTexture)), packedLight, packedOverlay, color[0], color[1], color[2], 1f);
-            if(bladeCount == WindmillBlockEntity.SLOTS && windmill.hasFullIdenticalSet()){
+
+            if(bladeCount == WindmillBlockEntity.SLOTS && windmill.hasFullIdenticalSet)
+            {
                 bladeModel.renderWindmillExtras(stack, bufferSource.getBuffer(RenderType.entityCutoutNoCull(bladeTexture)), packedLight, packedOverlay, color[0], color[1], color[2], 1f);
             }
 
