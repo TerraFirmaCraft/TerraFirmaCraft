006b06c51856aecd9caabada9e17d565433f8f25
Therighthon <52086285+Therighthon@users.noreply.github.com>
2025-04-05 17:39:39 -0700

    Sandstone placements and rain fix
    Fixed client rainfall being set to zero when rainfall variance is zero
    
    Added raw sandstone to blocks that epiphytes and dry plants can be placed on

--- src/data/java/net/dries007/tfc/data/providers/BuiltinBlockTags.java
+++ src/data/java/net/dries007/tfc/data/providers/BuiltinBlockTags.java
@@ -530,8 +530,8 @@ public class BuiltinBlockTags extends TagsProvider<Block> implements Accessors
         tag(WILD_CROP_GROWS_ON).addTag(BlockTags.DIRT);
         tag(SPREADING_FRUIT_GROWS_ON).addTags(BlockTags.DIRT, FARMLANDS, Tags.Blocks.GRAVELS);
         tag(BUSH_PLANTABLE_ON).addTags(BlockTags.DIRT, FARMLANDS);
-        tag(DRY_PLANT_PLANTABLE_ON).addTags(BlockTags.SAND, Tags.Blocks.SANDS, CRACKED_EARTH, Tags.Blocks.GRAVELS, COARSE_DIRT, BUSH_PLANTABLE_ON);
-        tag(EPIPHYTE_PLANTABLE_ON).addTags(BlockTags.LOGS, STONES_RAW, STONES_HARDENED);
+        tag(DRY_PLANT_PLANTABLE_ON).addTags(BlockTags.SAND, Tags.Blocks.SANDS, CRACKED_EARTH, Tags.Blocks.GRAVELS, COARSE_DIRT, BUSH_PLANTABLE_ON).add(TFCBlocks.SANDSTONE, SandstoneBlockType.RAW);
+        tag(EPIPHYTE_PLANTABLE_ON).addTags(BlockTags.LOGS, STONES_RAW, STONES_HARDENED).add(TFCBlocks.SANDSTONE, SandstoneBlockType.RAW);
         tag(GRASS_PLANTABLE_ON)
             .addTags(BlockTags.DIRT, FARMLANDS, CLAYS)
             .add(TFCBlocks.PEAT, TFCBlocks.PEAT_GRASS);
--- src/main/java/net/dries007/tfc/util/climate/OverworldClimateModel.java
+++ src/main/java/net/dries007/tfc/util/climate/OverworldClimateModel.java
@@ -163,7 +163,7 @@ public class OverworldClimateModel implements ClimateModel
         final float fractionOfYear = ICalendar.getFractionOfYear(calendarTicks, daysInMonth);
 
         // For positive values of variance, drought in winter, rain in summer, reverse for negative values
-        return rainVariance == 0 ? 0 : Helpers.triangle(rainVariance * rainAverage, rainAverage, 1f, fractionOfYear + 0.75f);
+        return rainVariance == 0 ? rainAverage : Helpers.triangle(rainVariance * rainAverage, rainAverage, 1f, fractionOfYear + 0.75f);
     }
 
     @Override
