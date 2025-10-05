254d0a9c128626fff5bf5c42c0331641eab76620
alekiponi <alekiponi@gmail.com>
2025-10-04 19:05:48 -0700

    Revert "- change wind to use a new scale and a new distribution"
    This reverts commit 48ebb967cd8e0290545c29245a91e7b67d5c4701.

--- src/main/java/net/dries007/tfc/client/ClientForgeEventHandler.java
+++ src/main/java/net/dries007/tfc/client/ClientForgeEventHandler.java
@@ -95,6 +95,7 @@ import net.dries007.tfc.network.PlaceBlockSpecialPacket;
 import net.dries007.tfc.network.RequestClimateModelPacket;
 import net.dries007.tfc.network.StackFoodPacket;
 import net.dries007.tfc.network.SwitchInventoryTabPacket;
+import net.dries007.tfc.util.EnvironmentHelpers;
 import net.dries007.tfc.util.Helpers;
 import net.dries007.tfc.util.PhysicalDamageType;
 import net.dries007.tfc.util.calendar.Calendars;
@@ -107,7 +108,6 @@ import net.dries007.tfc.util.tooltip.Tooltips;
 import net.dries007.tfc.world.ChunkGeneratorExtension;
 import net.dries007.tfc.world.chunkdata.ChunkData;
 
-import static net.dries007.tfc.util.tracker.WeatherHelpers.*;
 import static net.minecraft.ChatFormatting.*;
 
 public class ClientForgeEventHandler
@@ -172,7 +172,7 @@ public class ClientForgeEventHandler
                 ));
                 final Vec2 wind = ClimateRenderCache.INSTANCE.getWind();
                 tooltip.add(Component.translatable("tfc.tooltip.wind_speed",
-                    Mth.floor(windKMS(wind)),
+                    Mth.floor(320 * wind.length()),
                     String.format("%.0f", Mth.abs(wind.x * 100)),
                     Helpers.translateEnum(wind.x > 0 ? Direction.EAST : Direction.WEST),
                     String.format("%.0f", Mth.abs(wind.y * 100)),
@@ -306,10 +306,10 @@ public class ClientForgeEventHandler
                         first = false;
                     }
                     tooltip.add(Component.literal(DARK_GRAY
-                            + typeOfComponent(stack.getComponentsPatch().get(component.type()))
-                            + BuiltInRegistries.DATA_COMPONENT_TYPE.getKey(component.type())
-                            + " = "
-                            + component.value()
+                        + typeOfComponent(stack.getComponentsPatch().get(component.type()))
+                        + BuiltInRegistries.DATA_COMPONENT_TYPE.getKey(component.type())
+                        + " = "
+                        + component.value()
                         // Avoid showing the encoding, it's interesting but not necessary. Uncomment if needing to debug
                         //+ " = "
                         //+ component.encodeValue(RegistryOps.create(NbtOps.INSTANCE, Minecraft.getInstance().level.registryAccess()))
@@ -461,7 +461,7 @@ public class ClientForgeEventHandler
             Slot slot = inv.getSlotUnderMouse();
             if (slot != null)
             {
-                PacketDistributor.sendToServer(new StackFoodPacket(slot.index));
+               PacketDistributor.sendToServer(new StackFoodPacket(slot.index));
             }
         }
     }
