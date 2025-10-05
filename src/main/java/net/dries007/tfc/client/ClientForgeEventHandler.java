f53c4876f0065cc6746ac91427e9afb650883251
alekiponi <alekiponi@gmail.com>
2025-10-04 17:58:24 -0700

    don't spawn wind particles when it's raining

--- src/main/java/net/dries007/tfc/client/ClientForgeEventHandler.java
+++ src/main/java/net/dries007/tfc/client/ClientForgeEventHandler.java
@@ -172,11 +172,11 @@ public class ClientForgeEventHandler
                 ));
                 final Vec2 wind = ClimateRenderCache.INSTANCE.getWind();
                 tooltip.add(Component.translatable("tfc.tooltip.wind_speed",
-                    Mth.floor(windKMS(wind)),
-                    String.format("%.0f", Mth.abs(wind.x * 100)),
-                    Helpers.translateEnum(wind.x > 0 ? Direction.EAST : Direction.WEST),
-                    String.format("%.0f", Mth.abs(wind.y * 100)),
-                    Helpers.translateEnum(wind.y > 0 ? Direction.SOUTH : Direction.NORTH))
+                        Mth.floor(windKMS(wind)),
+                        String.format("%.0f", Mth.abs(wind.x * 100)),
+                        Helpers.translateEnum(wind.x > 0 ? Direction.EAST : Direction.WEST),
+                        String.format("%.0f", Mth.abs(wind.y * 100)),
+                        Helpers.translateEnum(wind.y > 0 ? Direction.SOUTH : Direction.NORTH))
                     .getString());
 
                 final ChunkData data = ChunkData.get(mc.level, pos);
@@ -428,17 +428,23 @@ public class ClientForgeEventHandler
                 return;
             final double xBias = wind.x > 0 ? 6 : -6;
             final double zBias = wind.y > 0 ? 6 : -6;
-            final ParticleOptions particle = ClimateRenderCache.INSTANCE.getTemperature() < 0f && level.getRainLevel(0) > 0 ? TFCParticles.SNOWFLAKE.get() : TFCParticles.WIND.get();
-            for (int i = 0; i < count; i++)
+
+            // don't spawn wind particles in rain
+            if (!(ClimateRenderCache.INSTANCE.getTemperature() > 0f && level.getRainLevel(0) > 0))
             {
-                final double x = pos.getX() + Mth.nextDouble(level.random, -12 - xBias, 12 - xBias);
-                final double y = pos.getY() + Mth.nextDouble(level.random, -1, 6);
-                final double z = pos.getZ() + Mth.nextDouble(level.random, -12 - zBias, 12 - zBias);
-                if (level.canSeeSky(BlockPos.containing(x, y, z)))
+                final ParticleOptions particle = ClimateRenderCache.INSTANCE.getTemperature() < 0f && level.getRainLevel(0) > 0 ? TFCParticles.SNOWFLAKE.get() : TFCParticles.WIND.get();
+                for (int i = 0; i < count; i++)
                 {
-                    level.addParticle(particle, x, y, z, 0D, 0D, 0D);
+                    final double x = pos.getX() + Mth.nextDouble(level.random, -12 - xBias, 12 - xBias);
+                    final double y = pos.getY() + Mth.nextDouble(level.random, -1, 6);
+                    final double z = pos.getZ() + Mth.nextDouble(level.random, -12 - zBias, 12 - zBias);
+                    if (level.canSeeSky(BlockPos.containing(x, y, z)))
+                    {
+                        level.addParticle(particle, x, y, z, 0D, 0D, 0D);
+                    }
                 }
             }
+
         }
     }
 
