bc7832121752b11c28fc43041b6f805065635733
AtobaAzul <atoba.azul.radical@gmail.com>
2025-10-11 12:49:05 -0300

    Beaching now removes color
    - Added RemoveDyeModifier itemstack modifier.
    - Dyeable bleaching recipe now uses that.

--- src/data/java/net/dries007/tfc/data/recipes/BarrelRecipes.java
+++ src/data/java/net/dries007/tfc/data/recipes/BarrelRecipes.java
@@ -8,6 +8,8 @@ package net.dries007.tfc.data.recipes;
 
 import java.util.function.Function;
 import java.util.stream.Stream;
+
+import net.dries007.tfc.common.recipes.outputs.*;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
@@ -43,13 +45,6 @@ import net.dries007.tfc.common.recipes.ingredients.HasTraitIngredient;
 import net.dries007.tfc.common.recipes.ingredients.HeatIngredient;
 import net.dries007.tfc.common.recipes.ingredients.LacksTraitIngredient;
 import net.dries007.tfc.common.recipes.ingredients.NotRottenIngredient;
-import net.dries007.tfc.common.recipes.outputs.AddHeatModifier;
-import net.dries007.tfc.common.recipes.outputs.AddTraitModifier;
-import net.dries007.tfc.common.recipes.outputs.CopyInputModifier;
-import net.dries007.tfc.common.recipes.outputs.DyeLeatherModifier;
-import net.dries007.tfc.common.recipes.outputs.EmptyBowlModifier;
-import net.dries007.tfc.common.recipes.outputs.ItemStackProvider;
-import net.dries007.tfc.common.recipes.outputs.RemoveTraitModifier;
 
 import static net.minecraft.world.item.crafting.Ingredient.*;
 
@@ -221,7 +216,7 @@ public interface BarrelRecipes extends Recipes
         barrel("bleach_dyeable")
             .input(fluidOf(SimpleFluid.LYE), 25)
             .input(Ingredient.of(ItemTags.DYEABLE))
-            .output(ItemStackProvider.of(CopyInputModifier.INSTANCE, DyeLeatherModifier.of(DyeColor.WHITE)))
+            .output(ItemStackProvider.of(CopyInputModifier.INSTANCE, RemoveDyeModifier.INSTANCE))
             .sealed(hours(1));
 
         musicDisc(DyeColor.YELLOW, Items.MUSIC_DISC_13);
