fa933f0083b099c4e6b87b97f7025c7170d060a4
Redeix <brayden.j.m.ford@gmail.com>
2026-08-25 14:31:54 -0500

    license check

--- src/main/java/net/dries007/tfc/compat/jei/category/LampFuelRecipeCategory.java
+++ src/main/java/net/dries007/tfc/compat/jei/category/LampFuelRecipeCategory.java
@@ -1,6 +1,6 @@
 /*
  * Licensed under the EUPL, Version 1.2.
- * You may obtain a copy of the License at:
+ * You may obtain a copy of the Licence at:
  * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
  */
 
