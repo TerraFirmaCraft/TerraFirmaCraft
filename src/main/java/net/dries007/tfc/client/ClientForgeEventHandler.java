32fbd59d57582ddf054c54caba6fab99ab5ed58a
Allmoz <allmoz@gmail.com>
2026-09-03 22:06:27 -0400

    fix a tick without cache

--- src/main/java/net/dries007/tfc/client/ClientForgeEventHandler.java
+++ src/main/java/net/dries007/tfc/client/ClientForgeEventHandler.java
@@ -15,7 +15,9 @@ import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.toasts.TutorialToast;
+import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
+import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.FogRenderer;
 import net.minecraft.client.resources.sounds.AmbientSoundHandler;
@@ -135,6 +137,7 @@ public class ClientForgeEventHandler
         bus.addListener(ClientForgeEventHandler::onEffectRender);
         bus.addListener(ClientForgeEventHandler::onRecipesUpdated);
         bus.addListener(IngameOverlays::checkGuiOverlays);
+        bus.addListener(ClientForgeEventHandler::onScreenOpen);
 
     }
 
@@ -397,13 +400,7 @@ public class ClientForgeEventHandler
 
     public static void onClientPlayerLoggedOut(ClientPlayerNetworkEvent.LoggingOut event)
     {
-        // This is fired when logging out, but also when a new server is being created, just after resources are loaded. We don't want
-        // to clear caches there, so guard this behind if there was an actual player that was logging out.
-        if (event.getPlayer() != null)
-        {
-            Calendars.CLIENT.resetToDefault();
-            IndirectHashCollection.clearAllCaches();
-        }
+        Calendars.CLIENT.resetToDefault();
     }
 
     public static void onClientTick(ClientTickEvent.Post event)
@@ -632,5 +629,13 @@ public class ClientForgeEventHandler
     public static void onRecipesUpdated(RecipesUpdatedEvent event)
     {
         Helpers.updateReloadableData(ClientHelpers.getLevelOrThrow().registryAccess(), event.getRecipeManager());
+    }
+
+    private static void onScreenOpen(ScreenEvent.Opening event)
+    {
+        if (event.getScreen() instanceof TitleScreen || event.getScreen() instanceof JoinMultiplayerScreen)
+        {
+            IndirectHashCollection.clearAllCaches();
+        }
     }
 }
