d54b315a6e0cb602a9675959ca18ba25c02302d8
Therighthon <52086285+Therighthon@users.noreply.github.com>
2026-09-07 09:55:42 -0700

    Let's keep that check in onLoggedOut

--- src/main/java/net/dries007/tfc/client/ClientForgeEventHandler.java
+++ src/main/java/net/dries007/tfc/client/ClientForgeEventHandler.java
@@ -403,7 +403,12 @@ public class ClientForgeEventHandler
 
     public static void onClientPlayerLoggedOut(ClientPlayerNetworkEvent.LoggingOut event)
     {
-        Calendars.CLIENT.resetToDefault();
+        // This is fired when logging out, but also when a new server is being created, just after resources are loaded. We don't want
+        // to clear caches there, so guard this behind if there was an actual player that was logging out.
+        if (event.getPlayer() != null)
+        {
+            Calendars.CLIENT.resetToDefault();
+        }
     }
 
     public static void onClientTick(TickEvent.ClientTickEvent event)
